cf3ea517a20a8840bae7ebca35a2d7cf3ed7597a
xiaowangzi123 <1103694415@qq.com>
2020-11-11 20:24:30 +0800

    485. Max Consecutive Ones

---
+++ num_0485_MaxConsecutiveOnes/src/main/java/com/leetcode/Solution.java
@@ -0,0 +1,29 @@
+package com.leetcode;
+
+import org.junit.Test;
+
+/**
+ * @author ：wyq
+ * @date ：Created in 2020/11/11
+ * @description：
+ */
+public class Solution {
+
+    @Test
+    public void test1(){
+        int[] arr = {1,1,0,1,1,1};
+        System.out.println(findMaxConsecutiveOnes(arr));
+    }
+
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int res = 0;
+        int count = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == 1){
+                count++;
+                res = Math.max(res,count);
+            }else count = 0;
+        }
+        return res;
+    }
+}
