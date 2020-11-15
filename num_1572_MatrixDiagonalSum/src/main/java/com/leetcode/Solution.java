1adfad791eca79a96c5694271c4952d6caf59658
xiaowangzi123 <1103694415@qq.com>
2020-11-15 21:48:44 +0800

    1480一位数组的相加问题

--- num_1572_MatrixDiagonalSum/src/main/java/com/leetcode/Solution.java
+++ num_1572_MatrixDiagonalSum/src/main/java/com/leetcode/Solution.java
@@ -6,4 +6,26 @@ package com.leetcode;
  * @description：
  */
 public class Solution {
+    public static void main(String[] args) {
+        int[][] mat = {{1, 2}, {4, 5}};
+        System.out.println(mat.length);
+        Solution obj = new Solution();
+        System.out.println(obj.diagonalSum(mat));
+    }
+
+    public int diagonalSum(int[][] mat) {
+        if (mat.length == 1) return mat[0][0];
+        int row = mat.length;
+        int sum = 0;
+
+        for (int i = 0; i < row; i ++) {
+            sum += mat[i][i] + mat[i][row -i -1];
+        }
+
+        if (row % 2 == 1) {
+            sum -= mat[row/2][row/2];
+        }
+        return sum;
+
+    }
 }
