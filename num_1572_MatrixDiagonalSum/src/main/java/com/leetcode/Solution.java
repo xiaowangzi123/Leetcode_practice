a64bf75e9848220f3186729e776dba21e2410fc3
xiaowangzi123 <1103694415@qq.com>
2020-11-16 17:44:02 +0800

    二维数组的相加问题

--- num_1572_MatrixDiagonalSum/src/main/java/com/leetcode/Solution.java
+++ num_1572_MatrixDiagonalSum/src/main/java/com/leetcode/Solution.java
@@ -14,15 +14,16 @@ public class Solution {
     }
 
     public int diagonalSum(int[][] mat) {
-        if (mat.length == 1) return mat[0][0];
+        if (mat.length == 1)
+            return mat[0][0];
         int row = mat.length;
         int sum = 0;
-        for (int i = 0; i < row; i ++) {
-            sum += mat[i][i] + mat[i][row -i -1];
+        for (int i = 0; i < row; i++) {
+            sum += mat[i][i] + mat[i][row - i - 1];
         }
 
         if (row % 2 == 1) {
-            sum -= mat[row/2][row/2];
+            sum -= mat[row / 2][row / 2];
         }
         return sum;
 
