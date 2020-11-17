54a1650967fcb1d12ac413ac83fd8bdc7124e3d5
xiaowangzi123 <1103694415@qq.com>
2020-11-17 18:47:26 +0800

    1588. Sum of All Odd Length Subarrays

---
+++ num_1588_SumofAllOddLengthSubarrays/src/main/java/com/leetcode/Solution.java
@@ -0,0 +1,20 @@
+package com.leetcode;
+
+/**
+ * @author ：wyq
+ * @date ：Created in 2020/11/17
+ * @description：
+ */
+public class Solution {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        Solution obj = new Solution();
+        System.out.println(obj.sumOddLengthSubarrays(arr));
+    }
+
+    public int sumOddLengthSubarrays(int[] arr) {
+        int res = 0;
+
+        return res;
+    }
+}
