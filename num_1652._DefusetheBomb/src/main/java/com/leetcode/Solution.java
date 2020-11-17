19c6850e2945abe67de15533833c92ca2471e9da
xiaowangzi123 <1103694415@qq.com>
2020-11-17 21:58:22 +0800

    1588. Sum of All Odd Length Subarrays

---
+++ num_1652._DefusetheBomb/src/main/java/com/leetcode/Solution.java
@@ -0,0 +1,38 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author ：wyq
+ * @date ：Created in 2020/11/17
+ * @description：
+ */
+public class Solution {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+        int k = 2;
+        Solution obj = new Solution();
+        System.out.println(Arrays.toString(obj.decrypt(arr, k)));
+
+    }
+
+    public int[] decrypt(int[] code, int k) {
+        int[] res = new int[code.length];
+        if (k == 0) return res;
+        //Define the initial window and initial sum
+        int start = 1, end = k, sum = 0;
+        if (k < 0) {//If k < 0, the starting point will be end of the array.
+            k = -k;
+            start = code.length - k;
+            end = code.length - 1;
+        }
+        for (int i = start; i <= end; i++) sum += code[i];
+        //Scan through the code array as i moving to the right, update the window sum.
+        for (int i = 0; i < code.length; i++) {
+            res[i] = sum;
+            sum -= code[(start++) % code.length];
+            sum += code[(++end) % code.length];
+        }
+        return res;
+    }
+}
