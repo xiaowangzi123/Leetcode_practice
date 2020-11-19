279f9d85951b441201fe2af7961b147a53debcb7
xiaowangzi123 <1103694415@qq.com>
2020-11-19 22:50:10 +0800

    1614.solution

---
+++ all_leetcode_solution/src/main/java/com/leetcode/string/Solution1614.java
@@ -0,0 +1,31 @@
+package com.leetcode.string;
+
+/**
+ * @author ：wyq
+ * @date ：Created in 2020/11/19
+ * @description：
+ */
+public class Solution1614 {
+
+    public static void main(String[] args) {
+        Solution1614 obj = new Solution1614();
+        String str = "()()";
+        System.out.println(obj.maxDepth(str));
+    }
+
+
+    public int maxDepth(String s) {
+        int maxDeep = 0, deep = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                deep++;
+            } else if (s.charAt(i) == ')') {
+                deep--;
+            }
+            maxDeep = Math.max(maxDeep, deep);
+        }
+        return maxDeep;
+    }
+
+
+}
