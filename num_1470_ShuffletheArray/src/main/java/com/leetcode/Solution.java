eab0bd768a48d2e4bd01a91db334c6c86dca45e4
xiaowangzi123 <1103694415@qq.com>
2020-11-19 20:02:39 +0800

    1470. Shuffle the Array

---
+++ num_1470_ShuffletheArray/src/main/java/com/leetcode/Solution.java
@@ -0,0 +1,22 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author ：wyq
+ * @date ：Created in 2020/11/19
+ * @description：
+ */
+public class Solution {
+    public static void main(String[] args) {
+        int[] arrs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        Solution obj = new Solution();
+        System.out.println(Arrays.toString(obj.shuffle(arrs, 3)));
+    }
+
+    public int[] shuffle(int[] nums, int n) {
+        int[] res = new int[nums.length];
+
+        return res;
+    }
+}
