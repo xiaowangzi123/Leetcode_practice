e18505951b0ce2a435fb781410ed330bb54a7831
xiaowangzi123 <1103694415@qq.com>
2020-11-19 21:51:07 +0800

    771

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0771.java
@@ -0,0 +1,31 @@
+package com.leetcode;
+
+/**
+ * @author ：wyq
+ * @date ：Created in 2020/11/19
+ * @description：
+ */
+public class Solution0771 {
+
+    public static void main(String[] args) {
+        Solution0771 obj = new Solution0771();
+        String j = "aA", s = "aAASSS";
+        System.out.println(obj.numJewelsInStones(j, s));
+    }
+
+
+    public int numJewelsInStones(String J, String S) {
+        int count = 0;
+        for (int i = 0; i < J.length(); i++) {
+            for (int j = 0; j < S.length(); j++) {
+                if (J.charAt(i) == S.charAt(j)) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+
+}
---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1342.java
@@ -0,0 +1,32 @@
+package com.leetcode;
+
+/**
+ * @author ：wyq
+ * @date ：Created in 2020/11/19
+ * @description：
+ */
+public class Solution1342 {
+
+    public static void main(String[] args) {
+        Solution1342 obj = new Solution1342();
+        int a = 14;
+        System.out.println(obj.numberOfSteps(a));
+    }
+
+
+    public int numberOfSteps(int num) {
+        int count = 0;
+        while (num > 0) {
+            if (num % 2 == 0) {
+                num = num / 2;
+                count++;
+            } else {
+                num = num - 1;
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+}
