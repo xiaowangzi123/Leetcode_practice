79fbf5841b86b7b60e66a574e1add04ba95a10fb
xiaowangzi123 <1103694415@qq.com>
2020-11-24 22:15:45 +0800

    1122 .solution

---
+++ all_leetcode_solution/src/main/java/com/leetcode/array/Solution1122.java
@@ -0,0 +1,24 @@
+package com.leetcode.array;
+
+/**
+ * @author ：wyq
+ * @date ：Created in 2020/11/19
+ * @description：
+ */
+public class Solution1122 {
+
+    public static void main(String[] args) {
+        Solution1122 obj = new Solution1122();
+        int[] arr1 = {2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19}, arr2 = {2, 1, 4, 3, 9, 6};
+        System.out.println(obj.relativeSortArray(arr1, arr2));
+    }
+
+
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int[] res = new int[arr1.length + arr2.length];
+
+        return res;
+    }
+
+
+}
