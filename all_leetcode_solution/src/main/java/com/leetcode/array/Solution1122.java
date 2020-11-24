2c72104a2ae3846358c2c373b4041b1992cfb9bd
xiaowangzi123 <1103694415@qq.com>
2020-11-24 22:34:45 +0800

    1122 .solution

--- all_leetcode_solution/src/main/java/com/leetcode/array/Solution1122.java
+++ all_leetcode_solution/src/main/java/com/leetcode/array/Solution1122.java
@@ -1,5 +1,7 @@
 package com.leetcode.array;
 
+import java.util.Arrays;
+
 /**
  * @author ：wyq
  * @date ：Created in 2020/11/19
@@ -10,14 +12,35 @@ public class Solution1122 {
     public static void main(String[] args) {
         Solution1122 obj = new Solution1122();
         int[] arr1 = {2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19}, arr2 = {2, 1, 4, 3, 9, 6};
-        System.out.println(obj.relativeSortArray(arr1, arr2));
+        System.out.println(Arrays.toString(obj.relativeSortArray(arr1, arr2)));
     }
 
 
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        int[] res = new int[arr1.length + arr2.length];
 
-        return res;
+        int[] count = new int[1001];
+        // 遍历arr1中的元素并计数,统计个元素出现个数
+        for (int num : arr1) {
+            count[num]++;
+        }
+
+        int index = 0;
+        // 处理arr2中的元素
+        for (int num : arr2) {
+            while (count[num] > 0) {
+                arr1[index++] = num;
+                count[num]--;
+            }
+        }
+
+        // 处理剩余不是arr2中的元素
+        for (int i=0; i<count.length; i++) {
+            while (count[i] > 0) {
+                arr1[index++] = i;
+                count[i]--;
+            }
+        }
+        return arr1;
     }
 
 
