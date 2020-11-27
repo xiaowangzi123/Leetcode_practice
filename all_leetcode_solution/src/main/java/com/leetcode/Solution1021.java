31c6ca973c8574156a9b4ebab8aa273e0533d157
xiaowangzi123 <1103694415@qq.com>
2020-11-27 20:08:54 +0800

    1021.solution

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1021.java
@@ -0,0 +1,23 @@
+package com.leetcode;
+
+import com.leetcode.bean.ListNode;
+
+/**
+ * @author ：wyq
+ * @date ：Created in 2020/11/20
+ * @description：
+ */
+public class Solution1021 {
+    public static void main(String[] args) {
+        String st = "(()())(())";
+
+        Solution1021 obj = new Solution1021();
+        System.out.println(obj.removeOuterParentheses(st));
+
+    }
+
+    public String removeOuterParentheses(String str) {
+
+        return "";
+    }
+}
