29d1f3e8a135e750a497ecddf1e89b740db8b1d0
xiaowangzi123 <1103694415@qq.com>
2021-06-28 22:30:37 +0800

    1.solution

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0001.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0001.java
@@ -10,15 +10,21 @@ import java.util.Arrays;
 public class Solution0001 {
     public static void main(String[] args) {
         Solution0001 obj = new Solution0001();
-        int[] nums = {2, 7, 11, 15};
+        int[] nums = {2, 11, 7, 15};
         int target = 9;
-        System.out.println(Arrays.toString(obj.twoSum(nums,target)));
+        System.out.println(Arrays.toString(obj.twoSum(nums, target)));
     }
 
     public int[] twoSum(int[] nums, int target) {
-        int[] res = null;
-
-
+        int[] res = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0+1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    res[0] = i;
+                    res[1] = j;
+                }
+            }
+        }
         return res;
     }
 
