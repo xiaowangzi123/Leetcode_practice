9a8e19ddc601fc969e8c7307b12bb65ed5d1735a
xiaowangzi123 <1103694415@qq.com>
2021-06-28 22:23:23 +0800

    1985.solution

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0001.java
@@ -0,0 +1,26 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author ：wyq
+ * @date ：Created in 2020/11/21
+ * @description：
+ */
+public class Solution0001 {
+    public static void main(String[] args) {
+        Solution0001 obj = new Solution0001();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        System.out.println(Arrays.toString(obj.twoSum(nums,target)));
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = null;
+
+
+        return res;
+    }
+
+
+}
