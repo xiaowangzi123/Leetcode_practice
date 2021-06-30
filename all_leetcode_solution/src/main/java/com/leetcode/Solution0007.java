7dc8bf9ab7c5b50bbc800ce3c2a7242a429d64b2
xiaowangzi123 <1103694415@qq.com>
2021-06-30 20:55:13 +0800

    7.solution

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0007.java
@@ -0,0 +1,22 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author ：wyq
+ * @date ：Created in 2020/11/21
+ * @description：
+ */
+public class Solution0007 {
+    public static void main(String[] args) {
+        Solution0007 obj = new Solution0007();
+        int target = 9;
+        System.out.println(obj.reverse(target));
+    }
+
+    public int reverse(int x) {
+
+    }
+
+
+}
