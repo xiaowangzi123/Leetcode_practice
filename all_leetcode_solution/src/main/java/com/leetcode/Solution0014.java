279edcd868b225171e2ec50bf2560abf0d47bda8
xiaowangzi123 <1103694415@qq.com>
2021-07-01 22:33:22 +0800

    7.solution

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0014.java
@@ -0,0 +1,24 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author ：wyq
+ * @date ：Created in 2020/11/21
+ * @description：
+ */
+public class Solution0014 {
+    public static void main(String[] args) {
+        Solution0014 obj = new Solution0014();
+        String[] str = {"flower", "flow", "flight"};
+        System.out.println(obj.longestCommonPrefix(str));
+    }
+
+    public String longestCommonPrefix(String[] strs) {
+        String res = "";
+
+        return res;
+    }
+
+
+}
