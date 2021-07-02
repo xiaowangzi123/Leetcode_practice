c090eb26fd1da96620faee0e6aa172086db9ca87
xiaowangzi123 <1103694415@qq.com>
2021-07-02 22:58:24 +0800

    14.solution

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0014.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0014.java
@@ -10,26 +10,29 @@ import java.util.Arrays;
 public class Solution0014 {
     public static void main(String[] args) {
         Solution0014 obj = new Solution0014();
-        String[] str = {"flower", "flow", "flight"};
+        /*String[] str = {"flower", "flow", "flight"};
         System.out.println(obj.longestCommonPrefix(str));
 
         String[] str2 = {"dog", "abc", "high"};
         System.out.println(obj.longestCommonPrefix(str2));
-
-        String[] str3 = {"ab", "a"};
+*/
+        String[] str3 = {"abc", "ab"};
         System.out.println(obj.longestCommonPrefix(str3));
         System.out.println("abc".charAt(1));
     }
 
     public String longestCommonPrefix(String[] strs) {
         //字符数组长度为零，直接返回
+        /**
+         *
+         */
         if (strs == null || strs.length == 0) {
             return "";
         }
         String res = strs[0];
         for (int i = 0; i < res.length(); i++) {
             for (int j = 1; j < strs.length; j++) {
-                if (res.charAt(i) != strs[j].charAt(i)) {
+                if (i == strs[j].length() || res.charAt(i) != strs[j].charAt(i)) {
                     return res.substring(0, i);
                 }
             }
