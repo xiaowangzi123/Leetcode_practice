bd9860ffa3af8126c02774d86fcc5cfcda207283
xiaowangzi123 <1103694415@qq.com>
2021-07-03 22:21:45 +0800

    20描述

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0020.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0020.java
@@ -3,6 +3,7 @@ package com.leetcode;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Stack;
 
 /**
  * @author ：wyq
@@ -15,16 +16,30 @@ public class Solution0020 {
         String s1 = "()[]{}";
         System.out.println(obj.isValid(s1));
 
-        Map<String,Character> map = new HashMap<>();
+        Map<String, Character> map = new HashMap<>();
         map.put("s", 's');
         System.out.println(map);
     }
 
     public boolean isValid(String s) {
-        if (s.length() % 2 == 1){
-            return false;
+        if (s.isEmpty()) {
+            return true;
         }
 
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (stack.isEmpty() || c != stack.pop())
+                return false;
+        }
+        if (stack.isEmpty()) {
+            return true;
+        }
         return false;
     }
 
