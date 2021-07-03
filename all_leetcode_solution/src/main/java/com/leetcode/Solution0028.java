0a6b42c0c9270f2e0b72e941a741b5deb11758bb
xiaowangzi123 <1103694415@qq.com>
2021-07-03 22:50:21 +0800

    28描述

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0028.java
@@ -0,0 +1,50 @@
+package com.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * @author ：wyq
+ * @date ：Created in 2020/11/21
+ * @description：
+ */
+public class Solution0028 {
+    public static void main(String[] args) {
+        Solution0028 obj = new Solution0028();
+        String haystack = "hello", needle = "ll";
+        System.out.println(obj.strStr(haystack, needle));
+        System.out.println(obj.strStr2(haystack, needle));
+    }
+
+    public int strStr(String haystack, String needle) {
+        int m = haystack.length(), n = needle.length();
+        for (int i = 0; i + n < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    return -1;
+                }
+            }
+            return i;
+        }
+        return -1;
+    }
+
+    public int strStr2(String haystack, String needle) {
+        int m = haystack.length(), n = needle.length();
+        char[] mm = haystack.toCharArray(), nn = needle.toCharArray();
+        for (int i = 0; i <= n - m; i++) {
+            int a = i, b = 0;
+            while (b<m && mm[a]==nn[b]){
+                a++;
+                b++;
+            }
+            if (b==m){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+}
