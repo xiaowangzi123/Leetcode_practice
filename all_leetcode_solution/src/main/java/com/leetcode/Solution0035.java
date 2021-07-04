2cf895c9b27c8cf21b3af5260fafa94606b24214
xiaowangzi123 <1103694415@qq.com>
2021-07-04 22:26:39 +0800

    35新方法

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0035.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0035.java
@@ -18,20 +18,43 @@ public class Solution0035 {
         int target3 = 7;
         System.out.println(obj.searchInsert(arr, target3));
 
+        System.out.println("-------------");
+        System.out.println(obj.searchInsert2(arr, target));
+        System.out.println(obj.searchInsert2(arr, target2));
+        System.out.println(obj.searchInsert2(arr, target3));
+
     }
 
     public int searchInsert(int[] nums, int target) {
+        //先找相等的
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == target) {
                 return i;
             }
         }
+        //从后往前遍历
         for (int i = nums.length - 1; i >= 0 && i < nums.length; i--) {
             if (nums[i] < target) {
                 return i + 1;
             }
         }
         return 0;
+    }
+
+
+    public int searchInsert2(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            }
+        }
+        return left;
     }
 
 
