f7a275cb548016662960c0dac6f2a23f6e636aa2
xiaowangzi123 <1103694415@qq.com>
2021-07-05 22:15:16 +0800

    错误答案，结果求的是最大的字符串

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0058.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0058.java
@@ -1,10 +1,27 @@
 package com.leetcode;
 
+import java.util.Arrays;
+
 /**
  * @author ：wyq
  * @date ：Created in 2021/7/5
  * @description：
  */
 public class Solution0058 {
+    public static void main(String[] args) {
+        Solution0058 obj = new Solution0058();
+        String s = "Hello World";
+        String s2 = " ";
+        System.out.println(obj.lengthOfLastWord(s));
+        System.out.println(obj.lengthOfLastWord(s2));
+    }
 
+    public int lengthOfLastWord(String s) {
+        String[] temp = s.split(" ");
+        int i = 0;
+        for (String x : temp) {
+            i = Math.max(i, x.length());
+        }
+        return i;
+    }
 }
