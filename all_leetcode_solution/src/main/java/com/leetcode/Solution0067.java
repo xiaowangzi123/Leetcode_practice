121ece65007afc983061da8049e726c44f91d240
xiaowangzi123 <1103694415@qq.com>
2021-07-06 14:33:51 +0800

    67提交

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0067.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0067.java
@@ -16,6 +16,19 @@ public class Solution0067 {
         return Integer.toBinaryString(Integer.parseInt(a, 2) + Integer.parseInt(b, 2));
     }
 
+    public String addBinary2(String a, String b) {
+        StringBuilder ans = new StringBuilder();
+        int ca = 0;
+        for (int i = a.length() - 1, j = b.length() - 1; i >= 0 || j >= 0; i--, j--) {
+            int sum = ca;
+            sum += i >= 0 ? a.charAt(i) - '0' : 0;
+            sum += j >= 0 ? b.charAt(j) - '0' : 0;
+            ans.append(sum % 2);
+            ca = sum / 2;
+        }
+        ans.append(ca == 1 ? ca : "");
+        return ans.reverse().toString();
+    }
 
 
 }
