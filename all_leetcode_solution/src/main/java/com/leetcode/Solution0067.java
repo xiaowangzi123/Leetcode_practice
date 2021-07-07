63a5538e7fb83a5f652873c353a607caae2afb74
xiaowangzi123 <1103694415@qq.com>
2021-07-07 21:59:01 +0800

    67描述

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0067.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0067.java
@@ -23,18 +23,28 @@ public class Solution0067 {
         return Integer.toBinaryString(Integer.parseInt(a, 2) + Integer.parseInt(b, 2));
     }
 
+
+
+    /**
+     * 整体思路是将两个字符串较短的用 00补齐，使得两个字符串长度一致，然后从末尾进行遍历计算，得到最终结果。
+     * 由于字符串操作原因，不确定最后的结果是否会多出一位进位，所以会有 2 种处理方式：
+     *
+     * 第一种，在进行计算时直接拼接字符串，会得到一个反向字符，需要最后再进行翻转
+     * 第二种，按照位置给结果字符赋值，最后如果有进位，则在前方进行字符串拼接添加进位
+     *
+     */
     public String addBinary2(String a, String b) {
-        StringBuilder ans = new StringBuilder();
+        StringBuilder res = new StringBuilder();
         int ca = 0;
         for (int i = a.length() - 1, j = b.length() - 1; i >= 0 || j >= 0; i--, j--) {
             int sum = ca;
             sum += i >= 0 ? a.charAt(i) - '0' : 0;
             sum += j >= 0 ? b.charAt(j) - '0' : 0;
-            ans.append(sum % 2);
+            res.append(sum % 2);
             ca = sum / 2;
         }
-        ans.append(ca == 1 ? ca : "");
-        return ans.reverse().toString();
+        res.append(ca == 1 ? ca : "");
+        return res.reverse().toString();
     }
 
 
