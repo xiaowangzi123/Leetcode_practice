4a58c97eed66677c8e8cd4ce6df2a81348d76b3e
xiaowangzi123 <1103694415@qq.com>
2021-07-09 22:03:28 +0800

    122解答

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0122.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0122.java
@@ -1,9 +1,24 @@
 package com.leetcode;
 
+import com.leetcode.array.Solution1122;
+
 /**
  * @author ：wyq
  * @date ：Created in 2021/7/9
  * @description：
  */
 public class Solution0122 {
+    public static void main(String[] args) {
+        Solution0122 obj = new Solution0122();
+        int[] prices = {7, 6, 4, 3, 1};
+        System.out.println(obj.maxProfit(prices));
+    }
+
+    public int maxProfit(int[] prices) {
+        int res = 0;
+        for (int i = 1; i < prices.length; ++i) {
+            res += Math.max(0, prices[i] - prices[i - 1]);
+        }
+        return res;
+    }
 }
