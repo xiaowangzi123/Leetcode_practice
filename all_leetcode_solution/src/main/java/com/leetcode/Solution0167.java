747272e11f489beba1b832ec4067ff4e4cd19278
xiaowangzi123 <1103694415@qq.com>
2021-07-11 21:18:11 +0800

    描述

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0167.java
@@ -0,0 +1,14 @@
+package com.leetcode;
+
+/**
+ * @author ：wyq
+ * @date ：Created in 2021/7/11
+ * @description：
+ */
+public class Solution0167 {
+    public static void main(String[] args) {
+        Solution0167 obj = new Solution0167();
+
+    }
+
+}
