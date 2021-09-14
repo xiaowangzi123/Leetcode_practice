360d1c3b94e713bbd4116e7982c90786c01fb91f
xiaowangzi123 <1103694415@qq.com>
2021-09-14 19:05:35 +0800

    test

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0136.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0136.java
@@ -1,5 +1,8 @@
 package com.leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author ：wyq
  * @date ：Created in 2021/9/14
@@ -7,6 +10,16 @@ package com.leetcode;
  */
 public class Solution0136 {
     public static void main(String[] args) {
-        
+        Solution0136 obj = new Solution0136();
+
+        int[] arr = {1, 2, 1, 4, 2};
+        System.out.println(obj.singleNumber(arr));
+    }
+
+    public int singleNumber(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i : nums){
+        }
+        return 0;
     }
 }
