323d34dc34d3631171568a1c90a10094d412aac1
xiaowangzi123 <1103694415@qq.com>
2021-09-14 14:44:16 +0800

    217解决方式

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0217.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0217.java
@@ -1,5 +1,6 @@
 package com.leetcode;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,7 +16,11 @@ public class Solution0217 {
         Solution0217 obj = new Solution0217();
         System.out.println(obj.containsDuplicate(arr));
         System.out.println(obj.containsDuplicate(arr2));
-        
+
+        System.out.println("-----------------------------");
+        System.out.println(obj.containsDuplicate2(arr));
+        System.out.println(obj.containsDuplicate2(arr2));
+
         System.out.println("-----------------------------");
         Set<Integer> set2 = new HashSet<>();
         System.out.println(set2.add(1));
@@ -26,10 +31,20 @@ public class Solution0217 {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> set = new HashSet<>();
         for (int i : nums) {
-            if (set.add(i)){
-                return false;
+            if (!set.add(i)){
+                return true;
             }
         }
-        return true;
+        return false;
+    }
+
+    public boolean containsDuplicate2(int[] nums) {
+        Arrays.sort(nums);
+        for (int i=0; i<nums.length-1; i++){
+            if (nums[i]==nums[i+1]){
+                return true;
+            }
+        }
+        return false;
     }
 }
