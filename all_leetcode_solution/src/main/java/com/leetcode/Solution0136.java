64d0540050528591bbea01deb22d1f54e1375bfb
xiaowangzi123 <1103694415@qq.com>
2021-09-14 18:58:39 +0800

    test

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0136.java
@@ -0,0 +1,12 @@
+package com.leetcode;
+
+/**
+ * @author ：wyq
+ * @date ：Created in 2021/9/14
+ * @description：
+ */
+public class Solution0136 {
+    public static void main(String[] args) {
+        
+    }
+}
