7b1faec9c433cabc00a651c9159027822c0b9d31
xiaowangzi123 <1103694415@qq.com>
2021-09-15 21:20:05 +0800

    56解答

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0056.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0056.java
@@ -15,10 +15,17 @@ public class Solution0056 {
         Solution0056 obj = new Solution0056();
         int[][] arr = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
         int[][] res = obj.merge(arr);
+        System.out.println(arr.length);
         System.out.println(Arrays.deepToString(res));
-        for (int[] i:res){
+        for (int[] i : res) {
             System.out.println(Arrays.toString(i));
         }
+
+        System.out.println("----------------------------");
+        int[][] arr2 = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+        int[][] res2 = obj.merge2(arr2);
+        System.out.println(Arrays.deepToString(res2));
+
     }
 
     public int[][] merge(int[][] intervals) {
@@ -28,16 +35,38 @@ public class Solution0056 {
 
         Arrays.sort(intervals, (o1, o2) -> o1[0] - o2[0]);
 
-        List<int[]> merged = new ArrayList<int[]>();
+        List<int[]> res = new ArrayList<int[]>();
         for (int i = 0; i < intervals.length; ++i) {
             int L = intervals[i][0], R = intervals[i][1];
-            if (merged.size() == 0 || merged.get(merged.size() - 1)[1] < L) {
-                merged.add(new int[]{L, R});
+            if (res.size() == 0 || res.get(res.size() - 1)[1] < L) {
+                res.add(new int[]{L, R});
             } else {
-                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], R);
+                res.get(res.size() - 1)[1] = Math.max(res.get(res.size() - 1)[1], R);
             }
         }
-        return merged.toArray(new int[merged.size()][]);
+        return res.toArray(new int[res.size()][]);
 
+    }
+
+    public int[][] merge2(int[][] intervals) {
+        List<int[]> res = new ArrayList<>();
+        if (intervals.length == 0 || intervals == null) return res.toArray(new int[0][]);
+        // 对起点终点进行排序
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        int i = 0;
+        while (i < intervals.length) {
+            int left = intervals[i][0];
+            int right = intervals[i][1];
+            // 如果有重叠，循环判断哪个起点满足条件
+            while (i < intervals.length - 1 && intervals[i + 1][0] <= right) {
+                i++;
+                right = Math.max(right, intervals[i][1]);
+            }
+            // 将现在的区间放进res里面
+            res.add(new int[]{left, right});
+            // 接着判断下一个区间
+            i++;
+        }
+        return res.toArray(new int[0][]);
     }
 }
