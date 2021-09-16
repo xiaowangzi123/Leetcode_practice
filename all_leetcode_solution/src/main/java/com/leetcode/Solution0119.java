d9c5bc5d0a597b8bcb07d3ef1719f5d50d7eadb8
xiaowangzi123 <1103694415@qq.com>
2021-09-16 11:42:17 +0800

    119题目解答

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0119.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0119.java
@@ -12,22 +12,40 @@ public class Solution0119 {
     public static void main(String[] args) {
         Solution0119 obj = new Solution0119();
         System.out.println(obj.getRow(3));
+        System.out.println(obj.getRow2(3));
     }
 
     public List<Integer> getRow(int rowIndex) {
         List<List<Integer>> lists = new ArrayList<>();
-        for (int i=0; i<=rowIndex; i++){
+        for (int i = 0; i <= rowIndex; i++) {
             List<Integer> row = new ArrayList<>();
-            for (int j=0; j<=i; j++){
-                if (j==0 || j==i){
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
                     row.add(1);
-                }else {
-                    row.add(lists.get(i-1).get(j-1)+lists.get(i-1).get(j));
+                } else {
+                    row.add(lists.get(i - 1).get(j - 1) + lists.get(i - 1).get(j));
                 }
             }
             lists.add(row);
         }
         return lists.get(rowIndex);
     }
-    
+
+
+    public List<Integer> getRow2(int rowIndex) {
+        List<Integer> list = new ArrayList<>(rowIndex);
+        for (int i = 0; i <= rowIndex; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(list.get(j-1)+list.get(j));
+                }
+            }
+            list = row;
+        }
+
+        return list;
+    }
 }
