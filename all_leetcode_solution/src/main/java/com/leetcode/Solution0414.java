00faa01e4048dfaecdd6691e9bbde9d7dd1c1434
xiaowangzi123 <1103694415@qq.com>
2021-09-16 19:19:32 +0800

    414

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0414.java
@@ -0,0 +1,12 @@
+package com.leetcode;
+
+/**
+ * @author ：wyq
+ * @date ：Created in 2021/9/16
+ * @description：
+ */
+public class Solution0414 {
+    public static void main(String[] args) {
+        Solution0414 obj = new Solution0414();
+    }
+}
