8d676aadc3cd03f373cbf4444839e4bb51bd9c7d
xiaowangzi123 <1103694415@qq.com>
2021-09-16 15:27:41 +0800

    48题目解答

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0048.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0048.java
@@ -21,14 +21,20 @@ public class Solution0048 {
         int[][] temp = new int[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                temp[i][j] = matrix[n-j-1][i];
+                temp[i][j] = matrix[n - j - 1][i];
             }
         }
 
-        for (int i = 0; i < n; i++) {
+        //方法一
+        /*for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 matrix[i][j] = temp[i][j];
             }
+        }*/
+
+        //方法二
+        for (int i = 0; i < n; i++) {
+            matrix[i] = temp[i].clone();
         }
 
     }
