b9703890da4985b644f88324fcf5411b6f597ae4
xiaowangzi123 <1103694415@qq.com>
2021-09-16 22:24:32 +0800

    204题目描述

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0204.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0204.java
@@ -1,6 +1,7 @@
 package com.leetcode;
 
 import java.io.Console;
+import java.util.Arrays;
 
 /**
  * @author ：wyq
@@ -15,11 +16,17 @@ public class Solution0204 {
         System.out.println(obj.countPrimes(2));
         System.out.println(obj.countPrimes(3));
         System.out.println(obj.countPrimes(4));
-        
+
         long t1 = System.currentTimeMillis();
-        System.out.println(obj.countPrimes(5000000));
+        System.out.println("500000:yi=="+obj.countPrimes(5000000));
         long t2 = System.currentTimeMillis();
-        System.out.println((t2-t1)/1000);
+        System.out.println("yi："+(t2 - t1) / 1000);
+
+
+        long t3 = System.currentTimeMillis();
+        System.out.println("500000:er=="+obj.countPrimes2(5000000));
+        long t4 = System.currentTimeMillis();
+        System.out.println("二："+(t4 - t3) / 1000);
     }
 
     //有超时的可能
@@ -32,11 +39,31 @@ public class Solution0204 {
     }
 
     public boolean isPrimes(int n) {
-        for (int i = 2; i * i <= n; i++) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) {
                 return false;
             }
         }
         return true;
+    }
+
+    public int countPrimes2(int n) {
+        boolean[] isPrimes = new boolean[n];
+        Arrays.fill(isPrimes, true);
+        for (int i = 2; i * i <= n; i++) {
+            if (isPrimes[i]) {
+                for (int j = i * i; j < n; j+=i) {
+                    isPrimes[j] = false;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrimes[i]) {
+                count++;
+            }
+        }
+        return count;
     }
 }
