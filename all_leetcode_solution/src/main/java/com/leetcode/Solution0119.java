b03faa852ca699ebb5893125c70d5c22753d7f2e
xiaowangzi123 <1103694415@qq.com>
2021-09-16 14:38:15 +0800

    119题目注释

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0119.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0119.java
@@ -18,7 +18,6 @@ public class Solution0119 {
         System.out.println(obj.getRow(3));
         System.out.println(obj.getRow2(3));
         System.out.println(obj.getRow3(3));
-        System.out.println(obj.getRow4(3));
     }
 
     public List<Integer> getRow(int rowIndex) {
@@ -54,24 +53,23 @@ public class Solution0119 {
     }
 
     public List<Integer> getRow3(int rowIndex) {
-        int[] res = new int[rowIndex+1];
+        int[] res = new int[rowIndex + 1];
         for (int i = 0; i <= rowIndex; i++) {
             res[i] = 1;
             for (int j = i - 1; j >= 1; j--) {
-                res[j] = res[j]+res[j-1];
+                res[j] = res[j] + res[j - 1];
             }
         }
+        //int[]转list，需要apache的commons-lang3包
         return Arrays.asList(ArrayUtils.toObject(res));
-    }
+        //java8以上版本可用lambda表达式，装箱操作
+//        return Arrays.stream(res).boxed().collect(Collectors.toList());
 
-    public List<Integer> getRow4(int rowIndex) {
-        int[] res = new int[rowIndex+1];
-        for (int i = 0; i <= rowIndex; i++) {
-            res[i] = 1;
-            for (int j = i - 1; j >= 1; j--) {
-                res[j] = res[j]+res[j-1];
-            }
-        }
-        return Arrays.stream(res).boxed().collect(Collectors.toList());
+        /*list转int[]*/
+        //方法一：
+//        Integer[] intArr =  list.toArray(new Integer[list.size()]);
+        //方法二：java8及以上版本
+//        int[] intArr1 =  list.stream().mapToInt(Integer::valueOf).toArray();
+
     }
 }
