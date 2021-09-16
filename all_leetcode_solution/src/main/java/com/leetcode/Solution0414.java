5cb2f1eb61eb35b400e3b41542b36d1d467b76af
xiaowangzi123 <1103694415@qq.com>
2021-09-16 19:35:54 +0800

    414错误解答

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0414.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0414.java
@@ -8,5 +8,24 @@ package com.leetcode;
 public class Solution0414 {
     public static void main(String[] args) {
         Solution0414 obj = new Solution0414();
+        int[] arr = {1, 2, 3, 1};
+        System.out.println(obj.thirdMax(arr));
+        System.out.println(Integer.MIN_VALUE);
+    }
+
+    public int thirdMax(int[] nums) {
+        int a = Integer.MIN_VALUE, b = Integer.MIN_VALUE, c = Integer.MIN_VALUE;
+        for (int i : nums) {
+            if (i > a) {
+                a = i;
+            }
+            if (i > b && i < a) {
+                b = i;
+            }
+            if (i > c && i < b) {
+                c = i;
+            }
+        }
+        return c == Integer.MIN_VALUE ? a : c;
     }
 }
