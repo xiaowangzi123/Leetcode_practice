f4fd7f2e2ce82455fba62d0e5e435a9233594f28
xiaowangzi123 <1103694415@qq.com>
2021-09-17 23:58:31 +0800

    453题目描述

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0453.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0453.java
@@ -12,14 +12,29 @@ public class Solution0453 {
         Solution0453 obj = new Solution0453();
         int[] arr = {1, 2, 3};
         System.out.println(obj.minMoves(arr));
+        System.out.println(obj.minMoves2(arr));
     }
 
+    /**
+     * n-1个数同时加一，就好比每次有一个数自身减一，所以反向考虑做减法，
+     * 最小操作就是数组最后的数只能是最小值。
+     * 每个元素减去最小值求其和就是答案
+     */
     public int minMoves(int[] nums) {
         Arrays.sort(nums);
         int sum = 0;
         for (int i : nums) {
             sum += i;
         }
-        return sum-nums[0]*nums.length;
+        return sum - nums[0] * nums.length;
+    }
+
+    public int minMoves2(int[] nums) {
+        Arrays.sort(nums);
+        int count = 0;
+        for (int i = 1; i < nums.length; i++) {
+            count += nums[i] - nums[0];
+        }
+        return count;
     }
 }
