abe2171ab5a380cb16ab68842fce2aa555a76322
xiaowangzi123 <1103694415@qq.com>
2021-09-17 10:04:16 +0800

    240题目描述

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0240.java
@@ -0,0 +1,19 @@
+package com.leetcode;
+
+/**
+ * @author ：wyq
+ * @date ：Created in 2021/9/17
+ * @description：
+ */
+public class Solution0240 {
+    public static void main(String[] args) {
+        Solution0240 ojb = new Solution0240();
+
+    }
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+    }
+
+
+}
