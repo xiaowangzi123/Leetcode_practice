d800479c8885765f5c889cb31bf16199de2a63e5
xiaowangzi123 <1103694415@qq.com>
2021-09-19 07:41:46 +0800

    392解答

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0392.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0392.java
@@ -10,9 +10,32 @@ public class Solution0392 {
         Solution0392 obj = new Solution0392();
         String s = "abc", t = "ahbgdc";
         System.out.println(obj.isSubsequence(s, t));
+        System.out.println(obj.isSubsequence2(s, t));
     }
 
     public boolean isSubsequence(String s, String t) {
-
+        if (s.length() > t.length()) {
+            return false;
+        }
+        int m = 0, n = 0;
+        while (m < s.length() && n < t.length()) {
+            if (s.charAt(m) == t.charAt(n)) {
+                m++;
+            }
+            n++;
+        }
+        return m == s.length();
     }
+
+    public boolean isSubsequence2(String s, String t) {
+        int index = -1;
+        for (int i = 0; i < s.length(); i++) {
+            index = t.indexOf(s.charAt(i), index + 1);
+            if (index == -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
