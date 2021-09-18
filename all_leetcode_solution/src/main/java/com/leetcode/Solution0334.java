1a81d8b637ef2d051e9bbfc33f6ea0bcf9524827
xiaowangzi123 <1103694415@qq.com>
2021-09-18 13:29:18 +0800

    注释

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0334.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0334.java
@@ -12,13 +12,20 @@ public class Solution0334 {
         int[] arr2 = {5, 4, 3, 2, 1};
         int[] arr3 = {2, 1, 5, 0, 4, 6};
 
-        System.out.println(obj.increasingTriplet(arr2));
+//        System.out.println(obj.increasingTriplet(arr2));
         System.out.println(obj.increasingTriplet(arr3));
         System.out.println((int) Math.pow(2, 31) == Integer.MAX_VALUE);
         System.out.println(Math.pow(2, 31));
         System.out.println(Integer.MAX_VALUE);
     }
 
+    /**
+     * {2,1,5,0,4,6}
+     * 1.先找出符合条件的不完整序列，{2,5}最小值2，中间值5
+     * 2.后面有值比mid值大，存在有序数列，返回true
+     *   有值比min值小，替换min值
+     *   有值比min大，比mid小，替换mid值
+     */
     public boolean increasingTriplet(int[] nums) {
         int min = Integer.MAX_VALUE, mid = Integer.MAX_VALUE;
         for (int i = 0; i < nums.length; i++) {
