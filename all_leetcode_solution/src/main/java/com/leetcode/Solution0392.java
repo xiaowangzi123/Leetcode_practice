2f3393bbc62606c23e4b5352cc871bdde4087c37
xiaowangzi123 <1103694415@qq.com>
2021-09-19 06:56:46 +0800

    IDE

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0392.java
@@ -0,0 +1,18 @@
+package com.leetcode;
+
+/**
+ * @author ：wyq
+ * @date ：Created in 2021/9/19
+ * @description：
+ */
+public class Solution0392 {
+    public static void main(String[] args) {
+        Solution0392 obj = new Solution0392();
+        String s = "abc", t = "ahbgdc";
+        System.out.println(obj.isSubsequence(s, t));
+    }
+
+    public boolean isSubsequence(String s, String t) {
+
+    }
+}
