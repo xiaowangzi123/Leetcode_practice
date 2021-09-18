7cf050c3f8337e8d8419b476b3155c2e11fabf8a
xiaowangzi123 <1103694415@qq.com>
2021-09-18 21:27:37 +0800

    超时

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0125.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0125.java
@@ -11,18 +11,25 @@ public class Solution0125 {
         String s = "A man, a plan, a canal: Panama";
         System.out.println(obj.isPalindrome(s));
 
+
         StringBuffer s2 = new StringBuffer("man");
         System.out.println(s2.hashCode());
         System.out.println(s2.reverse().hashCode());
         //Stringbuffer没有重写equals方法，比较是地址，两者地址一样
         System.out.println(s2.equals(s2.reverse()));
+
+        String s3 = "0P";
+        System.out.println("0P:" + obj.isPalindrome(s3));
+
+        System.out.println("0:" + Character.isLetter('0'));
+        System.out.println(Character.isLetter(1));
     }
 
     public boolean isPalindrome(String s) {
         StringBuffer s1 = new StringBuffer();
         for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (Character.isLetter(ch)) {
+            char ch = s.toLowerCase().charAt(i);
+            if (Character.isLetterOrDigit(ch)) {
                 s1.append(Character.toLowerCase(ch));
             }
         }
