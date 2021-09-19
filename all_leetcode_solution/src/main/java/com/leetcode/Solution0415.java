c810a18381394321ed316e9b9ee8f1e0d845864f
xiaowangzi123 <1103694415@qq.com>
2021-09-19 14:17:13 +0800

    IDEA提交失败

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0415.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0415.java
@@ -13,15 +13,26 @@ public class Solution0415 {
 
         String num3 = "435", num4 = "88";
         System.out.println(obj.addStrings2(num3, num4));
+
+        System.out.println(Integer.MAX_VALUE);// 2147483647
+//        System.out.println(Integer.parseInt("6913259244"));
+
+
+        String num5 = "6913259244", num6 = "88";
+        System.out.println(obj.addStrings2(num5, num6));
     }
 
     public String addStrings(String num1, String num2) {
+        //java.lang.NumberFormatException: For input string: "6913259244"
         int m = Integer.parseInt(num1), n = Integer.parseInt(num2);
         return String.valueOf(m + n);
     }
 
+    /**
+     *
+     */
     public String addStrings2(String num1, String num2) {
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         int m = num1.length() - 1, n = num2.length() - 1, k = 0;
         while (m >= 0 || n >= 0) {
             int i = m >= 0 ? num1.charAt(m) - '0' : 0;
@@ -35,5 +46,14 @@ public class Solution0415 {
             sb.append(1);
         }
         return sb.reverse().toString();
+    }
+
+    /**
+     * "3876620623801494171"
+     * "6529364523802684779"
+     */
+    public String addStrings3(String num1, String num2) {
+        long m = Long.parseLong(num1), n = Long.parseLong(num2);
+        return String.valueOf(m + n);
     }
 }
