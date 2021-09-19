71621f70883b7ccc5507d39a41c0a5f466f26e88
xiaowangzi123 <1103694415@qq.com>
2021-09-19 14:56:28 +0800

    解答

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0409.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0409.java
@@ -13,8 +13,9 @@ import java.util.Set;
 public class Solution0409 {
     public static void main(String[] args) {
         Solution0409 obj = new Solution0409();
-        String s = "ajelkjaklje";
+        String s = "ajelkjakljez";
         System.out.println(obj.longestPalindrome(s));
+        System.out.println(obj.longestPalindrome2(s));
 
         Map<Character, Integer> map = new HashMap<>();
         map.put('a', 23);
@@ -36,11 +37,28 @@ public class Solution0409 {
         int count = 0;
         Set<Map.Entry<Character, Integer>> entrySet = map.entrySet();
         for (Map.Entry<Character, Integer> entry : entrySet) {
-            count += (entry.getValue() / 2)*2;
+            count += (entry.getValue() / 2) * 2;
         }
         if (count != s.length()) {
             count++;
         }
         return count;
+    }
+
+    //大小写字母中间有6个字符
+    public int longestPalindrome2(String s) {
+        int[] cnt = new int[58];
+        for (char c : s.toCharArray()) {
+            cnt[c - 'A'] += 1;
+        }
+        int res = 0;
+        for (int i : cnt) {
+            if (i % 2 == 0) {
+                res += i;
+            } else {
+                res += (i - 1);
+            }
+        }
+        return res < s.length() ? res + 1 : res;
     }
 }
