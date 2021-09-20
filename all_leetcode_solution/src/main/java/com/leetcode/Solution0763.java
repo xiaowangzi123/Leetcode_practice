ed420c55109be367d9a1c247096b6980f7dc6643
xiaowangzi123 <1103694415@qq.com>
2021-09-20 09:48:58 +0800

    解答

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0763.java
@@ -0,0 +1,37 @@
+package com.leetcode;
+
+import org.hamcrest.text.CharSequenceLength;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author ：wyq
+ * @date ：Created in 2021/9/20
+ * @description：
+ */
+public class Solution0763 {
+    public static void main(String[] args) {
+        Solution0763 ojb = new Solution0763();
+        String s = "ababcbacadefegdehijhklij";
+        System.out.println(ojb.partitionLabels(s));
+    }
+
+    public List<Integer> partitionLabels(String s) {
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a'] = i;
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            end = Math.max(end, count[s.charAt(i) - 'a']);
+            if (i == end) {
+                ans.add(end - start + 1);
+                start = end + 1;
+            }
+        }
+        return ans;
+    }
+}
