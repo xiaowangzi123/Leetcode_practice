6724bcce61661e442376860287b1a3421a943559
xiaowangzi123 <1103694415@qq.com>
2021-09-20 23:14:19 +0800

    IDEA提交失败

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0673.java
@@ -0,0 +1,19 @@
+package com.leetcode;
+
+/**
+ * @author ：wyq
+ * @date ：Created in 2021/9/20
+ * @description：
+ */
+public class Solution0673 {
+    public static void main(String[] args) {
+        Solution0673 obj = new Solution0673();
+        int[] arr = {1,3,5,4,7};
+        System.out.println(obj.findNumberOfLIS(arr));
+
+    }
+
+    public int findNumberOfLIS(int[] nums) {
+
+    }
+}
