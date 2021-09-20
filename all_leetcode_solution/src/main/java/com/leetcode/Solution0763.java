a103b7afbf598da972296de600ae5b786763c2b9
xiaowangzi123 <1103694415@qq.com>
2021-09-20 09:54:45 +0800

    解答

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0763.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0763.java
@@ -15,6 +15,7 @@ public class Solution0763 {
         Solution0763 ojb = new Solution0763();
         String s = "ababcbacadefegdehijhklij";
         System.out.println(ojb.partitionLabels(s));
+        System.out.println(ojb.partitionLabels2(s));
     }
 
     public List<Integer> partitionLabels(String s) {
@@ -34,4 +35,18 @@ public class Solution0763 {
         }
         return ans;
     }
+
+    public List<Integer> partitionLabels2(String s) {
+        List<Integer> ans = new ArrayList<>();
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            end = Math.max(end, s.lastIndexOf(s.charAt(i)));
+            if (i == end) {
+                ans.add(end - start + 1);
+                start = end + 1;
+            }
+        }
+        return ans;
+    }
+
 }
