93c12987f596751021f183fbfbdf3ef048073962
xiaowangzi123 <1103694415@qq.com>
2021-09-20 13:07:25 +0800

    673解答

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0763.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0763.java
@@ -3,7 +3,9 @@ package com.leetcode;
 import org.hamcrest.text.CharSequenceLength;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author ：wyq
@@ -16,6 +18,7 @@ public class Solution0763 {
         String s = "ababcbacadefegdehijhklij";
         System.out.println(ojb.partitionLabels(s));
         System.out.println(ojb.partitionLabels2(s));
+        System.out.println(ojb.partitionLabels3(s));
     }
 
     public List<Integer> partitionLabels(String s) {
@@ -39,7 +42,7 @@ public class Solution0763 {
     /**
      * 1.找出每个子序列中，第一个字母最后出现位置的索引end
      * 2.遍历start到end位置中的每一个字母，找出此字母最后出现位置的索引
-     *     此索引大于end，替换当前end值
+     * 此索引大于end，替换当前end值
      * 3.当i==end时，表明第一个要找的子序列结束，记录此时的end值
      * 然后从当前位置的后一位开始计算start
      */
@@ -48,6 +51,24 @@ public class Solution0763 {
         int start = 0, end = 0;
         for (int i = 0; i < s.length(); i++) {
             end = Math.max(end, s.lastIndexOf(s.charAt(i)));
+            if (i == end) {
+                ans.add(end - start + 1);
+                start = end + 1;
+            }
+        }
+        return ans;
+    }
+
+    public List<Integer> partitionLabels3(String s) {
+        int n = s.length();
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.put(s.charAt(i), i);
+        }
+        List<Integer> ans = new ArrayList<>();
+        int start = 0, end = 0;
+        for (int i = 0; i < n; i++) {
+            end = Math.max(end, map.get(s.charAt(i)));
             if (i == end) {
                 ans.add(end - start + 1);
                 start = end + 1;
