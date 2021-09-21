3bfc2d3baba677fffed21ddfb61cd6774f7f886a
xiaowangzi123 <1103694415@qq.com>
2021-09-21 22:31:32 +0800

    解答

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0058.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0058.java
@@ -13,12 +13,26 @@ public class Solution0058 {
         String s = "Hello World";
         String s2 = " ";
         System.out.println(obj.lengthOfLastWord(s));
+        System.out.println(obj.lengthOfLastWord2(s));
+
+        System.out.println("---------------");
         System.out.println(obj.lengthOfLastWord(s2));
+        System.out.println(obj.lengthOfLastWord2(s2));
+
     }
 
     public int lengthOfLastWord(String s) {
         s = s.trim();
         int start = s.lastIndexOf(" ") + 1;
         return s.substring(start).length();
+    }
+
+    public int lengthOfLastWord2(String s) {
+        int end = s.length() - 1;
+        while(end >= 0 && s.charAt(end) == ' ') end--;
+        if(end < 0) return 0;
+        int start = end;
+        while(start >= 0 && s.charAt(start) != ' ') start--;
+        return end - start;
     }
 }
