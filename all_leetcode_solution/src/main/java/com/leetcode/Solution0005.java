63b3ddb5fa0cccf1eb608e8d222af55fafb4f6bb
xiaowangzi123 <1103694415@qq.com>
2021-09-22 22:15:12 +0800

    5题目描述

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0005.java
@@ -0,0 +1,44 @@
+package com.leetcode;
+
+/**
+ * @author ：wyq
+ * @date ：Created in 2021/9/22
+ * @description：
+ */
+public class Solution0005 {
+    public static void main(String[] args) {
+        Solution0005 obj = new Solution0005();
+        String s = "abcbacaed";
+        System.out.println(obj.longestPalindrome(s));
+    }
+
+
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+        //回文子串的初始
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int odd = palindromeLength(s, i, i);
+            int even = palindromeLength(s, i, i + 1);
+            int len = Math.max(odd, even);
+            //计算start和end值
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+
+        }
+        return s.substring(start, end + 1);
+    }
+
+    //回文子串的个数或奇或偶，计算以某一个或两个元素为中心的回文子串的长度
+    public int palindromeLength(String s, int l, int r) {
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            l--;
+            r++;
+        }
+        return r - l - 1;
+    }
+}
