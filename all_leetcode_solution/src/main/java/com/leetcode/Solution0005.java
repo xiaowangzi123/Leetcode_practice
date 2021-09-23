0605dc9dcb9e2b9a51ee93668f9d374db1090878
xiaowangzi123 <1103694415@qq.com>
2021-09-23 23:47:09 +0800

    github提交失败，重新提交

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0005.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0005.java
@@ -56,7 +56,26 @@ public class Solution0005 {
      *
      * 求 长度为 11 和长度为 22 的 P(i,j)P(i,j) 时不能用上边的公式
      */
+
     public String longestPalindrome2(String s) {
+        int length = s.length();
+        boolean[][] P = new boolean[length][length];
+        int maxLen = 0;
+        String maxPal = "";
+        for (int len = 1; len <= length; len++) //遍历所有的长度
+            for (int start = 0; start < length; start++) {
+                int end = start + len - 1;
+                if (end >= length) //下标已经越界，结束本次循环
+                    break;
+                P[start][end] = (len == 1 || len == 2 || P[start + 1][end - 1]) && s.charAt(start) == s.charAt(end); //长度为 1 和 2 的单独判断下
+                if (P[start][end] && len > maxLen) {
+                    maxPal = s.substring(start, end + 1);
+                }
+            }
+        return maxPal;
+    }
+
+    public String longestPalindrome3(String s) {
         int n = s.length();
         String res = "";
         boolean[][] dp = new boolean[n][n];
@@ -71,7 +90,7 @@ public class Solution0005 {
         return res;
     }
 
-    public String longestPalindrome3(String s) {
+    public String longestPalindrome4(String s) {
         int n = s.length();
         String res = "";
         boolean[] P = new boolean[n];
