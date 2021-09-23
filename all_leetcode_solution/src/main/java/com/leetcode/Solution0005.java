6bc0a1840292a95328af1a8f42710ae7ebff8264
xiaowangzi123 <1103694415@qq.com>
2021-09-23 23:19:15 +0800

    github提交失败，重新提交

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0005.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0005.java
@@ -10,6 +10,7 @@ public class Solution0005 {
         Solution0005 obj = new Solution0005();
         String s = "abcbacaed";
         System.out.println(obj.longestPalindrome(s));
+        System.out.println(obj.longestPalindrome2(s));
     }
 
 
@@ -42,5 +43,20 @@ public class Solution0005 {
             r++;
         }
         return r - l - 1;
+    }
+
+    public String longestPalindrome2(String s) {
+        int n = s.length();
+        String res = "";
+        boolean[][] dp = new boolean[n][n];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i < 2 || dp[i + 1][j - 1]); //j - i 代表长度减去 1
+                if (dp[i][j] &&  j - i + 1 > res.length()) {
+                    res = s.substring(i, j + 1);
+                }
+            }
+        }
+        return res;
     }
 }
