33c4aa4fd8cbb419688a1bc1b015439a2a3f4f41
xiaowangzi123 <1103694415@qq.com>
2021-09-23 17:04:49 +0800

    IDEA提交失败

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0362.java
@@ -0,0 +1,18 @@
+package com.leetcode;
+
+/**
+ * @author ：wyq
+ * @date ：Created in 2021/9/23
+ * @description：
+ */
+public class Solution0362 {
+    public static void main(String[] args) {
+        Solution0362 obj = new Solution0362();
+        System.out.println(obj.isPowerOfThree(27));
+        System.out.println(obj.isPowerOfThree(30));
+    }
+
+    public boolean isPowerOfThree(int n) {
+
+    }
+}
