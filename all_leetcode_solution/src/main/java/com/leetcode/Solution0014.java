a42505c39c51b1b2bf65ae3aee132b1f454d6694
xiaowangzi123 <1103694415@qq.com>
2021-09-24 10:23:53 +0800

    github提交失败，重新提交

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0014.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0014.java
@@ -22,8 +22,8 @@ public class Solution0014 {
     }
 
     public String longestCommonPrefix(String[] strs) {
-        //字符数组长度为零，直接返回
         /**
+         * 字符数组长度为零，直接返回
          * 纵向遍历每个字符串，从前往后遍历所有字符串的每一列
          * 比较相同列上的字符是否相同
          * 相同继续对下一列进行比较；不同时，当前字符串之前的部分就是最长公共前缀
@@ -35,6 +35,7 @@ public class Solution0014 {
         String res = strs[0];
         for (int i = 0; i < res.length(); i++) {
             for (int j = 1; j < strs.length; j++) {
+                //i == strs[j].length()表示strs[j]长度遍历结束
                 if (i == strs[j].length() || res.charAt(i) != strs[j].charAt(i)) {
                     return res.substring(0, i);
                 }
