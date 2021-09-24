8771fe41d794042a0cd766d6064310b56ccb02c8
xiaowangzi123 <1103694415@qq.com>
2021-09-24 11:39:54 +0800

    673

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0673.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0673.java
@@ -8,13 +8,39 @@ package com.leetcode;
 public class Solution0673 {
     public static void main(String[] args) {
         Solution0673 obj = new Solution0673();
-        int[] arr = {1,3,5,4,7};
+        int[] arr = {1, 3, 5, 4, 7};
         System.out.println(obj.findNumberOfLIS(arr));
 
     }
 
+    /**
+     * dp[i]表示以nums[i]结尾的最长上升子序列的长度
+     * 
+     */
     public int findNumberOfLIS(int[] nums) {
-
-        return 0;
+        int n = nums.length, maxLen = 0, ans = 0;
+        int[] dp = new int[n];
+        int[] cnt = new int[n];
+        for (int i = 0; i < n; ++i) {
+            dp[i] = 1;
+            cnt[i] = 1;
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] > nums[j]) {
+                    if (dp[j] + 1 > dp[i]) {
+                        dp[i] = dp[j] + 1;
+                        cnt[i] = cnt[j];
+                    } else if (dp[j] + 1 == dp[i]) {
+                        cnt[i] += cnt[j];
+                    }
+                }
+            }
+            if (dp[i] > maxLen) {
+                maxLen = dp[i];
+                ans = cnt[i];
+            } else if (dp[i] == maxLen) {
+                ans += cnt[i];
+            }
+        }
+        return ans;
     }
 }
