39281e737d6a1d6887710550219b77ec0daa05f8
xiaowangzi123 <1103694415@qq.com>
2021-09-25 22:29:07 +0800

    1143

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1143.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1143.java
@@ -12,6 +12,13 @@ public class Solution1143 {
         System.out.println(obj.longestCommonSubsequence(s1, s2));
     }
 
+    /**
+     * cnt[i][j]表示text1[0:i]和text2[0:j]的最长公共子序列的长度。
+     * text1[0:i]表示text1的长度为i的前缀，text2[0:j]表示text2的长度为j的前缀。
+     *
+     * 当i=0时，任意的j,有cnt[0][j]=0
+     * 当j=0时，任意的i,有cnt[i][0]=0
+     */
     public int longestCommonSubsequence(String text1, String text2) {
         int m = text1.length(), n = text2.length();
         int[][] count = new int[m + 1][n + 1];
