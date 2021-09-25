192394b6b933cc1a6b90945219714e3abb164c5d
xiaowangzi123 <1103694415@qq.com>
2021-09-25 21:22:53 +0800

    673解答

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0673.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0673.java
@@ -49,7 +49,7 @@ public class Solution0673 {
 
     /**
      * f[i]为nums[i]为结尾的最长上升子序列的长度
-     * 由于每个数都能独自一个成为子序列，因此起始必然有 f[i] = 1f[i]=1；
+     * 由于每个数都能独自一个成为子序列，因此起始必然有f[i]=1；
      * 枚举区间[0, i)的所有数 nums[j]：
      * 如果满足nums[j]<nums[i]，说明nums[i]可以接在nums[j]后面形成上升子序列，此时使用f[j]更新f[i]，即有f[i]=f[j]+1。
      * <p>
@@ -60,12 +60,11 @@ public class Solution0673 {
      * 这时候对f[i]和f[j]+1的大小关系进行分情况讨论：
      * 1.满足f[i]<f[j]+1：说明f[i]会被f[j]+1直接更新，此时同步直接更新g[i]=g[j] 即可；
      * 2.满足f[i]=f[j]+1：说明找到了一个新的符合条件的前驱，此时将值继续累加到结果中，即有g[i]+=g[j]。
-     * 同时记录全局最长上升子序列的最大长度 maxmax，最终答案为所有满足f[i]=max的g[i]的累加值。
+     * 同时记录全局最长上升子序列的最大长度 max，最终答案为所有满足f[i]=max的g[i]的累加值。
      */
     public int findNumberOfLIS2(int[] nums) {
-        int n = nums.length;
+        int n = nums.length, max = 1;
         int[] f = new int[n], g = new int[n];
-        int max = 1;
         for (int i = 0; i < n; i++) {
             f[i] = g[i] = 1;
             for (int j = 0; j < i; j++) {
