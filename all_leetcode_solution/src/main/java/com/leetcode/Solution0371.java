a97ad00d6ba187e55423ebe2a1abf484b5d87157
xiaowangzi123 <1103694415@qq.com>
2021-09-26 10:34:42 +0800

    371解答

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0371.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0371.java
@@ -10,6 +10,7 @@ public class Solution0371 {
         Solution0371 obj = new Solution0371();
         System.out.println(obj.getSum(2, 7));
         System.out.println(obj.getSum(3, 4));
+        System.out.println(obj.getSum2(3, 4));
     }
 
     public int getSum(int a, int b) {
@@ -17,6 +18,26 @@ public class Solution0371 {
             int carry = (a & b) << 1;
             a = a ^ b;
             b = carry;
+        }
+        return a;
+    }
+
+    public int getSum2(int a, int b) {
+        while(b!=0){
+            int c=a^b;
+            int d=(a&b)<<1;
+
+            a=c;
+            b=d;
+        }
+        return a;
+    }
+
+    public int getSum3(int a, int b) {
+        while(b!=0){
+            int c=a^b;
+            b=(a&b)<<1;
+            a=c;
         }
         return a;
     }
