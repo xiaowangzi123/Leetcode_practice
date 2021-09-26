ab031f9cdca602f30271776c1d3030a7227990d8
xiaowangzi123 <1103694415@qq.com>
2021-09-26 22:21:48 +0800

    36解答

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0036.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0036.java
@@ -3,27 +3,44 @@ package com.leetcode;
 /**
  * @author ：wyq
  * @date ：Created in 2021/9/26
- * @description：
+ * @description : 有效的数独
  */
 public class Solution0036 {
     public static void main(String[] args) {
         Solution0036 obj = new Solution0036();
-        String[][] arr = {{"5", "3", ".", ".", "7", ".", ".", ".", "."},
-                {"6", ".", ".", "1", "9", "5", ".", ".", "."},
-                {".", "9", "8", ".", ".", ".", ".", "6", "."},
-                {"8", ".", ".", ".", "6", ".", ".", ".", "3"},
-                {"4", ".", ".", "8", ".", "3", ".", ".", "1"},
-                {"7", ".", ".", ".", "2", ".", ".", ".", "6"},
-                {".", "6", ".", ".", ".", ".", "2", "8", "."},
-                {".", ".", ".", "4", "1", "9", ".", ".", "5"},
-                {".", ".", ".", ".", "8", ".", ".", "7", "9"}};
+        char[][] arr = {{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
         System.out.println(obj.isValidSudoku(arr));
     }
 
     /**
      * 数独
      */
-    public boolean isValidSudoku(String[][] board) {
-
+    public boolean isValidSudoku(char[][] board) {
+        int[][] rows = new int[9][9];
+        int[][] columns = new int[9][9];
+        int[][][] subBox = new int[3][3][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char ch = board[i][j];
+                if (ch != '.') {
+                    int index = ch - '0' - 1;
+                    rows[i][index]++;
+                    columns[j][index]++;
+                    subBox[i / 3][j / 3][index]++;
+                    if (rows[i][index] > 1 || columns[j][index] > 1 || subBox[i / 3][j / 3][index]>1) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
     }
 }
