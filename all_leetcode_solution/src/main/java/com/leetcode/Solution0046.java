a19fb85db93c55872072038d11e5d12cdf6902a2
xiaowangzi123 <1103694415@qq.com>
2021-09-27 17:43:47 +0800

    46解答

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0046.java
@@ -0,0 +1,47 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author ：wyq
+ * @date ：Created in 2021/9/27
+ * @description :全排列
+ */
+class Solution0046 {
+    public static void main(String[] args) {
+        Solution0046 obj = new Solution0046();
+        int[] arr = {1,2,3};
+        System.out.println(obj.permute(arr));
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        List<Integer> output = new ArrayList<>();
+        for (int num : nums) {
+            output.add(num);
+        }
+
+        int n = nums.length;
+        backtrack(n, output, res, 0);
+        return res;
+    }
+
+    public void backtrack(int n, List<Integer> output, List<List<Integer>> res, int first) {
+        // 所有数都填完了
+        if (first == n) {
+            res.add(new ArrayList<Integer>(output));
+        }
+        for (int i = first; i < n; i++) {
+            // 动态维护数组
+            Collections.swap(output, first, i);
+            // 继续递归填下一个数
+            backtrack(n, output, res, first + 1);
+            // 撤销操作
+            Collections.swap(output, first, i);
+        }
+    }
+}
