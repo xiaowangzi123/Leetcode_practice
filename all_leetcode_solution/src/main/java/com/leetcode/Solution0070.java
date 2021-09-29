17fa663756eec2240906163e483a733fd49778cc
xiaowangzi123 <1103694415@qq.com>
2021-09-29 21:11:06 +0800

    LeetCode_0070

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0070.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0070.java
@@ -9,19 +9,30 @@ import java.util.Arrays;
  */
 public class Solution0070 {
     public static void main(String[] args) {
-        System.out.println(climbStairs(1));
-        System.out.println(climbStairs(2));
-        System.out.println(climbStairs(3));
-        System.out.println(climbStairs(4));
-        System.out.println(climbStairs(5));
+        Solution0070 obj = new Solution0070();
+        System.out.println(obj.climbStairs(1));
+        System.out.println(obj.climbStairs(2));
+        System.out.println(obj.climbStairs(3));
+        System.out.println(obj.climbStairs(4));
+        System.out.println(obj.climbStairs(5));
+        System.out.println(obj.climbStairs2(5));
     }
 
     //超时
-    public static int climbStairs(int n) {
+    public int climbStairs(int n) {
         if (n <= 2) {
             return n;
         }
         return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+
+    public int climbStairs2(int n) {
+        int[] nums = new int[n + 1];
+        nums[0] = nums[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            nums[i] = nums[i - 1] + nums[i - 2];
+        }
+        return nums[n];
     }
 
 }
