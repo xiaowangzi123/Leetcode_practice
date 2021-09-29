e266d4fe5ea864d28f0f5eb37a6fa219aea239be
xiaowangzi123 <1103694415@qq.com>
2021-09-29 20:47:53 +0800

    LeetCode_0066.md

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0066.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0066.java
@@ -11,10 +11,10 @@ public class Solution0066 {
     public static void main(String[] args) {
         Solution0066 obj = new Solution0066();
         int[] nums = {1, 2, 9};
-        System.out.println(Arrays.toString(obj.plusOne(nums)));
+        System.out.println(Arrays.toString(obj.plusOne2(nums)));
 
         int[] nums2 = {9, 9, 9};
-        System.out.println(Arrays.toString(obj.plusOne(nums2)));
+        System.out.println(Arrays.toString(obj.plusOne2(nums2)));
     }
 
     public int[] plusOne(int[] digits) {
@@ -26,6 +26,25 @@ public class Solution0066 {
                 return digits;
             }
         }
+        digits = new int[n + 1];
+        digits[0] = 1;
+        return digits;
+    }
+
+    public int[] plusOne2(int[] digits) {
+        int m = digits.length - 1, n = 0, k = 0;
+        while (m >= 0 || n >= 0) {
+            int i = m >= 0 ? digits[m] : 0;
+            int j = n >= 0 ? 1 : 0;
+            digits[m] = (i + j + k) % 10;
+            k = (i + j + k) / 10;
+            if (k == 0) {
+                return digits;
+            }
+            m--;
+            n--;
+        }
+
         digits = new int[digits.length + 1];
         digits[0] = 1;
         return digits;
