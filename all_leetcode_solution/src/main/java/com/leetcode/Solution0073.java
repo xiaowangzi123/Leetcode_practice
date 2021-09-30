36c85355468bcfcd7c865442c2bf26f2bbb6bca3
xiaowangzi123 <1103694415@qq.com>
2021-09-30 23:28:08 +0800

    LeetCode_0086.md

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0073.java
@@ -0,0 +1,39 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author ：wyq
+ * @date ：Created in 2021/9/15
+ * @description：
+ */
+public class Solution0073 {
+    public static void main(String[] args) {
+        Solution0073 obj = new Solution0073();
+        int[][] matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+        obj.setZeroes(matrix);
+        System.out.println(Arrays.deepToString(matrix));
+    }
+
+
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        boolean[] row = new boolean[m], column = new boolean[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = column[j] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] || column[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+    }
+}
