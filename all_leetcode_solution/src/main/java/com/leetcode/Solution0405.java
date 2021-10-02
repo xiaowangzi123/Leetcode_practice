33786b16ce6e7aff20e71aa0d41133ec392fe1c3
xiaowangzi123 <1103694415@qq.com>
2021-10-02 11:38:54 +0800

    LeetCode_0405

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0405.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0405.java
@@ -1,21 +1,42 @@
 package com.leetcode;
 
+import static java.lang.Integer.parseInt;
+
 /**
  * @author :wyq
  * @date ：Created in 2021/10/2
- * @description :
+ * @description : 十六进制
+ * 所以对于正数，可以随意使用位移，与乘除几乎没有结果的区别
+ * 反码跟原码是正数时，一样；负数时，反码就是原码符号位除外，其他位按位取反。
+ * 正整数的补码是其二进制表示，与原码相同；
+ * 负整数的补码，将其原码除符号位外的所有位取反（0变1，1变0，符号位为1不变）后加1
+ * 负数以补码的形式存在
+ * 而对于负数来说，Java中的位移应该谨慎使用，因为位移运算在Java中称为带符号的位移
+ * -5<<1，我们以int为例，由于负数在内存是以补码存在
+ * -5二进制原码：1000 0000 0000 0000 0000 0000 0000 0101
+ *        反码：1111 1111 1111 1111 1111 1111 1111 1010
+ *        补码：1111 1111 1111 1111 1111 1111 1111 1011
+ * 向左移1位即结果为：1111 1111 1111 1111 1111 1111 1111 0110 (后位补0)
+ * 将结果计算反码为：1111 1111 1111 1111 1111 1111 1111 0101
+ * 将结果计算原码为：1000 0000 0000 0000 0000 0000 0000 1010
+ * 所以结果为：数字-10
  */
 public class Solution0405 {
     public static void main(String[] args) {
         Solution0405 obj = new Solution0405();
-        System.out.println(obj.toHex(1));
-        System.out.println(obj.toHex2(1));
-        System.out.println(Integer.toHexString(1));
+        System.out.println(obj.toHex(26));
+        System.out.println(obj.toHex2(26));
+        System.out.println(Integer.toHexString(26));
         System.out.println("-----------------");
 
         System.out.println(obj.toHex(-1));
         System.out.println(obj.toHex2(-1));
         System.out.println(Integer.toHexString(-1));
+        System.out.println("-----------------");
+
+        System.out.println(obj.toHex(-26));
+        System.out.println(obj.toHex2(-26));
+        System.out.println(Integer.toHexString(-26));
         System.out.println("-----------------");
 
         System.out.println(Integer.toHexString(-18));
@@ -28,8 +49,40 @@ public class Solution0405 {
         System.out.println(46 & 0xf);
         System.out.println(25 & 0xf);
 
-        System.out.println(Integer.toBinaryString(8));
+        System.out.println("---------------------------");
+        //转为二进制
+        System.out.println(Integer.toBinaryString(-5));
         System.out.println(Integer.toBinaryString(0xf));
+        System.out.println("---------------------------");
+
+        System.out.println("---------右移动和除以结果不一致------------------");
+        System.out.println(26/16);
+        System.out.println(26>>4);
+        System.out.println(-26/16);
+        System.out.println(-26>>4);
+
+        System.out.println("-------------二进制--------------");
+        System.out.println(Integer.toBinaryString(5));
+        System.out.println(Integer.toBinaryString(-5));
+
+        System.out.println(Integer.toBinaryString(5/2));
+        System.out.println(Integer.toBinaryString(5>>1));
+
+        System.out.println(-5/2);
+        System.out.println(-5>>1);
+        /**
+         * 补码：1111 1111 1111 1111 1111 1111 1111 1110
+         * 反码：1111 1111 1111 1111 1111 1111 1111 1101
+         * 原码：1000 0000 0000 0000 0000 0000 0000 0010
+         * 结果为-2
+         */
+        System.out.println(Integer.toBinaryString(-5/2));
+        /**
+         * 11111111111111111111111111111101
+         */
+        System.out.println(Integer.toBinaryString(-5>>1));
+
+
 
     }
 
@@ -48,21 +101,22 @@ public class Solution0405 {
 
     }
 
+    //执行速度比方法一慢
     public String toHex2(int num) {
         String s = "0123456789abcdef";
         if (num == 0) {
             return "0";
         }
-
         String res = "";
         /*while (num != 0 && res.length() < 8) {
             // 不能取余操作，负数时报错String index out of range
             res.append(s.charAt((num % 16)));
+            // 取整和位移操作，负数时结果不一致
             num /= 16;
         }*/
         while (num != 0 && res.length() < 8) {
-            res += s.charAt(num & 0xf);
-            num /= 4;
+            res = s.charAt(num & 0xf) + res;
+            num >>= 4;
         }
         return res;
 
