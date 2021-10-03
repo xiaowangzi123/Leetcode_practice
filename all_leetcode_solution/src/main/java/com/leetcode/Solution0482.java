e5ec535abbace720597fccc8855391189772ff51
xiaowangzi123 <1103694415@qq.com>
2021-10-04 00:26:24 +0800

    LeetCode_0094

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0482.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0482.java
@@ -10,9 +10,18 @@ public class Solution0482 {
         Solution0482 obj = new Solution0482();
         String s = "5F3Z-2e-9-w";
         System.out.println(obj.licenseKeyFormatting(s, 4));
+        System.out.println(obj.licenseKeyFormatting2(s, 4));
+        System.out.println(obj.licenseKeyFormatting3(s, 4));
 
         String s2 = "2-5g-3-J";
+        System.out.println(obj.licenseKeyFormatting(s2, 2));
         System.out.println(obj.licenseKeyFormatting2(s2, 2));
+        System.out.println(obj.licenseKeyFormatting3(s2, 2));
+
+        String s3 = "--a-a-a-a--";
+        System.out.println(obj.licenseKeyFormatting(s3, 2));
+        System.out.println(obj.licenseKeyFormatting2(s3, 2));
+        System.out.println(obj.licenseKeyFormatting3(s3, 2));
 
         int k = 2;
         StringBuilder sb = new StringBuilder("5F3Z2E9W");
@@ -36,7 +45,7 @@ public class Solution0482 {
         return sb.reverse().toString();
     }
 
-    //主要是insert()方法
+    //insert()方法
     public String licenseKeyFormatting2(String s, int k) {
         s = s.replaceAll("-", "").toUpperCase();
         StringBuilder sb = new StringBuilder(s);
@@ -44,5 +53,24 @@ public class Solution0482 {
             sb.insert(i, '-');
         }
         return sb.toString();
+    }
+
+    public String licenseKeyFormatting3(String s, int k) {
+        StringBuilder ans = new StringBuilder();
+        int count = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != '-') {
+                ans.append(Character.toUpperCase(s.charAt(i)));
+                count++;
+                if (count % k == 0) {
+                    ans.append("-");
+                }
+            }
+        }
+        if (ans.length() > 0 && ans.charAt(ans.length() - 1) == '-') {
+            ans.deleteCharAt(ans.length() - 1);
+        }
+
+        return ans.reverse().toString();
     }
 }
--- all_leetcode_solution/src/main/java/com/leetcode/tree/Solution0094.java
+++ all_leetcode_solution/src/main/java/com/leetcode/tree/Solution0094.java
@@ -15,11 +15,11 @@ import java.util.List;
 public class Solution0094 {
     public static void main(String[] args) {
         Solution0094 obj = new Solution0094();
-        TreeNode t7 = new TreeNode(7,null,null);
-        TreeNode t6 = new TreeNode(6,null,null);
-        TreeNode t5 = new TreeNode(5,null,null);
+        TreeNode t7 = new TreeNode(7, null, null);
+        TreeNode t6 = new TreeNode(6, null, null);
+        TreeNode t5 = new TreeNode(5, null, null);
         TreeNode t4 = new TreeNode(4);
-        TreeNode t3 = new TreeNode(3,t6,t7);
+        TreeNode t3 = new TreeNode(3, t6, t7);
         TreeNode t2 = new TreeNode(2, t4, t5);
         TreeNode t1 = new TreeNode(1, t2, t3);
 
@@ -50,7 +50,7 @@ public class Solution0094 {
 
     public List<Integer> inorderTraversal2(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        Deque<TreeNode> stack = new LinkedList<>();
         while (root != null || !stack.isEmpty()) {
             while (root != null) {
                 stack.push(root);
@@ -63,4 +63,40 @@ public class Solution0094 {
         return ans;
 
     }
+
+    //Morris 中序遍历
+    public List<Integer> inorderTraversal3(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        TreeNode predecessor = null;
+
+        while (root != null) {
+            if (root.left != null) {
+                // predecessor 节点就是当前 root 节点向左走一步，然后一直向右走至无法走为止
+                predecessor = root.left;
+                while (predecessor.right != null && predecessor.right != root) {
+                    predecessor = predecessor.right;
+                }
+
+                // 让 predecessor 的右指针指向 root，继续遍历左子树
+                if (predecessor.right == null) {
+                    predecessor.right = root;
+                    root = root.left;
+                }
+                // 说明左子树已经访问完了，我们需要断开链接
+                else {
+                    res.add(root.val);
+                    predecessor.right = null;
+                    root = root.right;
+                }
+            }
+            // 如果没有左孩子，则直接访问右孩子
+            else {
+                res.add(root.val);
+                root = root.right;
+            }
+        }
+        return res;
+    }
+
+
 }
