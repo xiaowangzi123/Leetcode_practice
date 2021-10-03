96dd0e25ebc7801a3147b65d21a4ad818d43bc68
xiaowangzi123 <1103694415@qq.com>
2021-10-03 21:02:17 +0800

    LeetCode_1652

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1652.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1652.java
@@ -13,29 +13,58 @@ public class Solution1652 {
         int k = -2;
         Solution1652 obj = new Solution1652();
         System.out.println(Arrays.toString(obj.decrypt(arr, k)));
+        System.out.println(Arrays.toString(obj.decrypt2(arr, k)));
 
     }
 
     public int[] decrypt(int[] code, int k) {
-        int[] res = new int[code.length];
+        int n = code.length;
+        int[] res = new int[n];
+        //k=0直接返回值全为0的数组
         if (k == 0) {
             return res;
         }
 
         int start = 1, sum = 0;
         if (k < 0) {
-            start = code.length + k;
-            k = code.length - 1;
+            start = n + k;
+            k = n - 1;
         }
         //第一个元素值sum
         for (int i = start; i <= k; i++) {
             sum += code[i];
         }
 
-        for (int i = 0; i < code.length; i++) {
+        for (int i = 0; i < n; i++) {
             res[i] = sum;
-            sum -= code[(start++) % code.length];
-            sum += code[(++k) % code.length];
+            //减去当前元素值
+            sum -= code[(start++) % n];
+            //加上下一元素，此时sum表示（i+1）元素的值
+            sum += code[(++k) % n];
+        }
+        return res;
+    }
+
+
+    public int[] decrypt2(int[] code, int k) {
+        int n = code.length;
+        int[] res = new int[n];
+        if (k == 0) {
+            return res;
+        }
+
+        for (int i = 0; i < n; i++) {
+            res[i] = 0;
+            if (k < 0) {
+                for (int j = i + n - 1; j >= i + k + n; j--) {
+                    res[i] += code[j % n];
+                }
+            }
+            if (k > 0) {
+                for (int j = i + n + 1; j <= i + k + n; j++) {
+                    res[i] += code[j % n];
+                }
+            }
         }
         return res;
     }
