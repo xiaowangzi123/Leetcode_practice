86e69bae6f72f83e010c53ac15f1eff042226d18
xiaowangzi123 <1103694415@qq.com>
2021-10-04 21:32:14 +0800

    LeetCode_0118

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0118.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0118.java
@@ -17,13 +17,13 @@ public class Solution0118 {
         System.out.println(obj.generate(4));
         System.out.println(obj.generate(4).get(3));
         System.out.println("-------------------");
-        System.out.println(obj.getRow2(3));
-        System.out.println(obj.getRow3(3));
+
+        System.out.println(obj.generate2(4));
     }
 
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> res = new  ArrayList<>();
-        if(numRows==0){
+        List<List<Integer>> res = new ArrayList<>();
+        if (numRows == 0) {
             return res;
         }
 
@@ -31,13 +31,13 @@ public class Solution0118 {
         arr.add(1);
         res.add(arr);
 
-        for(int i=2; i<=numRows; i++){
-            List<Integer> list = new  ArrayList<>();
+        for (int i = 2; i <= numRows; i++) {
+            List<Integer> list = new ArrayList<>();
             list.add(1);
 
-            List<Integer> pre = res.get(res.size()-1);
-            for(int j=1; j<i-1; j++){
-                list.add(pre.get(j-1)+pre.get(j));
+            List<Integer> pre = res.get(res.size() - 1);
+            for (int j = 1; j < i - 1; j++) {
+                list.add(pre.get(j - 1) + pre.get(j));
             }
             list.add(1);
             res.add(list);
@@ -45,56 +45,37 @@ public class Solution0118 {
         return res;
     }
 
-    public List<Integer> getRow(int rowIndex) {
-        List<List<Integer>> lists = new ArrayList<>();
-        for (int i = 0; i <= rowIndex; i++) {
-            List<Integer> row = new ArrayList<>();
-            for (int j = 0; j <= i; j++) {
-                if (j == 0 || j == i) {
-                    row.add(1);
-                } else {
-                    row.add(lists.get(i - 1).get(j - 1) + lists.get(i - 1).get(j));
-                }
+    public List<List<Integer>> generate2(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            arr.add(0, 1);
+
+            for (int j = 1; j < arr.size() - 1; ++j) {
+                arr.set(j, arr.get(j) + arr.get(j + 1));
             }
-            lists.add(row);
+            res.add(new ArrayList<>(arr));
         }
-        return lists.get(rowIndex);
+
+        return res;
     }
 
-    public List<Integer> getRow2(int rowIndex) {
-        List<Integer> list = new ArrayList<>(rowIndex);
-        for (int i = 0; i <= rowIndex; i++) {
-            List<Integer> row = new ArrayList<>();
-            for (int j = 0; j <= i; j++) {
-                if (j == 0 || j == i) {
-                    row.add(1);
-                } else {
-                    row.add(list.get(j - 1) + list.get(j));
-                }
+    public List<List<Integer>> generate3(int numRows) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+
+        for(int i=0; i<numRows; i++){
+            arr.add(0,1);
+
+            for(int j=1; j<arr.size()-1; ++j){
+                arr.set(j, arr.get(j)+arr.get(j+1));
             }
-            list = row;
+
+            res.add(new ArrayList<Integer>(arr));
         }
-        return list;
+
+        return res;
     }
 
-    public List<Integer> getRow3(int rowIndex) {
-        int[] res = new int[rowIndex + 1];
-        for (int i = 0; i <= rowIndex; i++) {
-            res[i] = 1;
-            for (int j = i - 1; j >= 1; j--) {
-                res[j] = res[j] + res[j - 1];
-            }
-        }
-        //int[]转list，需要apache的commons-lang3包
-        return Arrays.asList(ArrayUtils.toObject(res));
-        //java8以上版本可用lambda表达式，装箱操作
-//        return Arrays.stream(res).boxed().collect(Collectors.toList());
-
-        /*list转int[]*/
-        //方法一：
-//        Integer[] intArr =  list.toArray(new Integer[list.size()]);
-        //方法二：java8及以上版本
-//        int[] intArr1 =  list.stream().mapToInt(Integer::valueOf).toArray();
-
-    }
 }
