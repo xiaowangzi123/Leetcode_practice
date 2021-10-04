92e3d7811ae8e7452646d6ff3eab7b41bb55c59b
xiaowangzi123 <1103694415@qq.com>
2021-10-04 18:11:27 +0800

    LeetCode_0094

--- all_leetcode_solution/src/main/java/com/leetcode/tree/Solution0094.java
+++ all_leetcode_solution/src/main/java/com/leetcode/tree/Solution0094.java
@@ -25,6 +25,7 @@ public class Solution0094 {
 
         System.out.println(obj.inorderTraversal(t1));
         System.out.println(obj.inorderTraversal2(t1));
+        System.out.println(obj.inorderTraversal3(t1));
 
 
     }
@@ -64,20 +65,34 @@ public class Solution0094 {
 
     }
 
-    //Morris 中序遍历
+    /**
+     * Morris 遍历算法整体步骤如下（假设当前遍历到的节点为 x）：
+     * <p>
+     * 1.如果x无左孩子，先将x的值加入答案数组，再访问x的右孩子，即x=x.right。
+     * 2.如果x有左孩子，则找到x左子树上最右的节点（即左子树中序遍历的最后一个节点，x在中序遍历中的前驱节点）记为predecessor。
+     * 根据 predecessor 的右孩子是否为空，进行如下操作。
+     * 2.1如果predecessor的右孩子为空，则将其右孩子指向x，然后访问x的左孩子，即x=x.left。
+     * 2.2如果predecessor的右孩子不为空，则此时其右孩子指向x，说明我们已经遍历完x的左子树,
+     * 将predecessor的右孩子置空，将x的值加入答案数组，然后访问x的右孩子，即 x=x.right。
+     * 3.重复上述操作，直至访问完整棵树。
+     */
     public List<Integer> inorderTraversal3(TreeNode root) {
-        List<Integer> res = new ArrayList<Integer>();
+        List<Integer> res = new ArrayList<>();
         TreeNode predecessor = null;
 
         while (root != null) {
-            if (root.left != null) {
-                // predecessor 节点就是当前 root 节点向左走一步，然后一直向右走至无法走为止
+            // 如果没有左孩子，则直接访问右孩子
+            if (root.left == null) {
+                res.add(root.val);
+                root = root.right;
+            } else {
+                // predecessor 节点就是当前root节点向左走一步，然后一直向右走至无法走为止
                 predecessor = root.left;
                 while (predecessor.right != null && predecessor.right != root) {
                     predecessor = predecessor.right;
                 }
 
-                // 让 predecessor 的右指针指向 root，继续遍历左子树
+                // 让 predecessor 的右指针指向root，继续遍历左子树
                 if (predecessor.right == null) {
                     predecessor.right = root;
                     root = root.left;
@@ -88,11 +103,6 @@ public class Solution0094 {
                     predecessor.right = null;
                     root = root.right;
                 }
-            }
-            // 如果没有左孩子，则直接访问右孩子
-            else {
-                res.add(root.val);
-                root = root.right;
             }
         }
         return res;
