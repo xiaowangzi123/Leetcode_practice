9350cbaddffc0b5f7690b448f38dedc7182ca906
xiaowangzi123 <1103694415@qq.com>
2021-10-05 21:50:23 +0800

    LeetCode_0121

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0121.java
@@ -0,0 +1,48 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author ：wyq
+ * @date ：Created in 2021/7/9
+ * @description :买卖股票的最佳时机
+ */
+public class Solution0121 {
+    public static void main(String[] args) {
+        Solution0121 obj = new Solution0121();
+        int[] arr = {7,1,5,3,6,4};
+        System.out.println(obj.maxProfit(arr));
+        System.out.println(obj.maxProfit2(arr));
+    }
+
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int res = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < min) {
+                min = prices[i];
+            }
+            if ((prices[i] - min) > res) {
+                res = prices[i] - min;
+            }
+        }
+        return res;
+    }
+
+    public int maxProfit2(int[] prices) {
+        if (prices == null || prices.length <= 1) {
+            return 0;
+        }
+        int res = 0;
+        int min = prices[0];
+
+        for (int i = 1; i < prices.length; i++) {
+            res = Math.max(res, prices[i] - min);
+            min = Math.min(min, prices[i]);
+        }
+        return res;
+    }
+
+}
