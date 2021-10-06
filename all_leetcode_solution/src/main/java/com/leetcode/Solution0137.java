6c10984001dd077381f9e453ff328272e81ffa36
xiaowangzi123 <1103694415@qq.com>
2021-10-06 23:15:31 +0800

    LeetCode_0137.md

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0137.java
@@ -0,0 +1,34 @@
+package com.leetcode;
+
+import java.util.*;
+
+/**
+ * @author ：wyq
+ * @date ：Created in 2021/9/14
+ * @description：
+ */
+public class Solution0137 {
+    public static void main(String[] args) {
+        Solution0137 obj = new Solution0137();
+        int[] arr = {0, 1, 0, 1, 0, 1, 99};
+        System.out.println(obj.singleNumber(arr));
+
+
+    }
+
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+//        Entry<Integer, Integer> entry = map.entrySet();
+        for (Map.Entry<Integer, Integer> en : map.entrySet()) {
+            if (en.getValue() == 1) {
+                return en.getKey();
+            }
+        }
+
+        return 0;
+    }
+}
