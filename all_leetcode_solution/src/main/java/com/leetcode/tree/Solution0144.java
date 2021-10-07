1ff355e47d0dce811083d96f984500d6b37a25f1
xiaowangzi123 <1103694415@qq.com>
2021-10-07 14:16:12 +0800

    LeetCode_0144

---
+++ all_leetcode_solution/src/main/java/com/leetcode/tree/Solution0144.java
@@ -0,0 +1,113 @@
+package com.leetcode.tree;
+
+import com.leetcode.bean.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2021/10/2
+ * @description :二叉树的中序遍历
+ */
+public class Solution0144 {
+    public static void main(String[] args) {
+        Solution0144 obj = new Solution0144();
+        TreeNode t7 = new TreeNode(7, null, null);
+        TreeNode t6 = new TreeNode(6, null, null);
+        TreeNode t5 = new TreeNode(5, null, null);
+        TreeNode t4 = new TreeNode(4);
+        TreeNode t3 = new TreeNode(3, t6, t7);
+        TreeNode t2 = new TreeNode(2, t4, t5);
+        TreeNode t1 = new TreeNode(1, t2, t3);
+
+        System.out.println(obj.preorderTraversal(t1));
+        System.out.println(obj.preorderTraversal2(t1));
+        System.out.println(obj.preorderTraversal3(t1));
+
+
+    }
+
+    //前序遍历
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        preRecursive(root, res);
+        return res;
+    }
+
+    /**
+     * 递归
+     */
+    public void preRecursive(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        res.add(root.val);
+        preRecursive(root.left, res);
+        preRecursive(root.right, res);
+    }
+
+    public List<Integer> preorderTraversal2(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        Deque<TreeNode> stack = new LinkedList<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                ans.add(root.val);
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            root = root.right;
+        }
+        return ans;
+
+    }
+
+    /**
+     * Morris 遍历算法整体步骤如下（假设当前遍历到的节点为 x）：
+     * <p>
+     * 1.如果x无左孩子，先将x的值加入答案数组，再访问x的右孩子，即x=x.right。
+     * 2.如果x有左孩子，则找到x左子树上最右的节点（即左子树中序遍历的最后一个节点，x在中序遍历中的前驱节点）记为predecessor。
+     * 根据 predecessor 的右孩子是否为空，进行如下操作。
+     * 2.1如果predecessor的右孩子为空，则将其右孩子指向x，然后访问x的左孩子，即x=x.left。
+     * 2.2如果predecessor的右孩子不为空，则此时其右孩子指向x，说明我们已经遍历完x的左子树,
+     * 将predecessor的右孩子置空，将x的值加入答案数组，然后访问x的右孩子，即 x=x.right。
+     * 3.重复上述操作，直至访问完整棵树。
+     */
+    public List<Integer> preorderTraversal3(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (root == null) {
+            return res;
+        }
+
+        TreeNode p1 = root, p2 = null;
+
+        while (p1 != null) {
+            p2 = p1.left;
+            if (p2 != null) {
+                while (p2.right != null && p2.right != p1) {
+                    p2 = p2.right;
+                }
+                if (p2.right == null) {
+                    res.add(p1.val);
+                    p2.right = p1;
+                    p1 = p1.left;
+                    continue;
+                } else {
+                    p2.right = null;
+                }
+            } else {
+                res.add(p1.val);
+            }
+            p1 = p1.right;
+        }
+        return res;
+    }
+
+
+}
