368be50a1b64b2420fe3345b355a8d4f95bc1809
xiaowangzi123 <1103694415@qq.com>
2021-10-09 09:59:42 +0800

    LeetCode_0496

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0496.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0496.java
@@ -10,24 +10,28 @@ import java.util.*;
 public class Solution0496 {
     public static void main(String[] args) {
         Solution0496 obj = new Solution0496();
-        int[] nums1 = {4, 1, 2}, nums2 = {1, 3, 4, 2};
+//        int[] nums1 = {4, 1, 2}, nums2 = {1, 3, 4, 2};
+        int[] nums1 = {7, 1, 2}, nums2 = {2, 3, 5, 1, 0, 7, 6};
         System.out.println(Arrays.toString(obj.nextGreaterElement(nums1, nums2)));
         System.out.println(Arrays.toString(obj.nextGreaterElement2(nums1, nums2)));
     }
 
-    public int[] nextGreaterElement(int[] nums1, int[] nums) {
-        int n1 = nums1.length, n2 = nums.length;
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int n1 = nums1.length, n2 = nums2.length;
         int[] res = new int[n1];
         for (int i = 0; i < n1; i++) {
             for (int j = 0; j < n2; j++) {
-                if (nums1[i] == nums[j]) {
+                //找出nums1[i]在nums2中出现的位置j
+                if (nums1[i] == nums2[j]) {
                     int k = j + 1;
                     for (; k < n2; k++) {
-                        if (nums1[i] < nums[k]) {
-                            res[i] = nums[k];
+                        //从相同元素的下一位置j+1开始遍历，找到第一个大于当前元素的值，并结束当前循环
+                        if (nums1[i] < nums2[k]) {
+                            res[i] = nums2[k];
                             break;
                         }
                     }
+                    //找不到大于当前元素的值，赋值为-1
                     if (k == n2) {
                         res[i] = -1;
                     }
@@ -41,7 +45,8 @@ public class Solution0496 {
         int n1 = nums1.length, n2 = nums2.length;
 
         Deque<Integer> stack = new ArrayDeque<>();
-        Map<Integer, Integer> map = new HashMap<>();
+        //key为当前元素值，value为比当前元素大的下一个元素值
+        Map<Integer, Integer> map = new HashMap<>(n2);
 
         for (int i = 0; i < n2; i++) {
             while (!stack.isEmpty() && stack.peekLast() < nums2[i]) {
