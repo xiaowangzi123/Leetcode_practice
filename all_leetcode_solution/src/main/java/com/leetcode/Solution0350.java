2e89b83a7f84ecff8c8bc722405de219eeca2455
xiaowangzi123 <1103694415@qq.com>
2021-10-09 18:40:19 +0800

    LeetCode_0350

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0350.java
@@ -0,0 +1,66 @@
+package com.leetcode;
+
+import java.util.*;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2021/10/9
+ * @description :
+ */
+public class Solution0350 {
+    public static void main(String[] args) {
+        Solution0350 obj = new Solution0350();
+//        int[] nums1 = {4, 9, 5}, nums2 = {9, 4, 9, 8, 4};
+        int[] nums1 = {1, 2, 2, 1}, nums2 = {2, 2};
+        System.out.println(Arrays.toString(obj.intersect(nums1, nums2)));
+        System.out.println(Arrays.toString(obj.intersect2(nums1, nums2)));
+        System.out.println("----------------------------");
+
+
+    }
+
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        int n1 = nums1.length, n2 = nums2.length;
+        int[] ans = new int[n1];
+
+        int count = 0, i = 0, j = 0;
+        while (i < n1 && j < n2) {
+            if (nums1[i] == nums2[j]) {
+                ans[count++] = nums1[i];
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        return Arrays.copyOfRange(ans, 0, count);
+    }
+
+    //效率不如第一个
+    public int[] intersect2(int[] nums1, int[] nums2) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums1.length; i++) {
+            list.add(nums1[i]);
+        }
+
+        ArrayList<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums2.length; i++) {
+            if (list.contains(nums2[i])) {
+                res.add(nums2[i]);
+                list.remove(list.indexOf(nums2[i]));
+            }
+        }
+
+        return res.stream().mapToInt(i->i).toArray();
+        /*int[] arr = new int[res.size()];
+        for (int x = 0; x < res.size(); x++) {
+            arr[x] = (int) res.get(x);
+        }
+        return arr;*/
+    }
+}
