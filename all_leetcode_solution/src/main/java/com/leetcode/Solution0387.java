2cd7a3fecd10128cf155ba226cd3202ffa09fc68
xiaowangzi123 <1103694415@qq.com>
2021-10-09 20:30:50 +0800

    LeetCode_0387

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0387.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0387.java
@@ -32,11 +32,12 @@ public class Solution0387 {
 
     public int firstUniqChar2(String s) {
         Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
+        int n = s.length();
+        for (int i = 0; i <n; i++) {
             char ch = s.charAt(i);
             map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
-        for (int i=0;i<s.length();i++){
+        for (int i=0;i<n;i++){
             if (map.get(s.charAt(i))==1){
                 return i;
             }
