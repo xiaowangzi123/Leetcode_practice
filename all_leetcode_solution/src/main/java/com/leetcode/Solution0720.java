7be74d2c087efc30dfaecf4d359eab65c8d2e343
xiaowangzi123 <1103694415@qq.com>
2021-10-10 20:20:49 +0800

    LeetCode_0720

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0720.java
@@ -0,0 +1,36 @@
+package com.leetcode;
+
+import java.util.*;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2021/10/10
+ * @description :
+ */
+public class Solution0720 {
+    public static void main(String[] args) {
+        Solution0720 obj = new Solution0720();
+//        String[] words = {"w", "wo", "wor", "worl", "world"};
+        String[] words = {"a", "banana", "app", "appl", "ap", "apply", "apple"};
+        System.out.println(obj.longestWord(words));
+
+    }
+
+    /**
+     * 对数组排序，再利用Set对字母存储，小的单词一定包含在后面大的单词里面。后面只需要取前缀相同的
+     * 对字母排序后，第一个单词一定是共有的，后面只需在此基础上添加
+     * 结果有几个的话，按字母排序取第一个
+     */
+    public String longestWord(String[] words) {
+        Arrays.sort(words);
+        Set<String> set = new HashSet<>();
+        String ans = "";
+        for (String word : words) {
+            if (word.length() == 1 || set.contains(word.substring(0, word.length() - 1))) {
+                ans = word.length() > ans.length() ? word : ans;
+                set.add(ans);
+            }
+        }
+        return ans;
+    }
+}
