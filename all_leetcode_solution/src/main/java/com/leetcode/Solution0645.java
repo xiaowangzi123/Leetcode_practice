665f0d9305965131699f7ee370747a4cb07853cc
xiaowangzi123 <1103694415@qq.com>
2021-10-10 14:09:43 +0800

    LeetCode_0645输出有顺序要求

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0645.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0645.java
@@ -14,6 +14,7 @@ public class Solution0645 {
 //        int[] arr = {1, 1};
         int[] arr = {3, 2, 3, 4, 6, 5};
         System.out.println(Arrays.toString(obj.findErrorNums(arr)));
+        System.out.println(Arrays.toString(obj.findErrorNums2(arr)));
     }
 
     public int[] findErrorNums(int[] nums) {
@@ -28,10 +29,29 @@ public class Solution0645 {
 
         //找出缺失的数据
         for (int i = 0; i < nums.length; i++) {
-            if (!set.contains(i+1)) {
+            if (!set.contains(i + 1)) {
                 ans.add(i + 1);
             }
         }
         return ans.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public int[] findErrorNums2(int[] nums) {
+        int[] ans = new int[2];
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (int i = 0; i <= nums.length; i++) {
+            int temp = map.getOrDefault(i, 0);
+            if (temp == 0) {
+                ans[1] = i;
+            } else if (temp == 2) {
+                ans[0] = i;
+            }
+        }
+        return ans;
     }
 }
