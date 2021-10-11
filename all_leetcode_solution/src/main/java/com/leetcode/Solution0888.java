395313dec4b33a209d2cc0e1e6328dd9b85a90e6
xiaowangzi123 <1103694415@qq.com>
2021-10-11 17:53:37 +0800

    LeetCode_0888

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0888.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0888.java
@@ -15,6 +15,8 @@ public class Solution0888 {
         int[] arr = {1, 2, 5}, arr2 = {2, 4};
 
         System.out.println(Arrays.toString(obj.fairCandySwap(arr, arr2)));
+        System.out.println(Arrays.toString(obj.fairCandySwap2(arr, arr2)));
+        System.out.println("-------------------");
 
         System.out.println(Arrays.stream(arr).sum());
         System.out.println(Arrays.stream(arr).count());
@@ -29,9 +31,33 @@ public class Solution0888 {
     public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
         int sum1 = Arrays.stream(aliceSizes).sum();
         int sum2 = Arrays.stream(bobSizes).sum();
+        int x = (sum1 - sum2) / 2;
         int[] ans = new int[2];
+        for (int i = 0; i < aliceSizes.length; i++) {
+            for (int j = 0; j < bobSizes.length; j++) {
+                if (aliceSizes[i] - bobSizes[j] == x) {
+                    ans[0] = aliceSizes[i];
+                    ans[1] = bobSizes[j];
+                }
+            }
+        }
+        return ans;
+    }
 
-        return null;
+    //超时
+    public int[] fairCandySwap2(int[] aliceSizes, int[] bobSizes) {
+        int sum1 = Arrays.stream(aliceSizes).sum();
+        int sum2 = Arrays.stream(bobSizes).sum();
+        int x = (sum1 - sum2) / 2;
+        int[] ans = new int[2];
+        for (int i = 0; i < aliceSizes.length; i++) {
+            if (Arrays.asList(Arrays.stream(bobSizes).boxed().toArray(Integer[]::new)).contains(aliceSizes[i] - x)) {
+                ans[0] = aliceSizes[i];
+                ans[1] = aliceSizes[i]-x;
+            }
+
+        }
+        return ans;
     }
 
     public static boolean useList(String[] arr, String targetValue) {
