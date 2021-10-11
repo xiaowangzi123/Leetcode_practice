28e6cfa2ee86537f663f0e35d37fdff5e240e745
xiaowangzi123 <1103694415@qq.com>
2021-10-11 15:41:45 +0800

    LeetCode_0859

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0859.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0859.java
@@ -20,23 +20,24 @@ public class Solution0859 {
         StringBuilder s1 = new StringBuilder("ab");
         StringBuilder s2 = new StringBuilder("ba");
         StringBuilder s3 = new StringBuilder("ab");
+        StringBuilder s4 = new StringBuilder("ba");
 
         //false
         System.out.println(s1.equals(s3));
-//        System.out.println(s1.toString().equals(s3.reverse().toString())); //false
         //true
-//        System.out.println("ab".equals(s3.reverse().toString()));
-//        System.out.println(s1.equals(s2.reverse()));//false
-//        System.out.println(s1.toString().equals(s2.reverse().toString()));
-//        if (s1.toString().equals(s2.reverse().toString())){
-//            System.out.println("equals");
-//        }
-//        System.out.println("ab".equals(s2.reverse().toString()));
+        System.out.println(s1.toString().equals(s3.toString()));
+        //false
+        System.out.println(s1.equals(s2.reverse()));
+        //true
+        System.out.println(s1.toString().equals(s4.reverse().toString()));
     }
 
     /**
      * 1.A、B字符串长度不同，直接返回false
      * 2.A等于B,找其中一个字符串是否有重复元素，有重复，返回true；没有重复元素，返回false
+     * 3.A不等于B，找出AB对应位置不同的元素，组成S1，S2
+     * 3.1.如果S1，S2的长度不等于2，无论怎么交换一次都无法满足条件，返回false
+     * 3.1.S1，S2的长度等于2,对其中一个反转与另外一个相比，相等返回true；不相等返回false。
      */
     public boolean buddyStrings(String s, String goal) {
         if (s.length() != goal.length()) {
