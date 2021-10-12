961bce68f3f38e24436e2e28f4343e856c3c2a9e
xiaowangzi123 <1103694415@qq.com>
2021-10-12 15:29:43 +0800

    LeetCode_1935

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1935.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1935.java
@@ -8,73 +8,34 @@ import java.util.Map;
 /**
  * @author :wyq
  * @date ：Created in 2021/10/12
- * @description :
+ * @description :可以输入的最大单词数
  */
 public class Solution1935 {
     public static void main(String[] args) {
         Solution1935 obj = new Solution1935();
-        String[] words = {"bella", "label", "roller"};
+        String text = "hello world", brokenLetters = "ad";
 
-        System.out.println(obj.commonChars(words));
-        System.out.println(obj.commonChars2(words));
+        System.out.println(obj.canBeTypedWords(text, brokenLetters));
     }
 
-    public List<String> commonChars(String[] words) {
-        Map<Character, Integer> map = new HashMap<>();
-        List<String> ans = new ArrayList<>();
-        for (char ch : words[0].toCharArray()) {
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-        }
-
-        for (int i = 1; i < words.length; i++) {
-            Map<Character, Integer> temp = new HashMap<>();
-            for (char ch : words[i].toCharArray()) {
-                if (map.containsKey(ch)) {
-                    temp.put(ch, temp.getOrDefault(ch, 0) + 1);
+    public int canBeTypedWords(String text, String brokenLetters) {
+        int count = 0;
+        String[] str = text.split(" ");
+        for (String s : str) {
+            boolean flag = true;
+            for (int i = 0; i < s.length(); i++) {
+                char ch = s.charAt(i);
+                if (brokenLetters.contains(ch + "")) {
+                    flag = false;
+                    break;
                 }
             }
-
-            for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-                map.put(entry.getKey(), Math.min(entry.getValue(), temp.getOrDefault(entry.getKey(), 0)));
+            if (flag) {
+                count++;
             }
         }
-
-        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-            if (entry.getValue() > 0) {
-                for (int i = 0; i < entry.getValue(); i++) {
-                    ans.add(entry.getKey().toString());
-                }
-            }
-        }
-        return ans;
+        return count;
     }
 
-    public List<String> commonChars2(String[] words) {
-        List<String> ans = new ArrayList<>();
-        int[] res = new int[26];
-        for (char ch : words[0].toCharArray()) {
-            res[ch - 'a']++;
-        }
 
-        for (int i = 1; i < words.length; i++) {
-            int[] temp = new int[26];
-            for (char ch : words[i].toCharArray()) {
-                temp[ch - 'a']++;
-            }
-
-            for (int j = 0; j < 26; j++) {
-                res[j] = Math.min(res[j], temp[j]);
-            }
-        }
-
-        for (int i=0;i<26;i++){
-            if (res[i]>0){
-                for (int j=0;j<res[i];j++){
-                    ans.add(String.valueOf((char)(i+'a')));
-//                    ans.add((char)(i+'a')+"");
-                }
-            }
-        }
-        return ans;
-    }
 }
