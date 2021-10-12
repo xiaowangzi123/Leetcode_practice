8487a2215d19428e344350a483db9af7a24da3ad
xiaowangzi123 <1103694415@qq.com>
2021-10-12 10:59:31 +0800

    LeetCode_1002

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1002.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1002.java
@@ -16,6 +16,7 @@ public class Solution1002 {
         String[] words = {"bella", "label", "roller"};
 
         System.out.println(obj.commonChars(words));
+        System.out.println(obj.commonChars2(words));
     }
 
     public List<String> commonChars(String[] words) {
@@ -34,14 +35,43 @@ public class Solution1002 {
             }
 
             for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-                map.put(entry.getKey(), Math.min(entry.getValue(), temp.getOrDefault(entry.getKey(),0)));
+                map.put(entry.getKey(), Math.min(entry.getValue(), temp.getOrDefault(entry.getKey(), 0)));
             }
         }
 
         for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-            if (entry.getValue()>0){
-                for (int i=0;i<entry.getValue();i++){
+            if (entry.getValue() > 0) {
+                for (int i = 0; i < entry.getValue(); i++) {
                     ans.add(entry.getKey().toString());
+                }
+            }
+        }
+        return ans;
+    }
+
+    public List<String> commonChars2(String[] words) {
+        List<String> ans = new ArrayList<>();
+        int[] res = new int[26];
+        for (char ch : words[0].toCharArray()) {
+            res[ch - 'a']++;
+        }
+
+        for (int i = 1; i < words.length; i++) {
+            int[] temp = new int[26];
+            for (char ch : words[i].toCharArray()) {
+                temp[ch - 'a']++;
+            }
+
+            for (int j = 0; j < 26; j++) {
+                res[j] = Math.min(res[j], temp[j]);
+            }
+        }
+
+        for (int i=0;i<26;i++){
+            if (res[i]>0){
+                for (int j=0;j<res[i];j++){
+                    ans.add(String.valueOf((char)(i+'a')));
+//                    ans.add((char)(i+'a')+"");
                 }
             }
         }
