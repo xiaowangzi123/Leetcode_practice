32f48d686436d592c77b2fecde16649f5b09694f
xiaowangzi123 <1103694415@qq.com>
2021-10-13 18:49:23 +0800

    LeetCode_1624

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1624.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1624.java
@@ -11,41 +11,26 @@ import java.util.Map;
 public class Solution1624 {
     public static void main(String[] args) {
         Solution1624 obj = new Solution1624();
-        int[] nums = {1, 2, 3, 1, 1, 3};
-        System.out.println(obj.numIdenticalPairs(nums));
-        System.out.println(obj.numIdenticalPairs2(nums));
+        System.out.println(obj.maxLengthBetweenEqualCharacters("abca"));
+        System.out.println(obj.maxLengthBetweenEqualCharacters("cbzxy"));
+        
+        System.out.println("-------------");
+        String st = "abcdefa";
+        System.out.println(st.indexOf("a"));
+        System.out.println(st.indexOf("g"));
+        System.out.println(st.lastIndexOf("a"));
     }
 
 
-    public int numIdenticalPairs(int[] nums) {
-        int count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
-                    count++;
-                }
+    public int maxLengthBetweenEqualCharacters(String s) {
+        int max = -1;
+        for (char ch : s.toCharArray()) {
+            int index = s.indexOf(ch), index2 = s.lastIndexOf(ch);
+            if (index2 != -1) {
+                max = Math.max(max, index2 - index - 1);
             }
         }
 
-        return count;
-    }
-
-    /**
-     * 统计每个元素出现的次数k
-     * 此元素贡献k(k-1)/2对数量
-     */
-
-    public int numIdenticalPairs2(int[] nums) {
-        int count = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            int temp = entry.getValue();
-            count += (temp - 1) * temp / 2;
-        }
-
-        return count;
+        return max;
     }
 }
