b252ebec86d527dec453c613253a7d1c1f75cf03
xiaowangzi123 <1103694415@qq.com>
2021-10-13 11:50:11 +0800

    LeetCode_1365

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1394.java
@@ -0,0 +1,34 @@
+package com.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2021/10/13
+ * @description :找出数组中的幸运数
+ */
+public class Solution1394 {
+    public static void main(String[] args) {
+        Solution1394 obj = new Solution1394();
+//        int[] arr = {2, 2, 3, 4};
+        int[] arr = {2, 2, 2, 3, 3};
+        System.out.println(obj.findLucky(arr));
+
+
+        System.out.println("-------------------------");
+    }
+
+    public int findLucky(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getKey().equals(entry.getValue())) {
+                return entry.getKey();
+            }
+        }
+        return -1;
+    }
+}
