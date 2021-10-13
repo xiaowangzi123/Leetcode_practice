0ff89496ddc7c99dd0159820e8d19d0f137263c2
xiaowangzi123 <1103694415@qq.com>
2021-10-13 15:50:25 +0800

    Leetcode1399修改

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1399.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1399.java
@@ -11,19 +11,31 @@ import java.util.Map;
 public class Solution1399 {
     public static void main(String[] args) {
         Solution1399 obj = new Solution1399();
-        System.out.println(obj.countLargestGroup(2));
+//        System.out.println(obj.countLargestGroup(2));
         System.out.println(obj.countLargestGroup(13));
         System.out.println(obj.countLargestGroup(15));
 
         System.out.println("-------------------------");
+        
+        System.out.println(10/10);
+
+        System.out.println(obj.getSums(10));
+        System.out.println(obj.getSums2(10));
+        System.out.println(obj.getSums(78));
+        System.out.println(obj.getSums2(78));
+
+
     }
 
     public int countLargestGroup(int n) {
         int max = 0, count = 0;
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 1; i <= n; i++) {
-            //错误，需要修改TODO
-            int temp = i % 9;
+            int temp = 0, k = i;
+            while (k > 0) {
+                temp += k % 10;
+                k /= 10;
+            }
             map.put(temp, map.getOrDefault(temp, 0) + 1);
             max = Math.max(max, map.get(temp));
         }
@@ -37,4 +49,16 @@ public class Solution1399 {
     }
 
 
+    public int getSums(int n) {
+        int key = 0;
+        while (n > 0) {
+            key += n % 10;
+            n /= 10;
+        }
+        return key;
+    }
+
+    public int getSums2(int n) {
+        return n % 9;
+    }
 }
