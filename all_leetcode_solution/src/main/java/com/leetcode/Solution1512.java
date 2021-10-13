66aa3b7dc596bdaaab8b98cb938f19c34962bd71
xiaowangzi123 <1103694415@qq.com>
2021-10-13 18:02:57 +0800

    LeetCode_1512

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1512.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1512.java
@@ -1,6 +1,8 @@
 package com.leetcode;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -13,6 +15,7 @@ public class Solution1512 {
         Solution1512 obj = new Solution1512();
         int[] nums = {1, 2, 3, 1, 1, 3};
         System.out.println(obj.numIdenticalPairs(nums));
+        System.out.println(obj.numIdenticalPairs2(nums));
     }
 
 
@@ -24,6 +27,25 @@ public class Solution1512 {
                     count++;
                 }
             }
+        }
+
+        return count;
+    }
+
+    /**
+     * 统计每个元素出现的次数k
+     * 此元素贡献k(k-1)/2对数量
+     */
+
+    public int numIdenticalPairs2(int[] nums) {
+        int count = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int temp = entry.getValue();
+            count += (temp - 1) * temp / 2;
         }
 
         return count;
