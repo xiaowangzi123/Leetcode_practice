6f6bef1df0d7a835e1438c903de0a15df6bf0d65
xiaowangzi123 <1103694415@qq.com>
2021-10-13 11:40:48 +0800

    LeetCode_1394.md

--- all_leetcode_solution/src/main/java/com/leetcode/array/Solution1365.java
+++ all_leetcode_solution/src/main/java/com/leetcode/array/Solution1365.java
@@ -5,10 +5,9 @@ import java.util.Arrays;
 /**
  * @author ：wyq
  * @date ：Created in 2020/11/19
- * @description：
+ * @description :有多少小于当前数字的数字
  */
 public class Solution1365 {
-
     public static void main(String[] args) {
         Solution1365 obj = new Solution1365();
         int[] arr = {8, 1, 2, 2, 3};
