f78c3c31b7a369d77d0b2d0f9b9867f89f9cad5f
xiaowangzi123 <1103694415@qq.com>
2021-10-14 23:34:53 +0800

    LeetCode_1876

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1876.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1876.java
@@ -12,13 +12,26 @@ public class Solution1876 {
     public static void main(String[] args) {
         Solution1876 obj = new Solution1876();
         System.out.println(obj.countGoodSubstrings("xyzzaz"));
+        System.out.println(obj.countGoodSubstrings("aababcabc"));
+        System.out.println(obj.countGoodSubstrings2("aababcabc"));
         System.out.println("-----------------------------");
     }
 
     public int countGoodSubstrings(String s) {
-        int count =0;
-        for (int i=0;i<s.length()-2;i++){
-            if (s.charAt(i)!=s.charAt(i+1) &&s.charAt(i+1)!=s.charAt(i+2)&&s.charAt(i)!=s.charAt(i+2)){
+        int count = 0;
+        for (int i = 0; i < s.length() - 2; i++) {
+            if (s.charAt(i) != s.charAt(i + 1) && s.charAt(i + 1) != s.charAt(i + 2) && s.charAt(i) != s.charAt(i + 2)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int countGoodSubstrings2(String s) {
+        int count = 0;
+        char[] ch = s.toCharArray();
+        for (int i = 0; i < s.length() - 2; i++) {
+            if (ch[i] != ch[i + 1] && ch[i] != ch[i + 2] && ch[i + 1] != ch[i + 2]) {
                 count++;
             }
         }
