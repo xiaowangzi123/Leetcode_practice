77eb8445e3701d13133aa6fea61c9f470dd3eb0b
xiaowangzi123 <1103694415@qq.com>
2021-10-15 00:09:03 +0800

    LeetCode_1876

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1790.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1790.java
@@ -12,68 +12,60 @@ import java.util.*;
 public class Solution1790 {
     public static void main(String[] args) {
         Solution1790 obj = new Solution1790();
-//        String s1 = "bank", s2 = "kanb";
-//        String s1 = "attack", s2 = "defend";
-        String s1 = "qgqeg", s2 = "gqgeq";
-//        String s1 = "abba", s2 = "baab";
-        System.out.println(obj.areAlmostEqual(s1, s2));
-        System.out.println(obj.areAlmostEqual2(s1, s2));
+        int[][] ranges = {{1, 2}, {3, 4}, {5, 6}};
+        int left = 2, right = 5;
+        System.out.println(obj.isCovered(ranges, left, right));
+        System.out.println(obj.isCovered2(ranges, left, right));
+        System.out.println(obj.isCovered3(ranges, left, right));
         System.out.println("-----------------------------");
 
-        List<String> l1 = new LinkedList<>();
-        List<String> l2 = new LinkedList<>();
-        l1.add("a");
-        l1.add("b");
-        l2.add("b");
-        l2.add("a");
-        System.out.println(l1.equals(l2));
-        System.out.println(l1.remove(l2.get(0)));
-        System.out.println(l1.remove(l2.get(1)));
-        System.out.println(l1.size());
     }
 
 
-    public boolean areAlmostEqual(String s1, String s2) {
-        if (s1.equals(s2)) {
-            return true;
-        }
-        Set<Character> set1 = new HashSet<>();
-        Set<Character> set2 = new HashSet<>();
-        for (int i = 0; i < s1.length(); i++) {
-            if (s1.charAt(i) != s2.charAt(i)) {
-                if (!set1.add(s1.charAt(i)) || !set2.add(s2.charAt(i))) {
-                    return false;
+    public boolean isCovered(int[][] ranges, int left, int right) {
+        for (int i = left; i <= right; i++) {
+            boolean flag = false;
+            for (int[] range : ranges) {
+                if ((range[0] <= i && range[1] >= i)) {
+                    flag = true;
+                    continue;
                 }
-                set1.add(s1.charAt(i));
-                set2.add(s2.charAt(i));
             }
-        }
-        if (set1.size() != 2 || set2.size() != 2) {
-            return false;
-        }
-        return set1.equals(set2);
-    }
-
-    public boolean areAlmostEqual2(String s1, String s2) {
-        if (s1.equals(s2)) {
-            return true;
-        }
-        List<Character> l1 = new ArrayList<>();
-        List<Character> l2 = new ArrayList<>();
-        for (int i = 0; i < s1.length(); i++) {
-            if (s1.charAt(i) != s2.charAt(i)) {
-                l1.add(s1.charAt(i));
-                l2.add(s2.charAt(i));
-            }
-        }
-        if (l1.size() != 2 || l2.size() != 2) {
-            return false;
-        }
-        for (int i = 0; i < l1.size(); i++) {
-            if (!l2.remove(l1.get(i))) {
+            if (!flag) {
                 return false;
             }
         }
-        return l2.size() == 0;
+        return true;
     }
+
+
+    /**
+     * 数量50，可以设置初始数组
+     */
+    public boolean isCovered2(int[][] ranges, int left, int right) {
+        boolean[] flags = new boolean[51];
+        for (int[] rang : ranges) {
+            for (int i = rang[0]; i <= rang[1]; i++) {
+                flags[i] = true;
+            }
+        }
+        for (int i = left; i <= right; i++) {
+            if (!flags[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public boolean isCovered3(int[][] ranges, int left, int right) {
+        Arrays.sort(ranges, (x, y) -> x[0] - y[0]);
+        for (int[] r : ranges) {
+            if (r[0] <= left && left <= r[1]) {
+                left = r[1]+1;
+            }
+        }
+        return left > right;
+    }
+
 }
