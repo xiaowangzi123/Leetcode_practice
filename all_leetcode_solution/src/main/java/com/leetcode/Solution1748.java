8c138d93e9b9466838a31f23dfadd048e7b23ab5
xiaowangzi123 <1103694415@qq.com>
2021-10-14 14:13:47 +0800

    LeetCode_1748

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1748.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1748.java
@@ -12,45 +12,22 @@ public class Solution1748 {
     public static void main(String[] args) {
         Solution1748 obj = new Solution1748();
 
-        int lowLimit = 1, highLimit = 10;
-        System.out.println(obj.countBalls(lowLimit, highLimit));
-        System.out.println(obj.countBalls2(lowLimit, highLimit));
+        int[] nums = {1, 2, 3, 2};
+        System.out.println(obj.sumOfUnique(nums));
     }
 
 
-    public int countBalls(int lowLimit, int highLimit) {
-        int max = 0;
+    public int sumOfUnique(int[] nums) {
+        int sum = 0;
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i = lowLimit; i <= highLimit; i++) {
-            int temp = 0, k = i;
-            while (k > 0) {
-                temp += k % 10;
-                k /= 10;
-            }
-            map.put(temp, map.getOrDefault(temp, 0) + 1);
-            max = Math.max(max, map.get(temp));
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        return max;
-    }
-
-    /**
-     * 1 <= lowLimit <= highLimit <= 10^5
-     * 各位数相加最大值为99999的位数相加45,
-     * 初始化数组46
-     */
-    public int countBalls2(int lowLimit, int highLimit) {
-        int max = 0;
-        int[] count = new int[46];
-        for (int i = lowLimit; i <= highLimit; i++) {
-            int temp = 0, k = i;
-            while (k > 0) {
-                temp += k % 10;
-                k /= 10;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                sum += entry.getKey();
             }
-            count[temp]++;
-            max = Math.max(count[temp], max);
         }
-
-        return max;
+        return sum;
     }
 }
