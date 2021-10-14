2110e7de4175655776e759157497ce4c71bd7a9a
xiaowangzi123 <1103694415@qq.com>
2021-10-14 22:21:02 +0800

    LeetCode_1790正确答案

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1790.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1790.java
@@ -1,9 +1,8 @@
 package com.leetcode;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import sun.awt.dnd.SunDragSourceContextPeer;
+
+import java.util.*;
 
 /**
  * @author :wyq
@@ -18,17 +17,19 @@ public class Solution1790 {
         String s1 = "qgqeg", s2 = "gqgeq";
 //        String s1 = "abba", s2 = "baab";
         System.out.println(obj.areAlmostEqual(s1, s2));
+        System.out.println(obj.areAlmostEqual2(s1, s2));
         System.out.println("-----------------------------");
 
-        Set<Character> set1 = new HashSet<>();
-        Set<Character> set2 = new HashSet<>();
-        set1.add('b');
-        set1.add('a');
-        set2.add('a');
-        set2.add('b');
-        System.out.println(set1.equals(set2));
-        System.out.println("----------------");
-        System.out.println(set1.add('c'));
+        List<String> l1 = new LinkedList<>();
+        List<String> l2 = new LinkedList<>();
+        l1.add("a");
+        l1.add("b");
+        l2.add("b");
+        l2.add("a");
+        System.out.println(l1.equals(l2));
+        System.out.println(l1.remove(l2.get(0)));
+        System.out.println(l1.remove(l2.get(1)));
+        System.out.println(l1.size());
     }
 
 
@@ -40,7 +41,7 @@ public class Solution1790 {
         Set<Character> set2 = new HashSet<>();
         for (int i = 0; i < s1.length(); i++) {
             if (s1.charAt(i) != s2.charAt(i)) {
-                if (!set1.add(s1.charAt(i)) || !set2.add(s2.charAt(i))){
+                if (!set1.add(s1.charAt(i)) || !set2.add(s2.charAt(i))) {
                     return false;
                 }
                 set1.add(s1.charAt(i));
@@ -51,5 +52,28 @@ public class Solution1790 {
             return false;
         }
         return set1.equals(set2);
+    }
+
+    public boolean areAlmostEqual2(String s1, String s2) {
+        if (s1.equals(s2)) {
+            return true;
+        }
+        List<Character> l1 = new ArrayList<>();
+        List<Character> l2 = new ArrayList<>();
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                l1.add(s1.charAt(i));
+                l2.add(s2.charAt(i));
+            }
+        }
+        if (l1.size() != 2 || l2.size() != 2) {
+            return false;
+        }
+        for (int i = 0; i < l1.size(); i++) {
+            if (!l2.remove(l1.get(i))) {
+                return false;
+            }
+        }
+        return l2.size() == 0;
     }
 }
