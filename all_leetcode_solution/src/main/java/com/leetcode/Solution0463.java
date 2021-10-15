47116a230a07fea4ca82a8705e8ab17da7b7be93
xiaowangzi123 <1103694415@qq.com>
2021-10-15 22:32:43 +0800

    LeetCode_0463.md

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0463.java
@@ -0,0 +1,36 @@
+package com.leetcode;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2021/10/15
+ * @description :岛屿的周长
+ */
+public class Solution0463 {
+    public static void main(String[] args) {
+        Solution0463 obj = new Solution0463();
+        int[][] grid = {{0, 1, 0, 0}, {1, 1, 1, 0}, {0, 1, 0, 0}, {1, 1, 0, 0}};
+        System.out.println(obj.islandPerimeter(grid));
+
+    }
+
+    public int islandPerimeter(int[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    count += 4;
+                    if (i > 0 && grid[i - 1][j] == 1) {
+                        count -= 2;
+                    }
+                    if (j > 0 && grid[i][j - 1] == 1) {
+                        count -= 2;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
