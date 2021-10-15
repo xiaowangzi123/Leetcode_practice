139f92bc296f88c010736bb1e1bf490c7a7131a4
xiaowangzi123 <1103694415@qq.com>
2021-10-15 15:01:52 +0800

    LeetCode_1897

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1897.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1897.java
@@ -14,7 +14,8 @@ public class Solution1897 {
         String[] words = {"abc", "aabc", "bc"};
 
         System.out.println(obj.makeEqual(words));
-        
+        System.out.println(obj.makeEqual2(words));
+
     }
 
     public boolean makeEqual(String[] words) {
@@ -25,12 +26,27 @@ public class Solution1897 {
                 map.put(ch, map.getOrDefault(ch, 0) + 1);
             }
         }
-        for (Map.Entry<Character,Integer> entry:map.entrySet()){
-            if (entry.getValue()%n!=0){
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            if (entry.getValue() % n != 0) {
                 return false;
             }
         }
 
+        return true;
+    }
+
+    public boolean makeEqual2(String[] words) {
+        int[] count = new int[26];
+        for (String word : words) {
+            for (char ch : word.toCharArray()) {
+                count[ch-'a']++;
+            }
+        }
+        for (int i=0;i<26;i++){
+            if (count[i]%words.length!=0){
+                return false;
+            }
+        }
         return true;
     }
 }
