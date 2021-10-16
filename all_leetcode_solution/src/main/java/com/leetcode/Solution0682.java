977375270a51bff837af27eab94e74db41a002e8
xiaowangzi123 <1103694415@qq.com>
2021-10-16 23:27:48 +0800

    LeetCode_0682

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0682.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0682.java
@@ -1,57 +1,46 @@
 package com.leetcode;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * @author :wyq
  * @date ：Created in 2021/10/10
- * @description :最长连续递增序列
+ * @description :棒球比赛
  */
 public class Solution0682 {
     public static void main(String[] args) {
         Solution0682 obj = new Solution0682();
-//        int[] arr = {1, 3, 5, 4, 7};
-//        int[] arr = {2, 2, 2, 2, 2};
-        int[] arr = {1,3,2};
-        System.out.println(obj.findLengthOfLCIS(arr));
-        System.out.println(obj.findLengthOfLCIS2(arr));
-        System.out.println(obj.findLengthOfLCIS3(arr));
+        String[] st = {"5", "2", "C", "D", "+"};
+        System.out.println(obj.calPoints(st));
     }
 
-    //改进参考方法3
-    public int findLengthOfLCIS(int[] nums) {
-        int count = 0, start = 0, index = 0, n = nums.length;
-        while (index < n) {
-            if (index > 0 && nums[index - 1] < nums[index]) {
-                index++;
-                count = Math.max(count, index - start);
-            } else {
-                index++;
-                start = index;
+    public int calPoints(String[] ops) {
+        int sum = 0;
+        Deque<Integer> stack = new LinkedList<>();
+        for (String st : ops) {
+            switch (st) {
+                case "C":
+                    stack.pop();
+                    break;
+                case "D":
+                    int n1 = stack.peekFirst();
+                    stack.push(n1 * 2);
+                    break;
+                case "+":
+                    int t1 = stack.pop();
+                    int t2 = t1 + stack.peekFirst();
+                    stack.push(t1);
+                    stack.push(t2);
+                    break;
+                default:
+                    stack.push(Integer.valueOf(st));
             }
         }
-        return count + 1;
-    }
 
-    public int findLengthOfLCIS2(int[] nums) {
-        int count = 0, index = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (i > 0 && nums[i - 1] >= nums[i]) {
-                index = i;
-            }
-            count = Math.max(count, i - index + 1);
+        for (int s : stack) {
+            sum += s;
         }
-        return count;
-    }
-
-    public int findLengthOfLCIS3(int[] nums) {
-        int count = 0, start = 0, index = 0, n = nums.length;
-        while (index < n) {
-            if (index > 0 && nums[index - 1] >= nums[index]) {
-                start = index;
-            }
-            count = Math.max(count, index - start + 1);
-            index++;
-
-        }
-        return count;
+        return sum;
     }
 }
