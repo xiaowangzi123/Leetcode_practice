9d8e510c35ab3c2b18e68b484c78de9a0107aa6e
xiaowangzi123 <1103694415@qq.com>
2021-10-16 09:34:18 +0800

    LeetCode_0566.md

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0566.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0566.java
@@ -5,11 +5,11 @@ import java.util.Arrays;
 /**
  * @author :wyq
  * @date ：Created in 2021/10/16
- * @description :数组拆分
+ * @description :重塑矩阵
  */
-public class Solution0561 {
+public class Solution0566 {
     public static void main(String[] args) {
-        Solution0561 obj = new Solution0561();
+        Solution0566 obj = new Solution0566();
         int[] arr = {1, 4, 3, 2};
         System.out.println(obj.arrayPairSum(arr));
     }
