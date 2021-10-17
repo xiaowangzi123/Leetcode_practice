72e72d9d55417c5314c5fd42bcb21d7a2fb4a4e8
xiaowangzi123 <1103694415@qq.com>
2021-10-17 20:59:08 +0800

    LeetCode_0747

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0747.java
@@ -0,0 +1,33 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2021/10/17
+ * @description :
+ */
+public class Solution0747 {
+    public static void main(String[] args) {
+        Solution0747 obj = new Solution0747();
+//        int[] nums = {3, 6, 1, 0};
+        int[] nums = {1, 2, 3, 4};
+        System.out.println(obj.dominantIndex(nums));
+    }
+
+    public int dominantIndex(int[] nums) {
+        int ans = -1, max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (max < nums[i]) {
+                max = nums[i];
+                ans = i;
+            }
+        }
+        for (int num : nums) {
+            if (num != max && num * 2 > max) {
+                return -1;
+            }
+        }
+        return ans;
+    }
+}
