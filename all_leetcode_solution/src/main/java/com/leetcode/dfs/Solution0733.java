09ce3ebb931221ce1699cfbbbcde387c5b96042f
xiaowangzi123 <1103694415@qq.com>
2021-10-17 17:35:19 +0800

    LeetCode_0733

---
+++ all_leetcode_solution/src/main/java/com/leetcode/dfs/Solution0733.java
@@ -0,0 +1,44 @@
+package com.leetcode.dfs;
+
+import java.util.Arrays;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2021/10/17
+ * @description :图像渲染
+ */
+public class Solution0733 {
+    public static void main(String[] args) {
+        Solution0733 obj = new Solution0733();
+        int[][] image = {{1, 1, 1}, {1, 1, 0}, {1, 0, 1}};
+        int sr = 1, sc = 1, newColor = 2;
+
+        System.out.println(Arrays.deepToString(obj.floodFill(image, sr, sc, newColor)));
+
+    }
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        //初始像素值
+        int currColor = image[sr][sc];
+        dfs(image, sr, sc, currColor, newColor);
+
+        return image;
+    }
+
+    public void dfs(int[][] image, int x, int y, int color, int newColor) {
+        //上下左右四个方向
+        int[] dx = {1, 0, 0, -1};
+        int[] dy = {0, 1, -1, 0};
+        if (image[x][y] == color) {
+            image[x][y] = newColor;
+            for (int i = 0; i < 4; i++) {
+                int mx = x + dx[i], my = y + dy[i];
+                if (mx >= 0 && mx < image.length && my >= 0 && my < image[0].length) {
+                    dfs(image, mx, my, color, newColor);
+                }
+            }
+        }
+    }
+
+
+}
