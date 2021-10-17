0fc887e2cd765b1e74b262cea27a772f4df0ece4
xiaowangzi123 <1103694415@qq.com>
2021-10-17 19:20:07 +0800

    LeetCode_0746.md

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0744.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0744.java
@@ -5,22 +5,22 @@ import java.util.Arrays;
 /**
  * @author :wyq
  * @date ：Created in 2021/10/17
- * @description :
+ * @description :寻找比目标字母大的最小字母
  */
 public class Solution0744 {
     public static void main(String[] args) {
         Solution0744 obj = new Solution0744();
         char[] letters = {'c', 'f', 'j'};
-        char target = 'd';
+        char target = 'g';
         System.out.println(obj.nextGreatestLetter(letters, target));
 
-        System.out.println(target == letters[0]);
+        System.out.println(target > letters[0]);
     }
 
     public char nextGreatestLetter(char[] letters, char target) {
         for (int i = 0; i < letters.length; i++) {
-            if (target == letters[i] && i != letters.length - 1) {
-                return letters[i+1];
+            if (target < letters[i]) {
+                return letters[i];
             }
         }
 
