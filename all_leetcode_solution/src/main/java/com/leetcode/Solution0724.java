285bc751e4b6b6558326f1cf4cb1aaa7f059f3f1
xiaowangzi123 <1103694415@qq.com>
2021-10-17 16:51:53 +0800

    LeetCode_0724修改，初始化位置

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0724.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0724.java
@@ -1,5 +1,7 @@
 package com.leetcode;
 
+import java.util.Arrays;
+
 /**
  * @author :wyq
  * @date ：Created in 2021/10/17
@@ -10,6 +12,7 @@ public class Solution0724 {
         Solution0724 obj = new Solution0724();
         int[] arr = {1, 7, 3, 6, 5, 6};
         System.out.println(obj.pivotIndex(arr));
+        System.out.println(obj.pivotIndex2(arr));
     }
 
     public int pivotIndex(int[] nums) {
@@ -21,10 +24,24 @@ public class Solution0724 {
             for (int k = i + 1; k < nums.length; k++) {
                 sum2 += nums[k];
             }
-            if (sum1==sum2){
+            if (sum1 == sum2) {
                 return i;
             }
         }
+        return -1;
+    }
+
+
+    public int pivotIndex2(int[] nums) {
+        int sum = Arrays.stream(nums).sum();
+        int sum1 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (sum1 * 2 + nums[i] == sum) {
+                return i;
+            }
+            sum1 += nums[i];
+        }
+
         return -1;
     }
 }
