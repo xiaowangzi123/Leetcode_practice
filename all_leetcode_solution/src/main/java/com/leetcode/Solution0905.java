6a7e77ab48231805dfbe575a61d69ca843ed76e9
xiaowangzi123 <1103694415@qq.com>
2021-10-18 17:58:25 +0800

    LeetCode_0905

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0905.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0905.java
@@ -10,24 +10,25 @@ import java.util.Arrays;
 public class Solution0905 {
     public static void main(String[] args) {
         Solution0905 obj = new Solution0905();
-        int[] arr = {3, 1, 2, 4};
+//        int[] arr = {3, 1, 2, 4};
+        int[] arr = {0, 1};
         System.out.println(Arrays.toString(obj.sortArrayByParity(arr)));
     }
 
     public int[] sortArrayByParity(int[] nums) {
-        int start = 0, end = nums.length-1;
+        int start = 0, end = nums.length - 1;
         while (start < end) {
-            if (nums[start] % 2 == 0) {
+            while (start < end && nums[start] % 2 == 0) {
                 start++;
             }
-            if (nums[end] % 2 != 0) {
+            while (start < end && nums[end] % 2 != 0) {
                 end--;
             }
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-            start++;
-            end--;
+            if (nums[start] % 2 != 0 &&nums[end] % 2 == 0){
+                int temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+            }
         }
         return nums;
     }
