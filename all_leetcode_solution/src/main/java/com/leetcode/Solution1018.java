dbf9f56004f04a0b3aa361e240ccf8a84e3c7614
xiaowangzi123 <1103694415@qq.com>
2021-10-19 19:03:50 +0800

    LeetCode_1030

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1018.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1018.java
@@ -12,21 +12,33 @@ import java.util.List;
 public class Solution1018 {
     public static void main(String[] args) {
         Solution1018 obj = new Solution1018();
-        int[] arr = {0, 1, 1};
-        System.out.println(obj.prefixesDivBy5(arr));
+//        int[] arr = {0, 1, 1};
+        int[] arr = {1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1};
+        System.out.println(obj.prefixesDivBy52(arr));
     }
 
+    //java.lang.NumberFormatException
     public List<Boolean> prefixesDivBy5(int[] nums) {
         List<Boolean> ans = new ArrayList<>();
         String st = "";
-        for (int i : nums) {
-            st += i;
+        for (int i = 0; i < nums.length; i++) {
+            st += nums[i];
             int temp = Integer.valueOf(st, 2);
             if (temp % 5 == 0) {
                 ans.add(i, true);
-            }else {
-                ans.add(i,false);
+            } else {
+                ans.add(i, false);
             }
+        }
+        return ans;
+    }
+
+    public List<Boolean> prefixesDivBy52(int[] nums) {
+        List<Boolean> ans = new ArrayList<>();
+        int remainder = 0;
+        for (int i = 0; i < nums.length; i++) {
+            remainder = (remainder * 2 + nums[i]) % 5;
+            ans.add(remainder == 0);
         }
         return ans;
     }
