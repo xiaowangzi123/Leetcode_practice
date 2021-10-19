85cddb7dba37b427fa9a83041ead8464f9a02ff6
xiaowangzi123 <1103694415@qq.com>
2021-10-19 11:44:08 +0800

    LeetCode_1005两次排序

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1005.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1005.java
@@ -10,8 +10,10 @@ import java.util.Arrays;
 public class Solution1005 {
     public static void main(String[] args) {
         Solution1005 obj = new Solution1005();
-        int[] nums = {4, 2, 3};
-        System.out.println(obj.largestSumAfterKNegations(nums, 1));
+//        int[] nums = {4, 2, 3};
+//        System.out.println(obj.largestSumAfterKNegations(nums, 1));
+        int[] nums = {2, -3, -1, 5, -4};
+        System.out.println(obj.largestSumAfterKNegations2(nums, 2));
     }
 
     public int largestSumAfterKNegations(int[] nums, int k) {
@@ -25,6 +27,25 @@ public class Solution1005 {
             }
             k--;
         }
+        for (int num : nums) {
+            sum += num;
+        }
+        return sum;
+    }
+
+    public int largestSumAfterKNegations2(int[] nums, int k) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < 0 && k >0) {
+                nums[i] = -nums[i];
+                k--;
+            }
+        }
+        Arrays.sort(nums);
+        if (k % 2 == 1) {
+            nums[0] = -nums[0];
+        }
+        int sum = 0;
         for (int num : nums) {
             sum += num;
         }
