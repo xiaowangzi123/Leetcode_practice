10e2dc29475f6e4efbbb4b48eaded8ebe05bb26a
xiaowangzi123 <1103694415@qq.com>
2021-10-20 20:21:53 +0800

    LeetCode_0908

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0908.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0908.java
@@ -12,14 +12,21 @@ public class Solution0908 {
         Solution0908 obj = new Solution0908();
         int[] arr = {1, 3, 6};
         System.out.println(obj.smallestRangeI(arr, 3));
+        System.out.println(obj.smallestRangeI2(arr, 3));
     }
 
     public int smallestRangeI(int[] nums, int k) {
         int n = nums.length;
-        if (n < 2) {
-            return 0;
-        }
         Arrays.sort(nums);
-        return (nums[n - 1] - nums[0]) > k * 2?nums[n - 1] - nums[0]-k*2:0;
+        return (nums[n - 1] - nums[0]) > k * 2 ? nums[n - 1] - nums[0] - k * 2 : 0;
+    }
+
+    public int smallestRangeI2(int[] nums, int k) {
+        int min = nums[0], max = nums[0];
+        for (int x : nums) {
+            min = Math.min(min, x);
+            max = Math.max(max, x);
+        }
+        return Math.max(0, max - min - 2 * k);
     }
 }
