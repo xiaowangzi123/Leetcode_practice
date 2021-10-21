f6594f3c65b494a177bec0bd94df0e1296156ae0
xiaowangzi123 <1103694415@qq.com>
2021-10-21 14:23:39 +0800

    LeetCode_1200

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1200.java
@@ -0,0 +1,38 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2021/10/21
+ * @description : 最小绝对差
+ */
+public class Solution1200 {
+    public static void main(String[] args) {
+        Solution1200 obj = new Solution1200();
+//        int[] arr = {4, 2, 1, 3};
+        int[] arr = {40, 11, 26, 27, -20};
+        System.out.println(obj.minimumAbsDifference(arr));
+    }
+
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int min = Integer.MAX_VALUE;
+        Arrays.sort(arr);
+        for (int i = 1; i < arr.length; i++) {
+            List<Integer> list = new ArrayList<>();
+            int temp = arr[i] - arr[i - 1];
+            if (temp < min) {
+                min = temp;
+                list.add(arr[i - 1]);
+                list.add(arr[i]);
+            }
+            if (list.size() > 0) {
+                ans.add(list);
+            }
+        }
+        return ans;
+    }
+}
