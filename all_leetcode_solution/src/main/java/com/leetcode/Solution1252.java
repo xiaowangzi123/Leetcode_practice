f9c041c89c93691a9f4403b0d09fbdc98e3c56b6
xiaowangzi123 <1103694415@qq.com>
2021-10-21 19:39:15 +0800

    LeetCode_1252

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1252.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1252.java
@@ -11,6 +11,7 @@ public class Solution1252 {
         int[][] indices = {{0, 1}, {1, 1}};
         System.out.println(obj.oddCells(2, 3, indices));
         System.out.println(obj.oddCells2(2, 3, indices));
+        System.out.println(obj.oddCells3(2, 3, indices));
     }
 
     public int oddCells(int m, int n, int[][] indices) {
@@ -54,6 +55,26 @@ public class Solution1252 {
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (arr[i][j]  == 1) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public int oddCells3(int m, int n, int[][] indices) {
+        int count = 0;
+        int[] rows = new int[m], clos=new int[n];
+        for (int[] ind : indices) {
+            int a = ind[0];
+            int b = ind[1];
+            rows[a]++;
+            clos[b]++;
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((rows[i]+clos[j])%2==1){
                     count++;
                 }
             }
