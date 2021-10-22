aef57a4fd8c5913953658fbc89b3b027b47abc8a
xiaowangzi123 <1103694415@qq.com>
2021-10-22 21:07:53 +0800

    LeetCode_1304

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1304.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1304.java
@@ -10,9 +10,14 @@ import java.util.Arrays;
 public class Solution1304 {
     public static void main(String[] args) {
         Solution1304 obj = new Solution1304();
+        System.out.println(Arrays.toString(obj.sumZero(2)));
+        System.out.println(Arrays.toString(obj.sumZero2(2)));
+
         System.out.println(Arrays.toString(obj.sumZero(5)));
+        System.out.println(Arrays.toString(obj.sumZero2(5)));
     }
 
+    //n为2时有误
     public int[] sumZero(int n) {
         int[] ans = new int[n];
         int sum = 0;
@@ -21,6 +26,17 @@ public class Solution1304 {
             sum += i;
         }
         ans[n - 1] = -sum;
+        return ans;
+    }
+
+    public int[] sumZero2(int n) {
+        int[] ans = new int[n];
+        int sum = 0;
+        for (int i = 1; i < n; i++) {
+            ans[i] = i;
+            sum += i;
+        }
+        ans[0] = -sum;
         return ans;
     }
 }
