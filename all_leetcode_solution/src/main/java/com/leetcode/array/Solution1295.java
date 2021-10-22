41a0ada650b0c3dee0353ae2a8ca70468dc8cf94
xiaowangzi123 <1103694415@qq.com>
2021-10-22 17:57:07 +0800

    LeetCode_0299

--- all_leetcode_solution/src/main/java/com/leetcode/array/Solution1295.java
+++ all_leetcode_solution/src/main/java/com/leetcode/array/Solution1295.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 /**
  * @author ：wyq
  * @date ：Created in 2020/11/19
- * @description：
+ * @description :统计位数为偶数的数字
  */
 public class Solution1295 {
 
@@ -13,15 +13,31 @@ public class Solution1295 {
         Solution1295 obj = new Solution1295();
         int[] arr = {12, 345, 2, 6, 7896};
         System.out.println(obj.findNumbers(arr));
+        System.out.println(obj.findNumbers2(arr));
     }
 
 
     public int findNumbers(int[] nums) {
         int count = 0;
         int[] str = new int[nums.length];
-        for (int i=0; i<nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             str[i] = String.valueOf(nums[i]).length();
-            if (str[i]%2==0){
+            if (str[i] % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int findNumbers2(int[] nums) {
+        int count = 0;
+
+        for (int num : nums) {
+            if (10 <= num && num < 100) {
+                count++;
+            } else if (1000 <= num && num < 10000) {
+                count++;
+            } else if (num == 100000) {
                 count++;
             }
         }
