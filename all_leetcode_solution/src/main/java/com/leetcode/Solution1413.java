e9a9f643ce83c2e70d539d81ae68956dd4d09812
xiaowangzi123 <1103694415@qq.com>
2021-10-24 16:39:17 +0800

    LeetCode_1413

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1413.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1413.java
@@ -8,16 +8,21 @@ package com.leetcode;
 public class Solution1413 {
     public static void main(String[] args) {
         Solution1413 obj = new Solution1413();
-        int[] arr = {-3, 2, -3, 4, 2};
+//        int[] arr = {-3, 2, -3, 4, 2};
+//        int[] arr = {1, 2};
+        int[] arr = {1, -2, -3};
         System.out.println(obj.minStartValue(arr));
     }
 
+    /**
+     * 所求值max和sum的和每次都要大于1
+     */
     public int minStartValue(int[] nums) {
         int sum = 0, max = Integer.MIN_VALUE;
         for (int num : nums) {
             sum += num;
             max = Math.max(max, 1 - sum);
         }
-        return max;
+        return max <= 0 ? 1 : max;
     }
 }
