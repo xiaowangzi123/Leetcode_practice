f0d760b2a552106e0a223595cc90da83a3a5cd23
xiaowangzi123 <1103694415@qq.com>
2021-10-27 19:27:41 +0800

    LeetCode_0796

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0796.java
@@ -0,0 +1,36 @@
+package com.leetcode;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2021/10/26
+ * @description : 旋转字符串
+ */
+public class Solution0796 {
+    public static void main(String[] args) {
+        Solution0796 obj = new Solution0796();
+        String s = "abcde", goal = "cdeab";
+        System.out.println(obj.rotateString(s, goal));
+    }
+
+    public boolean rotateString(String s, String goal) {
+        int n = s.length();
+        if (goal.length() != n) {
+            return false;
+        }
+        int k = 0;
+        boolean flag = true;
+        while (k < n) {
+            for (int i = 0; i < n; i++) {
+                if (s.charAt((i + k) % n) != goal.charAt(i)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag){
+                return true;
+            }
+            k++;
+        }
+        return false;
+    }
+}
