8a92ca0587e5209c4ff7a66a484d31d5e9e2d8a5
xiaowangzi123 <1103694415@qq.com>
2021-10-29 22:46:55 +0800

    LeetCode_1021

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1021.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1021.java
@@ -1,23 +1,72 @@
 package com.leetcode;
 
-import com.leetcode.bean.ListNode;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @author ：wyq
  * @date ：Created in 2020/11/20
- * @description：
+ * @description : 删除最外层的括号
  */
 public class Solution1021 {
     public static void main(String[] args) {
-        String st = "(()())(())";
-
         Solution1021 obj = new Solution1021();
-        System.out.println(obj.removeOuterParentheses(st));
-
+        String st = "(()())(())";
+        System.out.println(obj.removeOuterParentheses2(st));
+        System.out.println("---------------");
+        System.out.println(obj.removeOuterParentheses3("(()())(())"));
     }
 
-    public String removeOuterParentheses(String str) {
+    public String removeOuterParentheses(String s) {
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        for (char c : s.toCharArray()) {
+            if (c == ')') {
+                count--;
+            }
+            if (count >= 1) {
+                sb.append(c);
+            }
+            if (c == '(') {
+                count++;
+            }
+        }
+        return sb.toString();
+    }
 
-        return "";
+    public String removeOuterParentheses2(String s) {
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                count++;
+                if (count == 1) {
+                    continue;
+                }
+            } else {
+                count--;
+                if (count == 0) {
+                    continue;
+                }
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+
+    public String removeOuterParentheses3(String s) {
+        LinkedList<Integer> stack = new LinkedList<>();
+        StringBuilder ret = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.add(i);
+            } else {
+                int temp = stack.removeFirst();
+                if (stack.isEmpty()) {
+                    ret.append(s, temp + 1, i);
+                }
+            }
+        }
+        return ret.toString();
     }
 }
