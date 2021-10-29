88d7ef018d477fbb18147f0bc4a9839b439344eb
xiaowangzi123 <1103694415@qq.com>
2021-10-30 01:24:51 +0800

    LeetCode_0260

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0260.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0260.java
@@ -14,6 +14,7 @@ public class Solution0260 {
         int[] arr = {1, 2, 1, 3, 2, 5};
         System.out.println(Arrays.toString(obj.singleNumber(arr)));
         System.out.println(Arrays.toString(obj.singleNumber2(arr)));
+        System.out.println(Arrays.toString(obj.singleNumber3(arr)));
     }
 
     public int[] singleNumber(int[] nums) {
@@ -49,10 +50,12 @@ public class Solution0260 {
     }
 
     /**
-     * 1把所有的元素进行异或操作，最终得到一个异或值。因为是不同的两个数字，所以这个值必定不为 0
-     * 2.取异或值最后一个二进制位为 1 的数字作为 mask，如果是 1 则表示两个数字在这一位上不同。
-     * 3.通过与这个 mask 进行与操作，如果为 0 的分为一个数组，为 1 的分为另一个数组。
-     * 这样就把问题降低成了：“有一个数组每个数字都出现两次，有一个数字只出现了一次，求出该数字”。
+     * 1把所有的元素进行异或操作，最终得到一个异或值。因为是不同的两个数字a^b，所以这个值必定不为 0
+     * 2. 位运算x&-x 取出 x 的二进制表示中最低位那个 1
+     * 取异或值最后一个二进制位为 1 的数字作为 mask，如果是 1 则表示两个数字在这一位上不同。
+     * 3.通过与这个mask进行与操作，如果为0的分为一个数组，为 1 的分为另一个数组。
+     * 出现两次的元素一定被分在同一组
+     * a和b一定被分在不同的组
      * 对这两个子问题分别进行全异或就可以得到两个解。也就是最终的数组了。
      */
     public int[] singleNumber3(int[] nums) {
