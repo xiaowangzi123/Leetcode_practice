8205f1b20b60693739d011220257deb456794e45
xiaowangzi123 <1103694415@qq.com>
2021-10-30 22:03:18 +0800

    LeetCode_1154

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1154.java
@@ -0,0 +1,28 @@
+package com.leetcode;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2021/10/30
+ * @description : 一年中的第几天
+ */
+public class Solution1154 {
+    public static void main(String[] args) {
+        Solution1154 obj = new Solution1154();
+        String date = "2019-01-09";
+        System.out.println(obj.dayOfYear(date));
+    }
+
+    public int dayOfYear(String date) {
+        String[] d = date.split("-");
+        int count = 0, year = Integer.valueOf(d[0]), month = Integer.valueOf(d[1]), day = Integer.valueOf(d[2]);
+        int[] months = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if ((year % 400 == 0) || (year % 4 == 0 && year % 100 == 0)) {
+            months[1] = 29;
+        }
+        for (int i = 1; i < month; i++) {
+            count += months[i - 1];
+        }
+        count += day;
+        return count;
+    }
+}
