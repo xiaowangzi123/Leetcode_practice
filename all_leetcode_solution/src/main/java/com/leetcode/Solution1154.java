d4279da07be15ebb95f0dd7e9411d93532895e70
xiaowangzi123 <1103694415@qq.com>
2021-10-30 22:07:35 +0800

    LeetCode_1154

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1154.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1154.java
@@ -8,15 +8,19 @@ package com.leetcode;
 public class Solution1154 {
     public static void main(String[] args) {
         Solution1154 obj = new Solution1154();
-        String date = "2019-01-09";
+//        String date = "2019-01-09";
+        String date = "2004-03-01";
         System.out.println(obj.dayOfYear(date));
     }
 
+    /**
+     * 四百年一润；四年一润，百年不润；
+     */
     public int dayOfYear(String date) {
         String[] d = date.split("-");
         int count = 0, year = Integer.valueOf(d[0]), month = Integer.valueOf(d[1]), day = Integer.valueOf(d[2]);
         int[] months = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        if ((year % 400 == 0) || (year % 4 == 0 && year % 100 == 0)) {
+        if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
             months[1] = 29;
         }
         for (int i = 1; i < month; i++) {
