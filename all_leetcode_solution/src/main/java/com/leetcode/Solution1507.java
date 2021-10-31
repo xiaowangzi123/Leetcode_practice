fde1d4c508ad5ec8b201fd0796e6de47c0a58cd0
xiaowangzi123 <1103694415@qq.com>
2021-11-01 01:03:38 +0800

    LeetCode_1507

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1507.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1507.java
@@ -11,20 +11,22 @@ import java.util.Map;
 public class Solution1507 {
     public static void main(String[] args) {
         Solution1507 obj = new Solution1507();
-        String date = "20th Oct 2052";
+//        String date = "20th Oct 2052";
+        String date = "6th Jun 1933";
         System.out.println(obj.reformatDate(date));
     }
 
     public String reformatDate(String date) {
-        String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
-        Map<String, Integer> map = new HashMap<>();
+        String[][] months = {{"", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"},
+                {"", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"}};
+        Map<String, String> map = new HashMap<>();
         for (int i = 1; i <= 12; i++) {
-            map.put(months[i - 1], i);
+            map.put(months[0][i], months[1][i]);
         }
-        String[] array = date.split(" ");
-        int year = Integer.parseInt(array[2]);
-        int month = map.get(array[1]);
-        int day = Integer.parseInt(array[0].substring(0, array[0].length() - 2));
+        String[] arr = date.split(" ");
+        String year = arr[2];
+        String month = map.get(arr[1]);
+        String day = arr[0].substring(0, arr[0].length() - 2);
         StringBuilder sb = new StringBuilder();
         sb.append(year).append("-").append(month).append("-").append(day);
 //        return String.format("%d-%02d-%02d", year, month, day);
