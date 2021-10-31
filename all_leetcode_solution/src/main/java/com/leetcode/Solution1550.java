9c9b57abeb408214426458e58bcedef8865e60f1
xiaowangzi123 <1103694415@qq.com>
2021-10-31 22:35:18 +0800

    LeetCode_1550

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1550.java
@@ -0,0 +1,23 @@
+package com.leetcode;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2021/10/31
+ * @description : 存在连续三个奇数的数组
+ */
+public class Solution1550 {
+    public static void main(String[] args) {
+        Solution1550 obj = new Solution1550();
+        int[] arr = {2, 6, 4, 1};
+        System.out.println(obj.threeConsecutiveOdds(arr));
+    }
+
+    public boolean threeConsecutiveOdds(int[] arr) {
+        for (int i = 0; i < arr.length - 2; i++) {
+            if (arr[i] % 2 == 0 && arr[i + 1] % 2 == 0 && arr[i + 2] == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
