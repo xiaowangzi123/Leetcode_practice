9127cc624f01de3759245930eb3cd23993d04180
xiaowangzi123 <1103694415@qq.com>
2021-11-01 00:12:02 +0800

    LeetCode_1360

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1360.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1360.java
@@ -16,6 +16,7 @@ public class Solution1360 {
 //        String date1 = "2020-01-15", date2 = "2019-12-31";
         String date1 = "2023-01-13", date2 = "2044-02-11";
         System.out.println(obj.daysBetweenDates(date1, date2));
+        System.out.println(obj.daysBetweenDates2(date1, date2));
     }
 
     public int daysBetweenDates(String date1, String date2) {
@@ -58,4 +59,37 @@ public class Solution1360 {
         return count;
     }
 
+
+    public int daysBetweenDates2(String date1, String date2) {
+        String[] d1 = date1.split("-"), d2 = date2.split("-");
+        int year1 = Integer.valueOf(d1[0]), year2 = Integer.valueOf(d2[0]);
+        int month1 = Integer.valueOf(d1[1]), month2 = Integer.valueOf(d2[1]);
+        int day1 = Integer.valueOf(d1[2]), day2 = Integer.valueOf(d2[2]);
+        int start = Math.min(year1, year2);
+        return Math.abs(getSumsOfDate(start, year1, month1, day1) - getSumsOfDate(start, year2, month2, day2));
+    }
+
+    public int getSumsOfDate(int start, int year, int month, int day) {
+        int[] months = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        if (isLeapYear(year)) {
+            months[1] = 29;
+        }
+
+        int count = 0;
+        for (int i = start; i < year; i++) {
+            if (isLeapYear(i)) {
+                count += 366;
+            } else {
+                count += 365;
+            }
+        }
+
+        for (int i = 1; i < month; i++) {
+            count += months[i - 1];
+        }
+
+        count += day;
+        return count;
+    }
 }
