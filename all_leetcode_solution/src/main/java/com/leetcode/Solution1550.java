ca6398d25b14bdf9f7c3af87ebeb8ea50971df0d
xiaowangzi123 <1103694415@qq.com>
2021-10-31 22:37:17 +0800

    LeetCode_1550

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1550.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1550.java
@@ -8,13 +8,14 @@ package com.leetcode;
 public class Solution1550 {
     public static void main(String[] args) {
         Solution1550 obj = new Solution1550();
-        int[] arr = {2, 6, 4, 1};
+//        int[] arr = {2, 6, 4, 1};
+        int[] arr = {1, 2, 34, 3, 4, 5, 7, 23, 12};
         System.out.println(obj.threeConsecutiveOdds(arr));
     }
 
     public boolean threeConsecutiveOdds(int[] arr) {
         for (int i = 0; i < arr.length - 2; i++) {
-            if (arr[i] % 2 == 0 && arr[i + 1] % 2 == 0 && arr[i + 2] == 0) {
+            if (arr[i] % 2 == 1 && arr[i + 1] % 2 == 1 && arr[i + 2] % 2 == 1) {
                 return true;
             }
         }
