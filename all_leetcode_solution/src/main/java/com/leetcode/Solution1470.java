b76cbf0533b9772c88a798de9cdb207b624db151
xiaowangzi123 <1103694415@qq.com>
2021-11-02 00:51:04 +0800

    LeetCode_1470

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1470.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1470.java
@@ -5,22 +5,23 @@ import java.util.Arrays;
 /**
  * @author ：wyq
  * @date ：Created in 2020/11/19
- * @description：
+ * @description： 重新排列数组
  */
-public class Solution {
+public class Solution1470 {
     public static void main(String[] args) {
+        Solution1470 obj = new Solution1470();
         int[] arrs = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        Solution obj = new Solution();
         System.out.println(Arrays.toString(obj.shuffle(arrs, 5)));
     }
 
     //此处参数n=nums.length/2
     public int[] shuffle(int[] nums, int n) {
-        int[] res = new int[nums.length];
-        int j = -1;
-        for (int i = 0; i < nums.length / 2; i++) {
-            res[++j] = nums[i];
-            res[++j] = nums[n++];
+        int len = nums.length;
+        int[] res = new int[len];
+        int j = 0;
+        for (int i = 0; i < len/2; i++) {
+            res[j++] = nums[i];
+            res[j++] = nums[n++];
         }
         return res;
     }
