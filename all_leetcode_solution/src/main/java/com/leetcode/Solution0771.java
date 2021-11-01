13b8172ca8093a6f876803b227724201e72b6748
xiaowangzi123 <1103694415@qq.com>
2021-11-01 15:24:00 +0800

    LeetCode_0771

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0771.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0771.java
@@ -3,27 +3,25 @@ package com.leetcode;
 /**
  * @author ：wyq
  * @date ：Created in 2020/11/19
- * @description：
+ * @description : 宝石与石头
  */
-public class Solution {
-
+public class Solution0771 {
     public static void main(String[] args) {
-        Solution obj = new Solution();
+        Solution0771 obj = new Solution0771();
         String j = "aA", s = "aAASSS";
         System.out.println(obj.numJewelsInStones(j, s));
     }
 
 
-    public int numJewelsInStones(String J, String S) {
+    public int numJewelsInStones(String jewels, String stones) {
         int count = 0;
-        for (int i = 0; i < J.length(); i++) {
-            for (int j = 0; j < S.length(); j++) {
-                if (J.charAt(i) == S.charAt(j)) {
+        for (int i = 0; i < jewels.length(); i++) {
+            for (int j = 0; j < stones.length(); j++) {
+                if (jewels.charAt(i) == stones.charAt(j)) {
                     count++;
                 }
             }
         }
-
         return count;
     }
 
