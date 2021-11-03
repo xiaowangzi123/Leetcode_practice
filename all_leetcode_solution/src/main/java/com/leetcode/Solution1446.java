2108b5f94156168a2a40ca5a0dd67e2fe6e13064
xiaowangzi123 <1103694415@qq.com>
2021-11-03 19:54:58 +0800

    LeetCode_1446

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1446.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1446.java
@@ -11,6 +11,8 @@ public class Solution1446 {
 //        System.out.println(obj.maxPower("leetcode"));
 //        System.out.println(obj.maxPower("l"));
         System.out.println(obj.maxPower("aa"));
+        System.out.println(obj.maxPower2("aa"));
+        System.out.println(obj.maxPower2("ababababa"));
     }
 
     public int maxPower(String s) {
@@ -22,6 +24,36 @@ public class Solution1446 {
                 count = 1;
             }
             ans = Math.max(count, ans);
+        }
+        return ans;
+    }
+
+    public int maxPower2(String s) {
+        int ans = 1, n = s.length();
+        int i = 0, j = 1;
+        while (j < n) {
+            while (j < n && s.charAt(i) == s.charAt(j)) {
+                j++;
+            }
+            ans = Math.max(ans, j - i);
+            i = j;
+        }
+        return ans;
+    }
+
+    /**
+     * 滑动窗口
+     */
+    public int maxPower3(String s) {
+        int ans = 1, n = s.length();
+        int i = 0, j = 1;
+        while (j < n) {
+            if (s.charAt(i) == s.charAt(j)) {
+                j++;
+            } else {
+                i++;
+            }
+            ans = Math.max(ans, j - i);
         }
         return ans;
     }
