88439f2640595d33a1aabe30663ecb9d1066fd31
xiaowangzi123 <1103694415@qq.com>
2021-11-03 17:52:32 +0800

    LeetCode_1446

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1446.java
@@ -0,0 +1,26 @@
+package com.leetcode;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2021/11/3
+ * @description : 连续字符
+ */
+public class Solution1446 {
+    public static void main(String[] args) {
+        Solution1446 obj = new Solution1446();
+        System.out.println(obj.maxPower("leetcode"));
+    }
+
+    public int maxPower(String s) {
+        int ans = 0, count = 0, index = 0;
+        for (int i = 0; i < s.length() - 2; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                count++;
+            } else {
+                count = 0;
+            }
+            ans = Math.max(count, ans);
+        }
+        return ans;
+    }
+}
