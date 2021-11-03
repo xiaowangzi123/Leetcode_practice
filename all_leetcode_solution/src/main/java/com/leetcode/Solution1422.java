9ffb657c7dade42303dbf21ff30fea884cc13986
xiaowangzi123 <1103694415@qq.com>
2021-11-03 14:35:31 +0800

    LeetCode_1422

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1422.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1422.java
@@ -10,17 +10,22 @@ import javax.swing.event.MouseInputAdapter;
 public class Solution1422 {
     public static void main(String[] args) {
         Solution1422 obj = new Solution1422();
-        System.out.println(obj.maxScore("011101"));
+//        System.out.println(obj.maxScore("011101"));
+        System.out.println(obj.maxScore("11111"));
+        System.out.println(obj.maxScore("00000"));
     }
 
     public int maxScore(String s) {
         int count = 0, index = 0;
-        while (s.charAt(index) == '0') {
+        while (index < s.length() && s.charAt(index) == '0') {
             index++;
         }
+        if (index == 0 || index == s.length()) {
+            return s.length() - 1;
+        }
         count = index;
-        for (int i=index;i<s.length();i++){
-            if (s.charAt(i)=='1'){
+        for (int i = index; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
                 count++;
             }
         }
