8b524580450014cca293babc9f1667db543256d1
xiaowangzi123 <1103694415@qq.com>
2021-11-03 17:56:28 +0800

    LeetCode_1446

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1446.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1446.java
@@ -8,12 +8,14 @@ package com.leetcode;
 public class Solution1446 {
     public static void main(String[] args) {
         Solution1446 obj = new Solution1446();
-        System.out.println(obj.maxPower("leetcode"));
+//        System.out.println(obj.maxPower("leetcode"));
+//        System.out.println(obj.maxPower("l"));
+        System.out.println(obj.maxPower("aa"));
     }
 
     public int maxPower(String s) {
-        int ans = 0, count = 1;
-        for (int i = 0; i < s.length() - 2; i++) {
+        int ans = 1, count = 1;
+        for (int i = 0; i < s.length() - 1; i++) {
             if (s.charAt(i) == s.charAt(i + 1)) {
                 count++;
             } else {
