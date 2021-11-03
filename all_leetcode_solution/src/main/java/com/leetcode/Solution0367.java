edf24db124bd8b60a0a6d91e66bd2298e4b6e3f7
xiaowangzi123 <1103694415@qq.com>
2021-11-04 01:12:42 +0800

    LeetCode_0367

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0367.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0367.java
@@ -10,16 +10,40 @@ public class Solution0367 {
         Solution0367 obj = new Solution0367();
         System.out.println(obj.isPerfectSquare(1));
         System.out.println(obj.isPerfectSquare(16));
+        System.out.println("---------------");
         System.out.println(obj.isPerfectSquare(2147483647));
+        System.out.println(obj.isPerfectSquare2(2147483647));
+        System.out.println(obj.isPerfectSquare3(2147483647));
     }
 
     /**
-     * 2147483647超时
+     * i*i<=num超时
+     * 2147483647
      */
     public boolean isPerfectSquare(int num) {
         for (int i = 0; i <= Math.sqrt(num); i++) {
             if (i * i == num) {
                 return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isPerfectSquare2(int num) {
+        int x = (int) Math.sqrt(num);
+        return x * x == num;
+    }
+
+    public boolean isPerfectSquare3(int num) {
+        int l = 0, r = num;
+        while (l <= r) {
+            int mid = (r - l) / 2 + l;
+            if (mid * mid == num) {
+                return true;
+            } else if (mid * mid <= num) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
             }
         }
         return false;
