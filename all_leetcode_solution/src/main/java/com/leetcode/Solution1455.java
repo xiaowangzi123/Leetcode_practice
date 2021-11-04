49a4febe765a76f01ac46273ff32d9dd54a3219b
xiaowangzi123 <1103694415@qq.com>
2021-11-04 14:06:53 +0800

    LeetCode_1455

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1455.java
@@ -0,0 +1,24 @@
+package com.leetcode;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2021/11/4
+ * @description : 检查单词是否为句中其他单词的前缀
+ */
+public class Solution1455 {
+    public static void main(String[] args) {
+        Solution1455 obj = new Solution1455();
+        String sentence = "i love eating burger", searchWord = "burg";
+        System.out.println(obj.isPrefixOfWord(sentence, searchWord));
+    }
+
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        String[] str = sentence.split(" +");
+        for (int i = 0; i < str.length; i++) {
+            if (str[i].contains(searchWord)) {
+                return i + 1;
+            }
+        }
+        return -1;
+    }
+}
