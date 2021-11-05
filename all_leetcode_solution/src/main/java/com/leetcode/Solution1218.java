0bf9370c282bf21c3fc4bd3818363f6c7abfe84b
xiaowangzi123 <1103694415@qq.com>
2021-11-05 15:25:10 +0800

    LeetCode_1218

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1218.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1218.java
@@ -1,5 +1,8 @@
 package com.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author :wyq
  * @date ：Created in 2021/11/5
@@ -10,6 +13,7 @@ public class Solution1218 {
         Solution1218 obj = new Solution1218();
         int[] arr = {1, 2, 3, 4};
         System.out.println(obj.longestSubsequence(arr, 1));
+        System.out.println(obj.longestSubsequence2(arr, 1));
     }
 
     public int longestSubsequence(int[] arr, int difference) {
@@ -18,6 +22,16 @@ public class Solution1218 {
         for (int num : arr) {
             dp[num + 20000] = dp[num + 20000 - difference] + 1;
             count = Math.max(count, dp[num + 20000]);
+        }
+        return count;
+    }
+
+    public int longestSubsequence2(int[] arr, int difference) {
+        int count = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int a : arr) {
+            map.put(a, map.getOrDefault(a - difference, 0) + 1);
+            count = Math.max(count, map.get(a));
         }
         return count;
     }
