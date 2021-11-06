129ae3cb53e3fa213cfeb132a291e6bdb0692ecf
xiaowangzi123 <1103694415@qq.com>
2021-11-06 16:55:07 +0800

    LeetCode_1668

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1668.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1668.java
@@ -24,4 +24,31 @@ public class Solution1668 {
         }
         return count;
     }
+
+    public int maxRepeating2(String sequence, String word) {
+        int count = 0;
+        StringBuilder sb = new StringBuilder(word);
+        while (sequence.contains(sb)) {
+            count++;
+            sb.append(word);
+        }
+        return count;
+    }
+
+    public int maxRepeating3(String sequence, String word) {
+        int ans = 0, n = word.length();
+        for (int k = 0; k < sequence.length(); k++) {
+            int j = 0, count = 0;
+            for (int i = k; i < sequence.length(); i++) {
+                if (sequence.charAt(i) == word.charAt(j++ % n)) {
+                    count++;
+                } else {
+                    break;
+                }
+            }
+            ans = Math.max(ans, count / n);
+        }
+        return ans;
+    }
+
 }
