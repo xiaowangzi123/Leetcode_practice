983dc50b88976263118017178967f272f6c567cb
xiaowangzi123 <1103694415@qq.com>
2021-11-07 01:56:21 +0800

    LeetCode_1784

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1784.java
@@ -0,0 +1,30 @@
+package com.leetcode;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2021/11/7
+ * @description : 检查二进制字符串字段
+ */
+public class Solution1784 {
+    public static void main(String[] args) {
+        Solution1784 obj = new Solution1784();
+        System.out.println(obj.checkOnesSegment("1001"));
+        System.out.println(obj.checkOnesSegment2("1001"));
+    }
+
+    /**
+     * 第一个字符为1，后续出现"01"就出现了两次连续为1的片段，返回false
+     */
+    public boolean checkOnesSegment(String s) {
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == '0' && s.charAt(i + 1) == '1') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean checkOnesSegment2(String s) {
+        return !s.contains("01");
+    }
+}
