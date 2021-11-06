6681489ad5393b725570f23e6a0fc622d4c134d5
xiaowangzi123 <1103694415@qq.com>
2021-11-07 02:18:12 +0800

    LeetCode_1812

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1812.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1812.java
@@ -9,14 +9,26 @@ public class Solution1812 {
     public static void main(String[] args) {
         Solution1812 obj = new Solution1812();
         System.out.println(obj.squareIsWhite("a1"));
+        System.out.println(obj.squareIsWhite2("b1"));
+        System.out.println("------------------");
         System.out.println(obj.squareIsWhite("h3"));
         System.out.println(obj.squareIsWhite("c7"));
     }
 
 
+    /**
+     * 如果abcd列转为1234列
+     * 那么每一行和每一列除以2余数为1时，格子是黑色的
+     */
     public boolean squareIsWhite(String coordinates) {
         int i = coordinates.charAt(0) - 'a';
         int j = coordinates.charAt(1) - '0';
         return i % 2 == j % 2;
+    }
+
+    public boolean squareIsWhite2(String coordinates) {
+        int i = coordinates.charAt(0) - 'a' + 1;
+        int j = coordinates.charAt(1) - '0';
+        return i % 2 != j % 2;
     }
 }
