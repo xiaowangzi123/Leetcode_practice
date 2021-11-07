f6d3d1359365c9540432bd5e3f5aa7e1bc42f9f4
xiaowangzi123 <1103694415@qq.com>
2021-11-07 20:25:58 +0800

    LeetCode_2027

--- all_leetcode_solution/src/main/java/com/leetcode/Solution2027.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution2027.java
@@ -11,6 +11,7 @@ public class Solution2027 {
         System.out.println(obj.minimumMoves("XXX"));
         System.out.println(obj.minimumMoves("XXOX"));
         System.out.println(obj.minimumMoves("OOOO"));
+        System.out.println(obj.minimumMoves2("OOOO"));
     }
 
     public int minimumMoves(String s) {
@@ -20,6 +21,33 @@ public class Solution2027 {
                 count++;
                 i += 2;
             }
+        }
+        return count;
+    }
+
+    public int minimumMoves2(String s) {
+        int count = 0;
+        int i = 0, n = s.length();
+        while (i < n) {
+            if (s.charAt(i) == 'X') {
+                count++;
+                i += 3;
+            } else {
+                i++;
+            }
+        }
+        return count;
+    }
+
+    public int minimumMoves3(String s) {
+        int count = 0;
+        int i = 0, n = s.length();
+        while (i < n) {
+            if (s.charAt(i) == 'X') {
+                count++;
+                i += 2;
+            }
+            i++;
         }
         return count;
     }
