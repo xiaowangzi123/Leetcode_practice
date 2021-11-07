5b2e6f83842c28c5ea956dee1eb295545617f784
xiaowangzi123 <1103694415@qq.com>
2021-11-07 18:08:22 +0800

    LeetCode_1903

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1903.java
@@ -0,0 +1,22 @@
+package com.leetcode;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2021/11/7
+ * @description : 字符串中的最大奇数
+ */
+public class Solution1903 {
+    public static void main(String[] args) {
+        Solution1903 obj = new Solution1903();
+        System.out.println(obj.largestOddNumber("35427"));
+    }
+
+    public String largestOddNumber(String num) {
+        for (int i = num.length() - 1; i >= 0; i--) {
+            if ((num.charAt(i) - '0') % 2 == 1) {
+                return num.substring(0, i);
+            }
+        }
+        return "";
+    }
+}
