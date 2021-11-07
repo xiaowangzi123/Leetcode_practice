b44b208794574e4f68c66dda4238381a24d6e14f
xiaowangzi123 <1103694415@qq.com>
2021-11-07 17:45:08 +0800

    LeetCode_1869

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1859.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1859.java
@@ -1,8 +1,5 @@
 package com.leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @author :wyq
  * @date ：Created in 2021/11/7
---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1869.java
@@ -0,0 +1,32 @@
+package com.leetcode;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2021/11/7
+ * @description : 哪种连续子字符串更长
+ */
+public class Solution1869 {
+    public static void main(String[] args) {
+        Solution1869 obj = new Solution1869();
+        System.out.println(obj.checkZeroOnes("1101"));
+    }
+
+    public boolean checkZeroOnes(String s) {
+        int zero = 0, one = 0;
+        int zMax = Integer.MIN_VALUE, oMax = Integer.MIN_VALUE;
+        int i = 0, n = s.length();
+        while (i < n) {
+            if (s.charAt(i) == '0') {
+                zero++;
+                zMax = Math.max(zMax, zero);
+                one = 0;
+            } else {
+                one++;
+                oMax = Math.max(oMax, one);
+                zero = 0;
+            }
+            i++;
+        }
+        return one > zero;
+    }
+}
