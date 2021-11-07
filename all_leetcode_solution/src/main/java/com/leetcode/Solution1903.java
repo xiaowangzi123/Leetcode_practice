52f9471d74a183646b363631c510ac34badc2e80
xiaowangzi123 <1103694415@qq.com>
2021-11-07 18:09:07 +0800

    LeetCode_1903

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1903.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1903.java
@@ -9,12 +9,13 @@ public class Solution1903 {
     public static void main(String[] args) {
         Solution1903 obj = new Solution1903();
         System.out.println(obj.largestOddNumber("35427"));
+        System.out.println(obj.largestOddNumber("3542"));
     }
 
     public String largestOddNumber(String num) {
         for (int i = num.length() - 1; i >= 0; i--) {
             if ((num.charAt(i) - '0') % 2 == 1) {
-                return num.substring(0, i);
+                return num.substring(0, i + 1);
             }
         }
         return "";
