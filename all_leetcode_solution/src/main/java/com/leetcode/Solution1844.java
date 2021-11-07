e837de77ec586befdfdeaa42c8b985a07b3c77f6
xiaowangzi123 <1103694415@qq.com>
2021-11-07 16:23:59 +0800

    LeetCode_1844

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1844.java
@@ -0,0 +1,26 @@
+package com.leetcode;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2021/11/7
+ * @description : 将所有数字用字符替换
+ */
+public class Solution1844 {
+    public static void main(String[] args) {
+        Solution1844 obj = new Solution1844();
+        System.out.println(obj.replaceDigits("a1c1e1"));
+
+        System.out.println((char) ('a' + 1));
+    }
+
+    public String replaceDigits(String s) {
+        char[] ch = s.toCharArray();
+        for (int i = 0; i < ch.length; i += 2) {
+            if (i % 2 == 1) {
+                int temp = ch[i - 1] - '0';
+                ch[i] = (char) (ch[i - 1] + temp);
+            }
+        }
+        return new String(ch);
+    }
+}
