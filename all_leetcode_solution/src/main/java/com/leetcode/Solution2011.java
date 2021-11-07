6cc1bd8f94764a8833abeea5874102b96b638a4a
xiaowangzi123 <1103694415@qq.com>
2021-11-07 20:11:11 +0800

    LeetCode_2011

--- all_leetcode_solution/src/main/java/com/leetcode/Solution2011.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution2011.java
@@ -10,12 +10,26 @@ public class Solution2011 {
         Solution2011 obj = new Solution2011();
         String[] operations = {"--X", "X++", "X++"};
         System.out.println(obj.finalValueAfterOperations(operations));
+        System.out.println(obj.finalValueAfterOperations2(operations));
     }
 
     public int finalValueAfterOperations(String[] operations) {
         int ans = 0;
         for (String s : operations) {
             if (s.contains("+")) {
+                ans++;
+            } else {
+                ans--;
+            }
+        }
+        return ans;
+    }
+
+    //比上面快
+    public int finalValueAfterOperations2(String[] operations) {
+        int ans = 0;
+        for (String s : operations) {
+            if (s.charAt(1)=='+') {
                 ans++;
             } else {
                 ans--;
