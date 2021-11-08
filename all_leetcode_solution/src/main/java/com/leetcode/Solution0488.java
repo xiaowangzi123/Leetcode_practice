9194ee69cfc3992704e7d86fed3a27a8c1656527
xiaowangzi123 <1103694415@qq.com>
2021-11-09 00:38:38 +0800

    LeetCode_0488

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0488.java
@@ -0,0 +1,12 @@
+package com.leetcode;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2021/11/9
+ * @description : todo
+ */
+public class Solution0488 {
+    public static void main(String[] args) {
+
+    }
+}
