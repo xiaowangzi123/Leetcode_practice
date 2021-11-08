8e44167ce675e61bb8872d4bc15b2c2fb8d894d0
xiaowangzi123 <1103694415@qq.com>
2021-11-09 01:05:51 +0800

    LeetCode_1464

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1464.java
@@ -0,0 +1,32 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2021/11/9
+ * @description : 数组中两元素的最大乘积
+ */
+public class Solution1464 {
+    public static void main(String[] args) {
+        Solution1464 obj = new Solution1464();
+        int[] arr = {3, 4, 5, 2};
+        System.out.println(obj.maxProduct2(arr));
+    }
+
+    public int maxProduct(int[] nums) {
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE;
+        for (int a : nums) {
+            if (a > max1) {
+                max1 = a;
+            }
+        }
+        return (max1 - 1) * (max2 - 1);
+    }
+
+    public int maxProduct2(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        return (nums[n - 1] - 1) * (nums[n - 2] - 1);
+    }
+}
