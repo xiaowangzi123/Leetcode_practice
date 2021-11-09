5d7ba3e8fe06fa13499b7e50703a3e4c6109d1e4
xiaowangzi123 <1103694415@qq.com>
2021-11-09 23:26:31 +0800

    LeetCode_1566

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1566.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1566.java
@@ -9,13 +9,17 @@ public class Solution1566 {
     public static void main(String[] args) {
         Solution1566 obj = new Solution1566();
         int[] arr = {1, 2, 4, 4, 4, 4};
-        System.out.println(obj.containsPattern(arr, 1, 3));
-        System.out.println(obj.containsPattern(arr, 2, 3));
+//        System.out.println(obj.containsPattern(arr, 1, 3));
+//        System.out.println(obj.containsPattern(arr, 2, 3));
+        System.out.println("--------------");
+
+        int[] arr2 = {4, 4};
+        System.out.println(obj.containsPattern(arr2, 1, 2));
     }
 
     public boolean containsPattern(int[] arr, int m, int k) {
         int n = arr.length;
-        for (int i = 0; i < n - m * k; i++) {
+        for (int i = 0; i <= n - m * k; i++) {
             int j = 0;
             while (j < m * k) {
                 if (arr[i + j] != arr[i + j % m]) {
@@ -26,6 +30,23 @@ public class Solution1566 {
                 if (j == m * k) {
                     return true;
                 }
+            }
+        }
+        return false;
+    }
+
+
+    public boolean containsPattern2(int[] arr, int m, int k) {
+        int n = arr.length;
+        for (int l = 0; l <= n - m * k; ++l) {
+            int offset;
+            for (offset = 0; offset < m * k; ++offset) {
+                if (arr[l + offset] != arr[l + offset % m]) {
+                    break;
+                }
+            }
+            if (offset == m * k) {
+                return true;
             }
         }
         return false;
