7c1546d3016089916e4dbf47fbe9472f6bb30e8f
xiaowangzi123 <1103694415@qq.com>
2021-11-09 22:45:21 +0800

    LeetCode_1502

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1502.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1502.java
@@ -10,14 +10,15 @@ import java.util.Arrays;
 public class Solution1502 {
     public static void main(String[] args) {
         Solution1502 obj = new Solution1502();
-        int[] arr = {3, 1, 5};
+//        int[] arr = {3, 1, 5};
+        int[] arr = {-509, -19, -439, -264, -404, -369, -299, -89, -229, -54, -194, 16, -544, -159, -124, -474, -334};
         System.out.println(obj.canMakeArithmeticProgression(arr));
     }
 
     public boolean canMakeArithmeticProgression(int[] arr) {
-        int n = arr.length, temp = Math.abs(arr[0] - arr[1]);
         Arrays.sort(arr);
-        for (int i = 2; i < n; i++) {
+        int temp = Math.abs(arr[0] - arr[1]);
+        for (int i = 2; i < arr.length; i++) {
             if (Math.abs(arr[i] - arr[i - 1]) != temp) {
                 return false;
             }
