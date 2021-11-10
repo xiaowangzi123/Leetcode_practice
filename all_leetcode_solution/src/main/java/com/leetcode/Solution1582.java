1dc87ce60dfd1b5269f3f979d8d8cb67dbadfef8
xiaowangzi123 <1103694415@qq.com>
2021-11-10 23:11:52 +0800

    LeetCode_1582

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1582.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1582.java
@@ -27,7 +27,7 @@ public class Solution1582 {
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (a[i] + a[j] == 1) {
+                if (mat[i][j] == 1 && a[i] + b[j] == 1) {
                     count++;
                 }
             }
