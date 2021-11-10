294074cfb5684f3afe560758392eabbc6356dd29
xiaowangzi123 <1103694415@qq.com>
2021-11-11 01:04:14 +0800

    LeetCode_1608

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1608.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1608.java
@@ -1,6 +1,7 @@
 package com.leetcode;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * @author :wyq
@@ -10,19 +11,20 @@ import java.util.Arrays;
 public class Solution1608 {
     public static void main(String[] args) {
         Solution1608 obj = new Solution1608();
-        int[] arr = {3, 5};
+//        int[] arr = {3, 5};
+        int[] arr = {0, 4, 3, 0, 4};
         System.out.println(obj.specialArray(arr));
     }
 
     public int specialArray(int[] nums) {
         Arrays.sort(nums);
         int n = nums.length;
-        for (int x = 0; x <= n; x++) {
-            int index = 0;
-            while (index < n && nums[index] >= x) {
-                index++;
+        for (int x = n; x >= 0; x--) {
+            int index = n;
+            while (index > 0 && nums[index-1] >= x) {
+                index--;
             }
-            if (index == x) {
+            if (n - index == x) {
                 return x;
             }
         }
