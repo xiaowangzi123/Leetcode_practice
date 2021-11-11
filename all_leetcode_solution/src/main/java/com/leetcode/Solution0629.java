b819401886ff8dbae2fb099ec85ad92d8a304246
xiaowangzi123 <1103694415@qq.com>
2021-11-11 19:37:00 +0800

    LeetCode_0629

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0629.java
@@ -0,0 +1,19 @@
+package com.leetcode;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2021/11/11
+ * @description : K个逆序对数组
+ */
+public class Solution0629 {
+    public static void main(String[] args) {
+        Solution0629 obj = new Solution0629();
+    }
+
+    /**
+     * TODO
+     */
+    public int kInversePairs(int n, int k) {
+        return -1;
+    }
+}
