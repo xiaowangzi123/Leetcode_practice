b7fe376dea92e4eb53d22c4f56002b0e2445098a
xiaowangzi123 <1103694415@qq.com>
2021-11-12 02:11:46 +0800

    LeetCode_0375

--- all_leetcode_solution/src/main/java/com/leetcode/dp/Solution0375.java
+++ all_leetcode_solution/src/main/java/com/leetcode/dp/Solution0375.java
@@ -16,7 +16,6 @@ public class Solution0375 {
 
     /**
      * TODO
-     * <p>
      * dp[i][j]表示我从[i,j]之前选择一个数字你来猜，你确保获胜所需要的最少现金
      * 转移方程：
      * dp[i][j] = min(i + dp[i + 1][j],
@@ -38,16 +37,15 @@ public class Solution0375 {
         }
         for (int j = 2; j < n + 1; j++) {
             for (int i = j - 1; i > 0; i--) {
-//                dp[i][j] = Math.min(i + dp[i + 1][j], j + dp[i][j - 1]);
+                //算切割点在两端时的值
+                dp[i][j] = Math.min(i + dp[i + 1][j], j + dp[i][j - 1]);
+                //算切割点从i+1到j-1时的值
                 for (int k = i + 1; k < j; k++) {
                     dp[i][j] = Math.min(k + Math.max(dp[i][k - 1], dp[k + 1][j]), dp[i][j]);
                 }
-                dp[i][j] = Math.min(dp[i][j], i + dp[i + 1][j]);
-                dp[i][j] = Math.min(dp[i][j], j + dp[i][j - 1]);
             }
         }
         return dp[1][n];
     }
-
 
 }
