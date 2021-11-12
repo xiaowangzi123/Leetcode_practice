49fb33824e40ec62574c5e8a0873c02318054b8d
xiaowangzi123 <1103694415@qq.com>
2021-11-12 12:49:20 +0800

    LeetCode_1700

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1700.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1700.java
@@ -1,5 +1,7 @@
 package com.leetcode;
 
+import java.util.LinkedList;
+
 /**
  * @author :wyq
  * @date ：Created in 2021/11/12
@@ -8,10 +10,15 @@ package com.leetcode;
 public class Solution1700 {
     public static void main(String[] args) {
         Solution1700 obj = new Solution1700();
-        int[] students = {1, 1, 0, 0}, sandwiches = {0, 1, 0, 1};
+        int[] students = {1, 1, 1, 0, 0, 1}, sandwiches = {1, 0, 0, 0, 1, 1};
         System.out.println(obj.countStudents(students, sandwiches));
+        System.out.println(obj.countStudents2(students, sandwiches));
     }
 
+    /**
+     * 学生的顺序不重要，如果不喜欢当前汉堡可以继续排队在队尾等待
+     * 但只剩喜欢0类型的汉堡的学生，此时刚好是汉堡1排在前面，则后续的学生都无法吃到汉堡
+     */
     public int countStudents(int[] students, int[] sandwiches) {
         int n = students.length;
         int[] ans = new int[2];
@@ -19,13 +26,35 @@ public class Solution1700 {
             ans[i]++;
         }
         for (int i = 0; i < n; i++) {
-            if (sandwiches[ans[i]] > 0) {
-                ans[i]--;
+            if (ans[sandwiches[i]] > 0) {
+                ans[sandwiches[i]]--;
             } else {
                 return n - i;
             }
         }
 
         return 0;
+    }
+
+    public int countStudents2(int[] students, int[] sandwiches) {
+        int n = students.length;
+        LinkedList<Integer> stack = new LinkedList<>();
+        for (int i : students) {
+            stack.add(i);
+        }
+        for (int i = 0; i < n; i++) {
+            int count = stack.size();
+            while (!stack.isEmpty() && sandwiches[i] != stack.peekFirst()) {
+                if (count == 0) {
+                    return stack.size();
+                }
+                count--;
+                int temp = stack.pollFirst();
+                stack.add(temp);
+            }
+            stack.pollFirst();
+        }
+
+        return stack.size();
     }
 }
