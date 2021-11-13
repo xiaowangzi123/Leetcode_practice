72773b6249214c896c0885b56cd97d358b8a9852
xiaowangzi123 <1103694415@qq.com>
2021-11-13 20:42:40 +0800

    LeetCode_1725

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1725.java
@@ -0,0 +1,28 @@
+package com.leetcode;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2021/11/13
+ * @description : 可以形成最大正方形的矩形数目
+ */
+public class Solution1725 {
+    public static void main(String[] args) {
+        Solution1725 obj = new Solution1725();
+        int[][] rectangles = {{5, 8}, {3, 9}, {5, 12}, {16, 5}};
+        System.out.println(obj.countGoodRectangles(rectangles));
+    }
+
+    public int countGoodRectangles(int[][] rectangles) {
+        int count = 0, max = Integer.MIN_VALUE;
+        for (int[] arr : rectangles) {
+            int temp = Math.min(arr[0], arr[1]);
+            if (max == temp) {
+                count++;
+            } else if (max < temp) {
+                max = temp;
+                count = 1;
+            }
+        }
+        return count;
+    }
+}
