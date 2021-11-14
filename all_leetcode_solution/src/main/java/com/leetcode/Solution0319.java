01c99450995bc140442c957c0dd74b890c64802d
xiaowangzi123 <1103694415@qq.com>
2021-11-15 01:26:31 +0800

    LeetCode_0319

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0319.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0319.java
@@ -13,9 +13,13 @@ public class Solution0319 {
     }
 
     /**
-     * @author: wyq
-     * @create time: 2021/11/15
-     * @description: TODO
+     * 灯泡编号1,2，……n，默认全关闭
+     * 可以理解为第i轮对编号为i的灯泡开关进行一次切换操作
+     * 如果i有偶数个因数，那么最终灯泡关闭
+     * 如果i有奇数个因数，那么灯泡最终打开
+     *
+     * 只有i为完全平方数的时候，灯泡最终是打开的
+     * 所以寻找n个数中的完全平方数即为答案
      */
     public int bulbSwitch(int n) {
         return (int) Math.sqrt(n + 0.5);
