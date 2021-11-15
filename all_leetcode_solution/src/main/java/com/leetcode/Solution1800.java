c296620a70c65753bd7ea8d698f134938e59edfc
xiaowangzi123 <1103694415@qq.com>
2021-11-16 00:15:12 +0800

    LeetCode_1800

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1800.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1800.java
@@ -8,9 +8,10 @@ package com.leetcode;
 public class Solution1800 {
     public static void main(String[] args) {
         Solution1800 obj = new Solution1800();
-//        int[] arr = {12, 17, 15, 13, 10, 11, 12};
-        int[] arr = {12};
+        int[] arr = {12, 17, 15, 13, 10, 11, 12};
+//        int[] arr = {12};
         System.out.println(obj.maxAscendingSum(arr));
+        System.out.println(obj.maxAscendingSum2(arr));
     }
 
     public int maxAscendingSum(int[] nums) {
@@ -28,6 +29,19 @@ public class Solution1800 {
             i++;
         }
 
+        return max;
+    }
+
+    public int maxAscendingSum2(int[] nums) {
+        int sum = nums[0], max = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i - 1] < nums[i]) {
+                sum += nums[i];
+            } else {
+                sum = nums[i];
+            }
+            max = Math.max(sum, max);
+        }
         return max;
     }
 }
