a3a766e92eb6b136ff5dd2d15cbbf5f3a5b46267
xiaowangzi123 <1103694415@qq.com>
2021-11-19 00:45:38 +0800

    LeetCode_0397

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0397.java
@@ -0,0 +1,24 @@
+package com.leetcode;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2021/11/19
+ * @description : 整数替换
+ */
+public class Solution0397 {
+    public static void main(String[] args) {
+        Solution0397 obj = new Solution0397();
+        System.out.println(obj.integerReplacement(7));
+        System.out.println(obj.integerReplacement(8));
+    }
+
+    public int integerReplacement(int n) {
+        if (n == 1) {
+            return 0;
+        }
+        if (n % 2 == 0) {
+            return 1 + integerReplacement(n / 2);
+        }
+        return 2 + Math.min(integerReplacement(n + 1), integerReplacement(n - 1));
+    }
+}
