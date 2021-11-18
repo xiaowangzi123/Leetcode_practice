418a25e214565d4c32aa1a0916e16001478b5d95
xiaowangzi123 <1103694415@qq.com>
2021-11-19 01:04:35 +0800

    LeetCode_0397

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0397.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0397.java
@@ -1,5 +1,8 @@
 package com.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author :wyq
  * @date ：Created in 2021/11/19
@@ -20,5 +23,20 @@ public class Solution0397 {
             return 1 + integerReplacement(n / 2);
         }
         return 2 + Math.min(integerReplacement(n / 2 + 1), integerReplacement(n / 2));
+    }
+
+    public int integerReplacement2(int n) {
+        Map<Integer, Integer> map = new HashMap<>();
+        if (n == 1) {
+            return 0;
+        }
+        if (!map.containsKey(n)) {
+            if (n % 2 == 0) {
+                map.put(n, 1 + integerReplacement2(n / 2));
+            } else {
+                map.put(n, 2 + Math.min(integerReplacement2(n / 2 + 1), integerReplacement2(n / 2)));
+            }
+        }
+        return map.get(n);
     }
 }
