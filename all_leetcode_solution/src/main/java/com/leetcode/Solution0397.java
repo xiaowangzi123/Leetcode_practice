366432954cd7329ea1e7d49121c1014e7a8a10fb
xiaowangzi123 <1103694415@qq.com>
2021-11-19 00:51:14 +0800

    LeetCode_0397数据溢出

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0397.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0397.java
@@ -19,6 +19,6 @@ public class Solution0397 {
         if (n % 2 == 0) {
             return 1 + integerReplacement(n / 2);
         }
-        return 1 + Math.min(integerReplacement(n + 1), integerReplacement(n - 1));
+        return 2 + Math.min(integerReplacement(n / 2 + 1), integerReplacement(n / 2));
     }
 }
