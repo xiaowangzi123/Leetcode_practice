8ff0be1d35475a3362097acf27441cccc9ee57a1
xiaowangzi123 <1103694415@qq.com>
2021-11-19 23:22:39 +0800

    LeetCode_1886

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1886.java
@@ -0,0 +1,41 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2021/11/19
+ * @description : 判断矩阵经轮转后是否一致
+ */
+public class Solution1886 {
+    public static void main(String[] args) {
+        Solution1886 obj = new Solution1886();
+        int[][] mat = {{0, 0, 0}, {0, 1, 0}, {1, 1, 1}};
+        int[][] target = {{1, 1, 1}, {0, 1, 0}, {0, 0, 0}};
+        System.out.println(obj.findRotation(mat, target));
+    }
+
+    public boolean findRotation(int[][] mat, int[][] target) {
+        int n = mat.length;
+        int[][] ans;
+        for (int k = 0; k < 3; k++) {
+            ans = turnMatrix(mat);
+            if (Arrays.deepEquals(ans, target)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int[][] turnMatrix(int[][] mat) {
+        int n = mat.length;
+        int[][] ans = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                ans[i][j] = mat[n - j - 1][i];
+            }
+        }
+
+        return ans;
+    }
+}
