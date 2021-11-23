b4d9ee52d2f4b7b345e3fad405f1091f1eff1cb8
xiaowangzi123 <1103694415@qq.com>
2021-11-23 08:48:21 +0800

    LeetCode_1909

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1909.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1909.java
@@ -47,6 +47,14 @@ public class Solution1909 {
         return true;
     }
 
+    /**
+     * 遍历数组，找到第一个递减的元素对，有nums[k]>=nums[k+1]
+     * 1.如果k=0,说明较大数出现在第一位，删除第一位满足条件
+     * 2.如果nums[k+1]>nums[k-1]，说明nums[k]为峰值，删除k位
+     * 3.如果k=n-2,说明较大数在n-2,较小数在最后一位，删除最后一位满足条件
+     * 4.如果nums[k]<nums[k+2],表明nums[k+1]为低谷值，删除k+1位置元素
+     * 以上四种情况只能出现一次，出现第二次时则不满足条件，直接返回false
+     */
     public boolean canBeIncreasing2(int[] nums) {
         boolean flag = true;
         int n = nums.length;
@@ -66,5 +74,27 @@ public class Solution1909 {
             }
         }
         return true;
+    }
+
+
+    /**
+     * 遍历数组，找到第一个递减的元素对，有nums[k]>=nums[k+1]
+     * 1.如果nums[k-1]>=nums[k+1]时，表明k+1为低谷值，将nums[k]赋值给nums[k+1]后，继续后续遍历
+     * 2.如果nums[k+1]>nums[k-1]且nums[k+1]>nums[k-2]，
+     */
+    public boolean canBeIncreasing3(int[] nums) {
+        int count = 0;
+        for (int i = 1; i < nums.length && count <= 1; i++) {
+            if (nums[i] > nums[i - 1]) {
+                continue;
+            }
+            count++;
+            if (i - 1 > 0 && nums[i] <= nums[i - 2]) {
+                nums[i] = nums[i - 1];
+            } else {
+                nums[i - 1] = nums[i];
+            }
+        }
+        return count <= 1;
     }
 }
