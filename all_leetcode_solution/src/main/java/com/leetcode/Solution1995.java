29f7630e2928b0523b9baaa23e162de8a8078e69
xiaowangzi123 <1103694415@qq.com>
2021-11-24 16:11:56 +0800

    LeetCode_1995

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1995.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1995.java
@@ -1,5 +1,8 @@
 package com.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author :wyq
  * @date ：Created in 2021/11/24
@@ -9,8 +12,11 @@ public class Solution1995 {
     public static void main(String[] args) {
         Solution1995 obj = new Solution1995();
         System.out.println(obj.countQuadruplets(new int[]{1, 2, 3, 6}));
+        System.out.println(obj.countQuadruplets2(new int[]{1, 2, 3, 6}));
         System.out.println(obj.countQuadruplets(new int[]{3, 3, 6, 4, 5}));
+        System.out.println(obj.countQuadruplets2(new int[]{3, 3, 6, 4, 5}));
         System.out.println(obj.countQuadruplets(new int[]{1, 1, 1, 3, 5}));
+        System.out.println(obj.countQuadruplets2(new int[]{1, 1, 1, 3, 5}));
     }
 
     public int countQuadruplets(int[] nums) {
@@ -29,4 +35,22 @@ public class Solution1995 {
         return count;
     }
 
+
+    public int countQuadruplets2(int[] nums) {
+        int n = nums.length, count = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int a = 1; a < n - 2; a++) {
+            for (int b = 0; b < a; b++) {
+                int temp = nums[a] + nums[b];
+                map.put(temp, map.getOrDefault(temp, 0) + 1);
+            }
+
+            for (int c = a + 2; c < n; c++) {
+                int temp = nums[c] - nums[a + 1];
+                count += map.getOrDefault(temp, 0);
+            }
+        }
+
+        return count;
+    }
 }
