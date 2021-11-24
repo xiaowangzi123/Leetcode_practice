f850a810382fb3ba668a68edb606567e2df07114
xiaowangzi123 <1103694415@qq.com>
2021-11-24 14:06:31 +0800

    LeetCode_1979

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1984.java
@@ -0,0 +1,23 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2021/11/24
+ * @description : 学生分数的最小差值
+ */
+public class Solution1984 {
+    public static void main(String[] args) {
+        Solution1984 obj = new Solution1984();
+        System.out.println(obj.minimumDifference(new int[]{9, 4, 1, 7}, 2));
+    }
+
+    public int minimumDifference(int[] nums, int k) {
+        int n = nums.length, min = Integer.MAX_VALUE;
+        for (int i = 0; i < n - k + 1; i++) {
+            min = Math.min(min, nums[i + k - 1] - nums[i]);
+        }
+        return min;
+    }
+}
