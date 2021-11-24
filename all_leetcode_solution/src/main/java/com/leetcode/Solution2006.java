78500a7263f786df2b0e1ee2990a0816e7876887
xiaowangzi123 <1103694415@qq.com>
2021-11-24 16:26:24 +0800

    LeetCode_2006

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution2006.java
@@ -0,0 +1,42 @@
+package com.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2021/11/24
+ * @description : 统计特殊四元组
+ */
+public class Solution2006 {
+    public static void main(String[] args) {
+        Solution2006 obj = new Solution2006();
+        System.out.println(obj.countKDifference(new int[]{1, 3, 3, 1}, 2));
+        System.out.println(obj.countKDifference2(new int[]{1, 3, 3, 1}, 2));
+    }
+
+    public int countKDifference(int[] nums, int k) {
+        int n = nums.length, ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (Math.abs(nums[i] - nums[j]) == k) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public int countKDifference2(int[] nums, int k) {
+        int[] cnt = new int[100];
+        for (int num : nums) {
+            cnt[num - 1]++;
+        }
+
+        int ans = 0;
+        for (int i = 0; i < 100 - k; i++) {
+            ans += cnt[i] * cnt[i + k];
+        }
+        return ans;
+    }
+}
