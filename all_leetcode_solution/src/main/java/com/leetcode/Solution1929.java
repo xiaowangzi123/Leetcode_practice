090bb017c8f8809f7bc7d0f865ccfec80da426ac
xiaowangzi123 <1103694415@qq.com>
2021-11-24 13:51:12 +0800

    LeetCode_1929

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1929.java
@@ -0,0 +1,24 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2021/11/24
+ * @description : 数组串联
+ */
+public class Solution1929 {
+    public static void main(String[] args) {
+        Solution1929 obj = new Solution1929();
+        System.out.println(Arrays.toString(obj.getConcatenation(new int[]{1, 2, 3})));
+    }
+
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n * 2];
+        for (int i = 0; i < n * 2; i++) {
+            ans[i] = nums[i % n];
+        }
+        return ans;
+    }
+}
