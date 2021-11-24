0b423b883bcbdc441aceda8a9dd514af10b3e345
xiaowangzi123 <1103694415@qq.com>
2021-11-24 23:28:22 +0800

    LeetCode_0303

---
+++ num_0303_RangeSumQueryImmutable/src/main/java/com/leetcode/NumArray.java
@@ -0,0 +1,29 @@
+package com.leetcode;
+
+/**
+ * @author: wyq
+ * @create time: 2021/11/24
+ * @description: sumRange函数要多次调用
+ */
+public class NumArray {
+    public static void main(String[] args) {
+        NumArray numArray = new NumArray(new int[]{-2, 0, 3, -5, 2, -1});
+        System.out.println(numArray.sumRange(0, 2));
+        System.out.println(numArray.sumRange(2, 5));
+        System.out.println(numArray.sumRange(0, 5));
+    }
+
+    int[] sum;
+
+    public NumArray(int[] nums) {
+        int n = nums.length;
+        sum = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            sum[i + 1] = sum[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        return sum[right + 1] - sum[left];
+    }
+}
