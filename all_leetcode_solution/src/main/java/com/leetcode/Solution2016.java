5910a489b048a60d7b85aeefd0f08bcaa50046be
xiaowangzi123 <1103694415@qq.com>
2021-11-24 16:32:40 +0800

    LeetCode_2016

--- all_leetcode_solution/src/main/java/com/leetcode/Solution2016.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution2016.java
@@ -9,13 +9,19 @@ public class Solution2016 {
     public static void main(String[] args) {
         Solution2016 obj = new Solution2016();
         System.out.println(obj.maximumDifference(new int[]{7, 1, 5, 4}));
+        System.out.println(obj.maximumDifference(new int[]{9, 4, 3, 2}));
+        System.out.println(obj.maximumDifference(new int[]{1, 5, 2, 10}));
     }
 
     public int maximumDifference(int[] nums) {
         int max = -1;
-
-
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] > nums[i]) {
+                    max = Math.max(max, nums[j] - nums[i]);
+                }
+            }
+        }
         return max;
-
     }
 }
