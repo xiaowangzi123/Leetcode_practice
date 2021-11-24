5f03440a6faf325296b6aa3f13ef57774203712d
xiaowangzi123 <1103694415@qq.com>
2021-11-24 16:34:28 +0800

    LeetCode_2016

--- all_leetcode_solution/src/main/java/com/leetcode/Solution2016.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution2016.java
@@ -17,8 +17,8 @@ public class Solution2016 {
         int max = -1;
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
-                if (nums[j] > nums[i]) {
-                    max = Math.max(max, nums[j] - nums[i]);
+                if (nums[j] > nums[i] && max < nums[j] - nums[i]) {
+                    max = nums[j] - nums[i];
                 }
             }
         }
