2f09660bd93e7f3b8af55aade83a021fa1140c0b
xiaowangzi123 <1103694415@qq.com>
2021-11-25 16:35:35 +0800

    LeetCode_0003

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0003.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0003.java
@@ -12,10 +12,13 @@ public class Solution0003 {
     public static void main(String[] args) {
         Solution0003 obj = new Solution0003();
         System.out.println(obj.lengthOfLongestSubstring(""));
+        System.out.println(obj.lengthOfLongestSubstring2(""));
         System.out.println(obj.lengthOfLongestSubstring("abcabcbb"));
         System.out.println(obj.lengthOfLongestSubstring2("abcabcbb"));
         System.out.println(obj.lengthOfLongestSubstring("bbbbb"));
+        System.out.println(obj.lengthOfLongestSubstring2("bbbbb"));
         System.out.println(obj.lengthOfLongestSubstring("pwwkew"));
+        System.out.println(obj.lengthOfLongestSubstring2("pwwkew"));
     }
 
     public int lengthOfLongestSubstring(String s) {
@@ -36,8 +39,8 @@ public class Solution0003 {
 
     public int lengthOfLongestSubstring2(String s) {
         int max = 0, n = s.length();
-        Set<Character> set = new HashSet<>();
         for (int i = 0; i < n; i++) {
+            Set<Character> set = new HashSet<>();
             int index = i;
             while (index < n && !set.contains(s.charAt(index))) {
                 set.add(s.charAt(index));
@@ -45,7 +48,6 @@ public class Solution0003 {
             }
             max = Math.max(max, set.size());
         }
-
         return max;
     }
 
