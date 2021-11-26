f2f625735af43b0b19072a53bc7afe9e095896aa
xiaowangzi123 <1103694415@qq.com>
2021-11-26 08:52:17 +0800

    LeetCode_0700

---
+++ all_leetcode_solution/src/main/java/com/leetcode/tree/Solution0700.java
@@ -0,0 +1,42 @@
+package com.leetcode.tree;
+
+import com.leetcode.bean.TreeNode;
+import com.sun.org.apache.regexp.internal.RE;
+import com.sun.xml.internal.ws.client.sei.ResponseBuilder;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+
+/**
+ * @author: wyq
+ * @create time: 2021/11/26
+ * @description: 二叉搜索树中的搜索
+ */
+public class Solution0700 {
+    public static void main(String[] args) {
+        Solution0700 obj = new Solution0700();
+        TreeNode t4 = new TreeNode(1, null, null);
+        TreeNode t5 = new TreeNode(3, null, null);
+        TreeNode t2 = new TreeNode(2, t4, t5);
+        TreeNode t3 = new TreeNode(7, null, null);
+        TreeNode t1 = new TreeNode(4, t2, t3);
+
+        System.out.println(obj.searchBST(t1, 2));
+
+
+    }
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        }
+        return searchBST(root.val > val ? root.left : root.right, val);
+    }
+
+}
