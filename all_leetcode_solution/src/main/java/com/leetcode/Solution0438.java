7cccd47381d65ca42f464747946a6fb64c995599
xiaowangzi123 <1103694415@qq.com>
2021-11-28 00:34:01 +0800

    LeetCode_0438

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0438.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0438.java
@@ -13,8 +13,8 @@ import java.util.List;
 public class Solution0438 {
     public static void main(String[] args) {
         Solution0438 obj = new Solution0438();
-        System.out.println(obj.findAnagrams("cbaebabacd", "abc"));
-        System.out.println(obj.findAnagrams("abab", "ab"));
+        System.out.println(obj.findAnagrams2("cbaebabacd", "abc"));
+        System.out.println(obj.findAnagrams2("abab", "ab"));
     }
 
     public List<Integer> findAnagrams(String s, String p) {
@@ -41,8 +41,45 @@ public class Solution0438 {
             cnt1[s1.charAt(i) - 'a']++;
             cnt2[s2.charAt(i) - 'a']++;
         }
-        for (int i = 0; i < 26; i++) {
+        /*for (int i = 0; i < 26; i++) {
             if (cnt1[i] != cnt2[i]) {
+                return false;
+            }
+        }
+        return true;*/
+        return Arrays.equals(cnt1, cnt2);
+    }
+
+
+    public List<Integer> findAnagrams2(String s, String p) {
+        List<Integer> ans = new ArrayList<>();
+        int m = s.length(), n = p.length();
+        if (m < n) {
+            return ans;
+        }
+
+        int[] cnt1 = new int[26];
+        int[] cnt2 = new int[26];
+        for (int i = 0; i < n; i++) {
+            cnt1[s.charAt(i) - 'a']++;
+            cnt2[p.charAt(i) - 'a']++;
+        }
+        if (isEqualsInt(cnt1, cnt2)) {
+            ans.add(0);
+        }
+        for (int i = n; i < m; i++) {
+            cnt1[s.charAt(i) - 'a']++;
+            cnt1[s.charAt(i - n) - 'a']--;
+            if (Arrays.equals(cnt1, cnt2)) {
+                ans.add(i - n + 1);
+            }
+        }
+        return ans;
+    }
+
+    public boolean isEqualsInt(int[] a, int[] b) {
+        for (int i = 0; i < 26; i++) {
+            if (a[i] != b[i]) {
                 return false;
             }
         }
