3d2359668558dd709e66d082386489ec841371b9
xiaowangzi123 <1103694415@qq.com>
2021-11-28 00:19:40 +0800

    LeetCode_0438

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0438.java
@@ -0,0 +1,51 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * @author ：wyq
+ * @date ：Created in 2021/9/17
+ * @description ：找到字符串中所有字母异位词
+ */
+public class Solution0438 {
+    public static void main(String[] args) {
+        Solution0438 obj = new Solution0438();
+//        System.out.println(obj.findAnagrams("cbaebabacd", "abc"));
+        System.out.println(obj.findAnagrams("abab", "ab"));
+    }
+
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> ans = new ArrayList<>();
+        int m = s.length(), n = p.length();
+        if (m < n) {
+            return ans;
+        }
+        for (int i = 0; i < m - n; i++) {
+            if (stringIsEquals(s.substring(i, i + n), p)) {
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+
+    public boolean stringIsEquals(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        int[] cnt1 = new int[26];
+        int[] cnt2 = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            cnt1[s1.charAt(i) - 'a']++;
+            cnt2[s2.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (cnt1[i] != cnt2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
