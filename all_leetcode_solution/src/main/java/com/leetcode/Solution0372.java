50730508c7ed5ae49aa82f9e7865e1db786851ce
xiaowangzi123 <1103694415@qq.com>
2021-12-05 09:56:17 +0800

    LeetCode_0372

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0372.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0372.java
@@ -9,11 +9,16 @@ public class Solution0372 {
     public static void main(String[] args) {
         Solution0372 obj = new Solution0372();
         System.out.println(obj.superPow(2, new int[]{3}));
-        System.out.println(obj.superPow2(2, new int[]{3}));
+//        System.out.println(obj.superPow2(2, new int[]{3}));
         System.out.println(obj.superPow(2, new int[]{1, 0}));
         System.out.println(obj.superPow2(2, new int[]{1, 0}));
         System.out.println(obj.superPow(2147483647, new int[]{2, 0, 0}));
         System.out.println(obj.superPow2(2147483647, new int[]{2, 0, 0}));
+
+        System.out.println("---------------------");
+
+        System.out.println(obj.getNumMod(2,0));
+        System.out.println(obj.pow(2,0));
     }
 
     /**
@@ -47,11 +52,19 @@ public class Solution0372 {
 
     public int getNumMod(int a, int b) {
         int ans = 1;
+        a %= 1337;
         for (int i = 0; i < b; i++) {
-            ans *= (a % 1337);
-            ans %= 1337;
+            ans = (ans * a) % 1337;
         }
         return ans;
+    }
 
+    public int pow(int a, int b) {
+        int ans = 1;
+        a %= 1337;
+        while (b-- > 0) {
+            ans = ans * a % 1337;
+        }
+        return ans;
     }
 }
