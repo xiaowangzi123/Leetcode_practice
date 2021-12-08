6bb6c3b8fd263229fc9d2030178a38ace9a6c020
xiaowangzi123 <1103694415@qq.com>
2021-12-08 09:44:40 +0800

    LeetCode_0689

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0689.java
@@ -0,0 +1,40 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2021/12/8
+ * @description : 三个无重叠子数组的最大和
+ */
+public class Solution0689 {
+    public static void main(String[] args) {
+        Solution0689 obj = new Solution0689();
+        System.out.println(Arrays.toString(obj.maxSumOfOneSubarrays(new int[]{1, 2, 3, 5, 2, 7}, 2)));
+    }
+
+
+    public int[] maxSumOfOneSubarrays(int[] nums, int k) {
+        int[] ans = new int[1];
+        int sum1 = 0, max1 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum1 += nums[i];
+            if (i > k - 1) {
+                if (sum1 > max1) {
+                    max1 = sum1;
+                    ans[0] = i - k + 1;
+                }
+                sum1 -= nums[i - k + 1];
+            }
+        }
+        return ans;
+    }
+
+    public int[] maxSumOfThreeSubarrays(int[] nums, int k) {
+        int[] ans = new int[3];
+
+        return ans;
+    }
+
+
+}
