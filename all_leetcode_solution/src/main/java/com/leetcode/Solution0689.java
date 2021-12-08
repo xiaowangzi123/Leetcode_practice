15a9114d1e93f8a97fb6d87f3a2b298069e76e45
xiaowangzi123 <1103694415@qq.com>
2021-12-08 10:03:33 +0800

    LeetCode_0689

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0689.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0689.java
@@ -10,16 +10,39 @@ import java.util.Arrays;
 public class Solution0689 {
     public static void main(String[] args) {
         Solution0689 obj = new Solution0689();
-        System.out.println(Arrays.toString(obj.maxSumOfOneSubarrays(new int[]{1, 2, 3, 5, 2, 7}, 2)));
+        System.out.println(Arrays.toString(obj.maxSumOfOneSubarrays(new int[]{1, 2, 3, 5, 2, 7}, 3)));
     }
 
 
+    /**
+     * 数组的一个k个元素的最大连续子序列
+     */
     public int[] maxSumOfOneSubarrays(int[] nums, int k) {
         int[] ans = new int[1];
         int sum1 = 0, max1 = 0;
         for (int i = 0; i < nums.length; i++) {
             sum1 += nums[i];
-            if (i > k - 1) {
+            if (i >= k - 1) {
+                if (sum1 > max1) {
+                    max1 = sum1;
+                    ans[0] = i - k + 1;
+                }
+                sum1 -= nums[i - k + 1];
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * 数组的两个k个元素的最大连续子序列
+     */
+    public int[] maxSumOfTwoSubarrays(int[] nums, int k) {
+        int[] ans = new int[2];
+        int sum1 = 0, max1 = 0;
+        int sum2 = 0, max2 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum1 += nums[i];
+            if (i >= k - 1) {
                 if (sum1 > max1) {
                     max1 = sum1;
                     ans[0] = i - k + 1;
