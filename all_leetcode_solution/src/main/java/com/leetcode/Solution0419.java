52015bbb88c9075388af8c131e090d256aba6705
xiaowangzi123 <1103694415@qq.com>
2021-12-18 09:51:55 +0800

    LeetCode_0419

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0419.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0419.java
@@ -33,5 +33,22 @@ public class Solution0419 {
         return ans;
     }
 
+    public int countBattleships2(char[][] board) {
+        int ans = 0, m = board.length, n = board[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'X') {
+                    if (i > 0 && board[i - 1][j] == 'X') {
+                        continue;
+                    }
+                    if (j > 0 && board[i][j - 1] == 'X') {
+                        continue;
+                    }
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
 
 }
