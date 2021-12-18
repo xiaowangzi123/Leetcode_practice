e64e96a0882abf21b1448daf95ed318146b6c91c
xiaowangzi123 <1103694415@qq.com>
2021-12-18 10:03:29 +0800

    LeetCode_0419

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0419.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0419.java
@@ -9,10 +9,12 @@ public class Solution0419 {
     public static void main(String[] args) {
         Solution0419 obj = new Solution0419();
         System.out.println(obj.countBattleships(new char[][]{{'X', '.', '.', 'X'}, {'.', '.', '.', 'X'}, {'.', '.', '.', 'X'}}));
+        System.out.println(obj.countBattleships2(new char[][]{{'X', '.', '.', 'X'}, {'.', '.', '.', 'X'}, {'.', '.', '.', 'X'}}));
     }
 
     /**
      * 战舰一排或者一列组成战舰群
+     * 确定第一个X位置的坐标，从此坐标向右或向下遍历，如果值为X，重新赋值为其他
      */
     public int countBattleships(char[][] board) {
         int ans = 0, m = board.length, n = board[0].length;
@@ -33,6 +35,10 @@ public class Solution0419 {
         return ans;
     }
 
+    /**
+     * 遍历二维数组，找到值为X的坐标(i,j)
+     * 判断(i-1,j)和(i,j-1)的值是否为X，如果是，则(i,j)属于上一个战舰群；否，则（i,j）是新的战舰群的开始
+     */
     public int countBattleships2(char[][] board) {
         int ans = 0, m = board.length, n = board[0].length;
         for (int i = 0; i < m; i++) {
