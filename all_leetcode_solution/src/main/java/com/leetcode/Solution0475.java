baf551032e1be0d79056e28f6ead9fab62ce7640
xiaowangzi123 <1103694415@qq.com>
2021-12-20 13:27:15 +0800

    LeetCode_0475

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0475.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0475.java
@@ -12,20 +12,20 @@ import java.util.Arrays;
 public class Solution0475 {
     public static void main(String[] args) {
         Solution0475 obj = new Solution0475();
-//        System.out.println(obj.findRadius(new int[]{1, 2, 3}, new int[]{2}));
-//        System.out.println(obj.findRadius2(new int[]{1, 2, 3}, new int[]{2}));
-//        System.out.println(obj.findRadius(new int[]{1, 2, 3, 4}, new int[]{1, 4}));
-//        System.out.println(obj.findRadius2(new int[]{1, 2, 3, 4}, new int[]{1, 4}));
-//        System.out.println(obj.findRadius(new int[]{1, 5}, new int[]{2}));
-//        System.out.println(obj.findRadius2(new int[]{1, 5}, new int[]{2}));
+        System.out.println(obj.findRadius(new int[]{1, 2, 3}, new int[]{2}));
+        System.out.println(obj.findRadius2(new int[]{1, 2, 3}, new int[]{2}));
+        System.out.println(obj.findRadius(new int[]{1, 2, 3, 4}, new int[]{1, 4}));
+        System.out.println(obj.findRadius2(new int[]{1, 2, 3, 4}, new int[]{1, 4}));
+        System.out.println(obj.findRadius(new int[]{1, 5}, new int[]{2}));
+        System.out.println(obj.findRadius2(new int[]{1, 5}, new int[]{2}));
 
-//        System.out.println(obj.findRadius(new int[]{282475249, 622650073, 984943658, 144108930, 470211272, 101027544, 457850878, 458777923}, new int[]{823564440, 115438165, 784484492, 74243042, 114807987, 137522503, 441282327, 16531729, 823378840, 143542612}));
-//        System.out.println(obj.findRadius2(new int[]{282475249, 622650073, 984943658, 144108930, 470211272, 101027544, 457850878, 458777923}, new int[]{823564440, 115438165, 784484492, 74243042, 114807987, 137522503, 441282327, 16531729, 823378840, 143542612}));
+        System.out.println(obj.findRadius(new int[]{282475249, 622650073, 984943658, 144108930, 470211272, 101027544, 457850878, 458777923}, new int[]{823564440, 115438165, 784484492, 74243042, 114807987, 137522503, 441282327, 16531729, 823378840, 143542612}));
+        System.out.println(obj.findRadius2(new int[]{282475249, 622650073, 984943658, 144108930, 470211272, 101027544, 457850878, 458777923}, new int[]{823564440, 115438165, 784484492, 74243042, 114807987, 137522503, 441282327, 16531729, 823378840, 143542612}));
 
 
         System.out.println(obj.findRadius(new int[]{617819336, 399125485, 156091745, 356425228}, new int[]{585640194, 937186357}));
         System.out.println(obj.findRadius2(new int[]{617819336, 399125485, 156091745, 356425228}, new int[]{585640194, 937186357}));
-        System.out.println("------------");
+        System.out.println("-------------------------");
 //        System.out.println(obj.minDistance(new int[]{1, 5, 8, 10}, 1));
         System.out.println(obj.minDistance(new int[]{1, 5, 8, 10}, 2));
         System.out.println(obj.minDistance(new int[]{1, 5, 8, 10}, 9));
@@ -59,7 +59,8 @@ public class Solution0475 {
         int ans = Integer.MIN_VALUE;
         Arrays.sort(heaters);
         for (int h : houses) {
-            ans = Math.max(ans, minDistance(heaters, h));
+            int temp = minDistance(heaters, h);
+            ans = Math.max(ans, temp);
         }
         return ans;
     }
@@ -78,8 +79,8 @@ public class Solution0475 {
                     l = mid + 1;
                 }
             }
-            if (r < 0) {
-                return h - arr[0];
+            if (r < 1) {
+                return Math.abs(arr[0] - h);
             } else if (r == arr.length - 1) {
                 return Math.abs(arr[r] - h);
             } else {
