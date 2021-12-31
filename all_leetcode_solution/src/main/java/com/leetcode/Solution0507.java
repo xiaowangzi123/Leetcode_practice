d6fff0aeeeab56f271d70a5531656a4710bb0770
xiaowangzi123 <1103694415@qq.com>
2021-12-31 09:52:08 +0800

    LeetCode_0507

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0507.java
@@ -0,0 +1,34 @@
+package com.leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author: wyq
+ * @create time: 2021/12/31
+ * @description: TODO
+ */
+public class Solution0507 {
+    public static void main(String[] args) {
+        Solution0507 obj = new Solution0507();
+        System.out.println(obj.checkPerfectNumber(6));
+        System.out.println(obj.checkPerfectNumber(28));
+        System.out.println(obj.checkPerfectNumber(496));
+        System.out.println(obj.checkPerfectNumber(8128));
+    }
+
+    public boolean checkPerfectNumber(int num) {
+        int sum = 1;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                sum += i;
+                if (i * i < num) {
+                    sum += num / i;
+                }
+            }
+        }
+        return sum == num;
+    }
+
+}
