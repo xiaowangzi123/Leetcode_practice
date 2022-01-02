263aae3fff9258cf34ba169a68d99e4abbbce41b
xiaowangzi123 <1103694415@qq.com>
2022-01-02 09:25:33 +0800

    LeetCode_0390

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0390.java
@@ -0,0 +1,43 @@
+package com.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author: wyq
+ * @create time: 2022/1/2
+ * @description: 消除游戏
+ */
+public class Solution0390 {
+    public static void main(String[] args) {
+        Solution0390 obj = new Solution0390();
+        System.out.println(obj.lastRemaining(9));
+    }
+
+    public int lastRemaining(int n) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i <= n; i++) {
+            sb.append(i);
+        }
+        boolean flag = true;
+        while (sb.length() > 1) {
+            if (flag) {
+                int m = sb.length();
+                for (int i = 0; i < m; i++) {
+                    sb.deleteCharAt(i);
+                }
+                flag = false;
+            } else {
+                int l = sb.length();
+                for (int i = l - 1; i >= 0; i--) {
+                    sb.deleteCharAt(i);
+                }
+                flag = true;
+            }
+
+        }
+
+        return Integer.valueOf(String.valueOf(sb));
+    }
+
+}
