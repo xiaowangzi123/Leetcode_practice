552ea2084c4b5bceccc97120d9ca6be0a0edb6ba
xiaowangzi123 <1103694415@qq.com>
2022-01-04 09:26:16 +0800

    LeetCode_1185

---
+++ all_leetcode_solution/src/main/java/com/leetcode/todo/Solution0913.java
@@ -0,0 +1,20 @@
+package com.leetcode.todo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2021/10/11
+ * @description :猫和老鼠
+ */
+public class Solution0913 {
+    public static void main(String[] args) {
+        Solution0913 obj = new Solution0913();
+    }
+
+    public int catMouseGame(int[][] graph) {
+        return 0;
+    }
+
+}
