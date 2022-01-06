d3f1bd99114271f0c8125d5c21d9ebce4a0d86ed
xiaowangzi123 <1103694415@qq.com>
2022-01-07 07:55:29 +0800

    LeetCode_1561

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1561.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1561.java
@@ -10,13 +10,16 @@ import java.util.*;
 public class Solution1561 {
     public static void main(String[] args) {
         Solution1561 obj = new Solution1561();
+        System.out.println(obj.maxCoins(new int[]{2, 4, 5}));
         System.out.println(obj.maxCoins(new int[]{2, 4, 1, 2, 7, 8}));
+        System.out.println(obj.maxCoins(new int[]{9, 8, 7, 6, 5, 1, 2, 3, 4}));
+        System.out.println(obj.maxCoins(new int[]{9, 8, 7, 6, 5, 1, 2, 3, 4,11,10,12}));
     }
 
     public int maxCoins(int[] piles) {
         int max = 0, len = piles.length;
         Arrays.sort(piles);
-        for (int i = len / 3; i < len * 2 / 3; i++) {
+        for (int i = len - 2; i >= len/3; i -= 2) {
             max += piles[i];
         }
         return max;
