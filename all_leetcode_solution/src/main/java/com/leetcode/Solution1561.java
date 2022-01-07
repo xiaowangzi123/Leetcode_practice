957b113946dcfddd0acbd1722c871fe4d4c45fbf
xiaowangzi123 <1103694415@qq.com>
2022-01-07 08:02:41 +0800

    LeetCode_1561

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1561.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1561.java
@@ -10,12 +10,15 @@ import java.util.*;
 public class Solution1561 {
     public static void main(String[] args) {
         Solution1561 obj = new Solution1561();
-        System.out.println(obj.maxCoins2(new int[]{2, 4, 5}));
-        System.out.println(obj.maxCoins2(new int[]{2, 4, 1, 2, 7, 8}));
+        System.out.println(obj.maxCoins(new int[]{2, 4, 5}));
+        System.out.println(obj.maxCoins(new int[]{2, 4, 1, 2, 7, 8}));
         System.out.println(obj.maxCoins2(new int[]{9, 8, 7, 6, 5, 1, 2, 3, 4}));
         System.out.println(obj.maxCoins2(new int[]{9, 8, 7, 6, 5, 1, 2, 3, 4, 11, 10, 12}));
     }
 
+    /**
+     * 对数组排序，三个一组的数组中，每次取第二大的数
+     */
     public int maxCoins(int[] piles) {
         int max = 0, len = piles.length;
         Arrays.sort(piles);
