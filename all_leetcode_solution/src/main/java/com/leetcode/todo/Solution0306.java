a3e03020a844d44370affb4432775e39a9357210
xiaowangzi123 <1103694415@qq.com>
2022-01-10 08:56:34 +0800

    LeetCode_0306

---
+++ all_leetcode_solution/src/main/java/com/leetcode/todo/Solution0306.java
@@ -0,0 +1,15 @@
+package com.leetcode.todo;
+
+/**
+ * @author: wyq
+ * @create time: 2022/1/10
+ * @description: 累加数
+ */
+public class Solution0306 {
+    public static void main(String[] args) {
+
+    }
+    public boolean isAdditiveNumber(String num) {
+
+    }
+}
