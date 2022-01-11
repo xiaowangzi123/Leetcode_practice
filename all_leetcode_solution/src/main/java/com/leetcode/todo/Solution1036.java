79c29384067585c6d133ff6ab6f6d80fb6f36ce7
xiaowangzi123 <1103694415@qq.com>
2022-01-11 22:02:40 +0800

    LeetCode_1036

---
+++ all_leetcode_solution/src/main/java/com/leetcode/todo/Solution1036.java
@@ -0,0 +1,21 @@
+package com.leetcode.todo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author: wyq
+ * @create time: 2021/12/16
+ * @description: 逃离大迷宫
+ * TODO
+ */
+public class Solution1036 {
+    public static void main(String[] args) {
+        Solution1036 obj = new Solution1036();
+    }
+
+    public boolean isEscapePossible(int[][] blocked, int[] source, int[] target) {
+        return true;
+    }
+}
