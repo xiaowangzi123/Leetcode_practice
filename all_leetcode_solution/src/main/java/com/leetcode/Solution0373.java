eebdcffeda25c73b2a540f720a212a3e86952547
xiaowangzi123 <1103694415@qq.com>
2022-01-14 09:16:33 +0800

    LeetCode_0373

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0373.java
@@ -0,0 +1,34 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+
+/**
+ * @author: wyq
+ * @create time: 2022/1/14
+ * @description: 查找和最小的K对数字
+ */
+public class Solution0373 {
+    public static void main(String[] args) {
+        Solution0373 obj = new Solution0373();
+        System.out.println(obj.kSmallestPairs(new int[]{1, 7, 11}, new int[]{2, 4, 6}, 3));
+        System.out.println(obj.kSmallestPairs(new int[]{1, 7}, new int[]{2}, 3));
+    }
+
+    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        List<List<Integer>> ans = new ArrayList<>();
+        PriorityQueue<List<Integer>> pq = new PriorityQueue<>(k, (a, b) -> (a.get(0) + a.get(1) - b.get(0) - b.get(1)));
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                List<Integer> temp = new ArrayList<>(Arrays.asList(nums1[i], nums2[j]));
+                pq.add(temp);
+            }
+        }
+        for (int i = 0; i < k && !pq.isEmpty(); i++) {
+            ans.add(pq.poll());
+        }
+        return ans;
+    }
+}
