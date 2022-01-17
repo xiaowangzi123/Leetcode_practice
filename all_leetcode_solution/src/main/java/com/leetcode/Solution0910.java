9c3ab812493043423dadec031d21aed747ad3d7c
xiaowangzi123 <1103694415@qq.com>
2022-01-17 10:34:35 +0800

    LeetCode_1702

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0910.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0910.java
@@ -19,6 +19,9 @@ public class Solution0910 {
      * 数组首先排序，如果整体都加k或者都减去k，最小的差值是ans=nums[n-1]-nums[0]
      * 如果从0-i的元素减少k,从i到n-1的元素都加k，则最小的差值是ans+k*2，不符合题目要求最小
      * 考虑从0-i的元素加k,从i到n-1的元素都减去k，
+     * 遍历截取节点，从0到n-2,
+     * 最大值取nums[n - 1] - k, nums[i] + k之一
+     * 最小值取nums[0] + k, nums[i + 1] - k之一
      */
     public int smallestRangeII(int[] nums, int k) {
         int n = nums.length;
