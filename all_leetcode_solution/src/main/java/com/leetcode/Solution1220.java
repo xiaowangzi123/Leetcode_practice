329b1e5ce85b4f8be3697f0e4a416271d184d515
xiaowangzi123 <1103694415@qq.com>
2022-01-17 10:45:27 +0800

    LeetCode_1220

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1220.java
@@ -0,0 +1,21 @@
+package com.leetcode;
+
+/**
+ * @author: wyq
+ * @create time: 2022/1/17
+ * @description: 统计元音字母序列的数目
+ */
+public class Solution1220 {
+    public static void main(String[] args) {
+        Solution1220 obj = new Solution1220();
+        System.out.println(obj.countVowelPermutation(1));
+        System.out.println(obj.countVowelPermutation(2));
+        System.out.println(obj.countVowelPermutation(3));
+        System.out.println(obj.countVowelPermutation(5));
+    }
+    public int countVowelPermutation(int n) {
+        int ans =0;
+
+        return ans;
+    }
+}
