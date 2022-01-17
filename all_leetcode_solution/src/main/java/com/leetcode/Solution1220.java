223d13f1aab4312489aab75bfb2826b32f45690e
xiaowangzi123 <1103694415@qq.com>
2022-01-17 11:06:46 +0800

    LeetCode_1220

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1220.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1220.java
@@ -1,5 +1,7 @@
 package com.leetcode;
 
+import java.util.Arrays;
+
 /**
  * @author: wyq
  * @create time: 2022/1/17
@@ -13,9 +15,27 @@ public class Solution1220 {
         System.out.println(obj.countVowelPermutation(3));
         System.out.println(obj.countVowelPermutation(5));
     }
-    public int countVowelPermutation(int n) {
-        int ans =0;
 
+    public int countVowelPermutation(int n) {
+        int ans = 0, mod = 1000000007;
+        int[][] dp = new int[n + 1][5];
+        //序列长度为1时，赋值1
+        Arrays.fill(dp[1], 1);
+
+        for (int i = 2; i <= n; i++) {
+            dp[i][0] = dp[i - 1][1] + dp[i - 1][2] + dp[i - 1][4];
+            dp[i][1] = dp[i - 1][0] + dp[i - 1][2];
+            dp[i][2] = dp[i - 1][1] + dp[i - 1][3];
+            dp[i][3] = dp[i - 1][2];
+            dp[i][4] = dp[i - 1][2] + dp[i - 1][3];
+            for (int j = 0; j < 5; j++) {
+                dp[i][j] %= mod;
+            }
+        }
+        for (int j = 0; j < 5; j++) {
+            ans += dp[n][j];
+            ans %= mod;
+        }
         return ans;
     }
 }
