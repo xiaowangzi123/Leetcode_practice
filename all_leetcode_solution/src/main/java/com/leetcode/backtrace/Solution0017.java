9f64d4c495dfacff1f3c3fb73b33a5a6a8e59c76
xiaowangzi123 <1103694415@qq.com>
2022-01-18 23:26:41 +0800

    LeetCode_0017

--- all_leetcode_solution/src/main/java/com/leetcode/backtrace/Solution0017.java
+++ all_leetcode_solution/src/main/java/com/leetcode/backtrace/Solution0017.java
@@ -12,7 +12,9 @@ import java.util.List;
 public class Solution0017 {
     public static void main(String[] args) {
         Solution0017 obj = new Solution0017();
-        System.out.println(obj.letterCombinations("23"));
+        System.out.println(obj.letterCombinations(""));
+//        System.out.println(obj.letterCombinations("2"));
+//        System.out.println(obj.letterCombinations("23"));
     }
 
     private String digLetter[] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
@@ -20,6 +22,9 @@ public class Solution0017 {
 
     public List<String> letterCombinations(String digits) {
         int n = digits.length();
+        if (n == 0) {
+            return ans;
+        }
         backtrack(0, n, new StringBuilder(), digits);
         return ans;
     }
@@ -32,7 +37,7 @@ public class Solution0017 {
         char dig = digits.charAt(start);
         String digLet = digLetter[dig - '0'];
         for (int i = 0; i < digLet.length(); i++) {
-            path.append(digits.charAt(i));
+            path.append(digLet.charAt(i));
             backtrack(start + 1, n, path, digits);
             path.deleteCharAt(start);
         }
