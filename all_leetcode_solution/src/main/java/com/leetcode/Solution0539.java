9a9f1fb807cf5b56483611eba4fd5856026a739c
xiaowangzi123 <1103694415@qq.com>
2022-01-18 10:28:59 +0800

    LeetCode_0539

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0539.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0539.java
@@ -10,18 +10,26 @@ import java.util.*;
 public class Solution0539 {
     public static void main(String[] args) {
         Solution0539 obj = new Solution0539();
-        List<String> list = new ArrayList<>(Arrays.asList("23:59","00:00","12:34"));
+        List<String> list = new ArrayList<>(Arrays.asList("23:59", "00:00", "12:34"));
         System.out.println(obj.findMinDifference(list));
         System.out.println("23:59".compareTo("00:00"));
     }
 
     public int findMinDifference(List<String> timePoints) {
-        int ans=Integer.MAX_VALUE;
         Collections.sort(timePoints, (o1, o2) -> o1.compareTo(o2));
+        int n = timePoints.size();
+        int ans = getMinter(timePoints.get(n - 1)) - getMinter(timePoints.get(0));
 
-
+        for (int i = 0; i < n - 1; i++) {
+            ans = Math.min(ans, getMinter(timePoints.get(i + 1)) - getMinter(timePoints.get(i)));
+        }
         return ans;
     }
 
-
+    public int getMinter(String s) {
+        int minter = 0;
+        minter += ((s.charAt(0) - '0') * 10 + (s.charAt(1) - '0')) * 60;
+        minter += (s.charAt(3) - '0') * 10 + (s.charAt(4) - '0');
+        return minter;
+    }
 }
