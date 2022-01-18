f00d4cca57cb1095e33b553646859dc7b93dd3b1
xiaowangzi123 <1103694415@qq.com>
2022-01-18 18:10:26 +0800

    LeetCode_0039

--- all_leetcode_solution/src/main/java/com/leetcode/backtrace/Solution0040.java
+++ all_leetcode_solution/src/main/java/com/leetcode/backtrace/Solution0040.java
@@ -5,25 +5,25 @@ import java.util.*;
 /**
  * @author: wyq
  * @create time: 2022/1/18
- * @description: 组合总和
+ * @description: 组合总和 II
  */
-public class Solution0039 {
+public class Solution0040 {
     public static void main(String[] args) {
-        Solution0039 obj = new Solution0039();
-        System.out.println(obj.combinationSum(new int[]{2, 3, 6, 7}, 7));
+        Solution0040 obj = new Solution0040();
+        System.out.println(obj.combinationSum2(new int[]{10, 1, 2, 7, 6, 1, 5}, 8));
     }
 
     //ArrayDeque没有ArrayList速度快
     List<List<Integer>> ans = new ArrayList<>();
     Deque<Integer> path = new ArrayDeque<>();
 
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
         int n = candidates.length;
         if (n == 0) {
             return ans;
         }
 
-        Arrays.sort(candidates);
         backtrack(0, n, target, candidates);
         return ans;
     }
@@ -42,12 +42,8 @@ public class Solution0039 {
 
         //元素可以重复，下次添加元素，还是要从当前元素开始，所以回溯方法中i没有加1
         for (int i = start; i < n; i++) {
-            //加入这个剪枝条件，要求数组有序
-            if (target - candidates[i] < 0) {
-                break;
-            }
             path.offer(candidates[i]);
-            backtrack(i, n, target - candidates[i], candidates);
+            backtrack(i + 1, n, target - candidates[i], candidates);
             path.removeLast();
         }
 
