320cd133a223c4a9cd3b376e95366be07759a8cd
xiaowangzi123 <1103694415@qq.com>
2022-01-19 12:42:35 +0800

    LeetCode_0093通过

--- all_leetcode_solution/src/main/java/com/leetcode/backtrace/Solution0093.java
+++ all_leetcode_solution/src/main/java/com/leetcode/backtrace/Solution0093.java
@@ -12,6 +12,7 @@ public class Solution0093 {
     public static void main(String[] args) {
         Solution0093 obj = new Solution0093();
 //        System.out.println(obj.restoreIpAddresses("abcde"));
+//        System.out.println(obj.restoreIpAddresses("012345678"));
         System.out.println(obj.restoreIpAddresses("25525511135"));
         System.out.println("----------------");
         String str = "123456";
@@ -29,27 +30,40 @@ public class Solution0093 {
             return ans;
         }
 
-        backtrack(0, 0, n, s, "");
+        backtrack(0, 0, n, s);
         return ans;
     }
 
-
-    /**
-     *
-     */
-    public void backtrack(int index, int count, int n, String s, String sub) {
+    public void backtrack(int index, int count, int n, String s) {
         if (index == s.length()) {
             if (count == 4) {
                 ans.add(String.join(".", path));
             }
             return;
+        } else if (count > 4) {
+            return;
         }
 
         for (int i = index; i < index + 3 && i < n; i++) {
+            if (count == 4) {
+                break;
+            }
             String temp = s.substring(index, i + 1);
+            if (!checkIp(temp)) {
+                continue;
+            }
             path.add(temp);
-            backtrack(i + 1, count + 1, n, s, s);
+            backtrack(i + 1, count + 1, n, s);
             path.remove(path.size() - 1);
         }
+    }
+
+    public boolean checkIp(String ip) {
+        int len = ip.length();
+        if (len > 1 && ip.charAt(0) == '0') {
+            return false;
+        }
+        int res = Integer.valueOf(ip);
+        return res >= 0 && res <= 255;
     }
 }
