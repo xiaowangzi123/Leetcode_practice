f59be0b5881252b72bfb7a1810302b9c58c51d07
xiaowangzi123 <1103694415@qq.com>
2022-01-19 11:28:08 +0800

    LeetCode_0093

--- all_leetcode_solution/src/main/java/com/leetcode/backtrace/Solution0093.java
+++ all_leetcode_solution/src/main/java/com/leetcode/backtrace/Solution0093.java
@@ -12,17 +12,36 @@ public class Solution0093 {
     public static void main(String[] args) {
         Solution0093 obj = new Solution0093();
         System.out.println(obj.restoreIpAddresses("25525511135"));
+
+
     }
 
     List<String> ans = new ArrayList<>();
+    List<String> path = new ArrayList<>();
 
     public List<String> restoreIpAddresses(String s) {
+        int n = s.length();
+        if (n < 4 || n > 12) {
+            return ans;
+        }
 
         return ans;
     }
 
 
-    public void backtrack(int i, int j, int index, String s) {
+    /**
+     *
+     */
+    public void backtrack(int index, int count, String s) {
+        if (index == s.length()) {
+            if (count == 0) {
+                ans.add(String.join(".", path));
+            }
+            return;
+        }
 
+        for (int i = 1; i < s.length(); i++) {
+
+        }
     }
 }
