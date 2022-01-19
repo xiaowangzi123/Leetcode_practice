1738738de5625bcd535d9218f732567194a3a366
xiaowangzi123 <1103694415@qq.com>
2022-01-19 11:02:48 +0800

    LeetCode_0093

---
+++ all_leetcode_solution/src/main/java/com/leetcode/backtrace/Solution0093.java
@@ -0,0 +1,28 @@
+package com.leetcode.backtrace;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: wyq
+ * @create time: 2022/1/19
+ * @description: 复原 IP 地址
+ */
+public class Solution0093 {
+    public static void main(String[] args) {
+        Solution0093 obj = new Solution0093();
+        System.out.println(obj.restoreIpAddresses("25525511135"));
+    }
+
+    List<String> ans = new ArrayList<>();
+
+    public List<String> restoreIpAddresses(String s) {
+
+        return ans;
+    }
+
+
+    public void backtrack(int i, int j, int index, String s) {
+
+    }
+}
