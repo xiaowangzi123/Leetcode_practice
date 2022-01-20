b83914801bba2bbd82646c42f29720e8be341ed4
xiaowangzi123 <1103694415@qq.com>
2022-01-20 08:59:55 +0800

    LeetCode_2029

--- all_leetcode_solution/src/main/java/com/leetcode/Solution2029.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution2029.java
@@ -1,5 +1,7 @@
 package com.leetcode;
 
+import java.util.spi.CurrencyNameProvider;
+
 /**
  * @author: wyq
  * @create time: 2022/1/20
@@ -8,11 +10,20 @@ package com.leetcode;
 public class Solution2029 {
     public static void main(String[] args) {
         Solution2029 obj = new Solution2029();
-        System.out.println(obj.stoneGameIX(new int[]{2,1}));
+        System.out.println(obj.stoneGameIX(new int[]{2, 1}));
+        System.out.println(obj.stoneGameIX(new int[]{2}));
+        System.out.println(obj.stoneGameIX(new int[]{5,1,2,4,3}));
     }
 
     public boolean stoneGameIX(int[] stones) {
-
+        int[] cnt = new int[3];
+        for (int i : stones) {
+            cnt[i % 3]++;
+        }
+        if (cnt[0] % 2 == 0) {
+            return cnt[1] > 0 && cnt[2] > 0;
+        }
+        return Math.abs(cnt[1] - cnt[2]) > 2;
     }
 
 }
