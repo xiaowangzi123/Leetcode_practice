e19b56e32b8ef8cdb6d0031089ea7910c9d95bd9
xiaowangzi123 <1103694415@qq.com>
2022-01-20 12:38:40 +0800

    LeetCode_0006

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0006.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0006.java
@@ -12,6 +12,7 @@ public class Solution0006 {
     public static void main(String[] args) {
         Solution0006 obj = new Solution0006();
         System.out.println(obj.convert("0123456789abcdefghi", 4));
+        System.out.println(obj.convert2("0123456789abcdefghi", 4));
     }
 
 
@@ -34,11 +35,31 @@ public class Solution0006 {
                     sb.append(s.charAt(step * j + i));
                 }
             } else {
+                //边界怎么确定
                 for (int j = 0; step * j + i < n; j++) {
                     if (j > 0) {
                         sb.append(s.charAt(step * j - i));
                     }
                     sb.append(s.charAt(step * j + i));
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+
+
+    public String convert2(String s, int num) {
+        if (num == 1) {
+            return s;
+        }
+        StringBuilder sb = new StringBuilder();
+        int n = s.length(), step = 2 * (num - 1);
+        for (int i = 0; i < num; i++) {
+            for (int k = 0; k + i < n; k += step) {
+                sb.append(s.charAt(k + i));
+                if (i != 0 && i != num - 1 && k + step - i < n) {
+                    sb.append(s.charAt(k + step - i));
                 }
             }
         }
