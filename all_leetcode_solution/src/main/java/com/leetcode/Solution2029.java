72e509989050d9d59400a93e2293b5f648a7d19b
xiaowangzi123 <1103694415@qq.com>
2022-01-20 08:39:42 +0800

    LeetCode_2029

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution2029.java
@@ -0,0 +1,18 @@
+package com.leetcode;
+
+/**
+ * @author: wyq
+ * @create time: 2022/1/20
+ * @description: 石子游戏 IX
+ */
+public class Solution2029 {
+    public static void main(String[] args) {
+        Solution2029 obj = new Solution2029();
+        System.out.println(obj.stoneGameIX(new int[]{2,1}));
+    }
+
+    public boolean stoneGameIX(int[] stones) {
+
+    }
+
+}
