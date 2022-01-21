5278cf96073ffa4220afa7d41093e73fd770999e
xiaowangzi123 <1103694415@qq.com>
2022-01-21 12:48:01 +0800

    LeetCode_1345

--- all_leetcode_solution/src/main/java/com/leetcode/todo/Solution1345.java
+++ all_leetcode_solution/src/main/java/com/leetcode/todo/Solution1345.java
@@ -11,7 +11,9 @@ import java.util.*;
 public class Solution1345 {
     public static void main(String[] args) {
         Solution1345 obj = new Solution1345();
+//        System.out.println(obj.minJumps(new int[]{100, 100}));
         System.out.println(obj.minJumps(new int[]{100, -23, -23, 404, 100, 23, 23, 23, 3, 404}));
+        System.out.println(obj.minJumps2(new int[]{100, -23, -23, 404, 100, 23, 23, 23, 3, 404}));
     }
 
     public int minJumps(int[] arr) {
@@ -26,34 +28,87 @@ public class Solution1345 {
             indexMap.get(arr[i]).add(i);
         }
 
+        //遍历过的元素集合
         Set<Integer> visitedIndex = new HashSet<>();
+        //存储下标和到达此下标需要的步数
         Queue<int[]> queue = new ArrayDeque<>();
+        //添加第一个坐标到队列中
         queue.offer(new int[]{0, 0});
         visitedIndex.add(0);
+
         while (!queue.isEmpty()) {
             int[] idxStep = queue.poll();
             int idx = idxStep[0], step = idxStep[1];
             if (idx == arr.length - 1) {
                 return step;
             }
-            int v = arr[idx];
+            int val = arr[idx];
             step++;
-            if (indexMap.containsKey(v)) {
-                for (int i : indexMap.get(v)) {
+            //到达与当前元素相同的位置
+            if (indexMap.containsKey(val)) {
+                for (int i : indexMap.get(val)) {
                     if (visitedIndex.add(i)) {
                         queue.offer(new int[]{i, step});
                     }
                 }
-                indexMap.remove(v);
+                indexMap.remove(val);
             }
-            if (idx + 1 < arr.length && visitedIndex.add(idx + 1)) {
+            //到达当前元素的下一个位置
+            if (idx + 1 < n && visitedIndex.add(idx + 1)) {
                 queue.offer(new int[]{idx + 1, step});
             }
+            //到达当前元素的前一个位置
             if (idx - 1 >= 0 && visitedIndex.add(idx - 1)) {
+                queue.offer(new int[]{idx - 1, step});
+            }
+        }
+        return -1;
+    }
+
+    //超时
+    public int minJumps2(int[] arr) {
+        int n = arr.length;
+        if (n == 1) {
+            return 0;
+        }
+        //把相同元素的下标放在一起
+        Map<Integer, List<Integer>> indexMap = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            indexMap.putIfAbsent(arr[i], new ArrayList<>());
+            indexMap.get(arr[i]).add(i);
+        }
+
+        boolean[] used = new boolean[n];
+        //存储下标和步数
+        LinkedList<int[]> queue = new LinkedList<>();
+        //添加第一个坐标到队列中
+        queue.offer(new int[]{0, 0});
+        used[0] = true;
+        while (!queue.isEmpty()) {
+            int[] idxStep = queue.poll();
+            int idx = idxStep[0], step = idxStep[1];
+            if (idx == arr.length - 1) {
+                return step;
+            }
+            int val = arr[idx];
+            step++;
+            if (indexMap.containsKey(val)) {
+                for (int i : indexMap.get(val)) {
+                    if (!used[i]) {
+                        queue.offer(new int[]{i, step});
+                    }
+                }
+                indexMap.remove(val);
+            }
+            if (idx + 1 < arr.length && !used[idx + 1]) {
+                queue.offer(new int[]{idx + 1, step});
+            }
+            if (idx - 1 >= 0 && !used[idx - 1]) {
                 queue.offer(new int[]{idx - 1, step});
             }
         }
         return -1;
 
     }
+
 }
