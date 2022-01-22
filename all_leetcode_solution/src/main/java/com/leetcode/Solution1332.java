d69f38ea9caf246754f72ebd3e17ed4f87cdcd0b
xiaowangzi123 <1103694415@qq.com>
2022-01-22 10:47:15 +0800

    LeetCode_1345，遍历过的元素没有重新赋值为true

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1332.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1332.java
@@ -4,6 +4,8 @@ package com.leetcode;
  * @author :wyq
  * @date ：Created in 2021/11/1
  * @description : 删除回文子序列
+ * 只有ab组成，一次删除a,第二次删除b,最多删除两次
+ * 如果字符串本身是一个回文字符串，那仅仅需要删除一次
  */
 public class Solution1332 {
     public static void main(String[] args) {
@@ -11,6 +13,7 @@ public class Solution1332 {
         System.out.println(obj.removePalindromeSub("ababaa"));
         System.out.println(obj.removePalindromeSub2("ababaa"));
     }
+
 
     public int removePalindromeSub(String s) {
         if (isPalindrome(s)) {
