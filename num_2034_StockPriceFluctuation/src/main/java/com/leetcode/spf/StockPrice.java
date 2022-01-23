2fe03c5c8153551840d3982a82466e7a445b089c
xiaowangzi123 <1103694415@qq.com>
2022-01-23 10:06:33 +0800

    LeetCode_2034

--- num_2034_StockPriceFluctuation/src/main/java/com/leetcode/spf/StockPrice.java
+++ num_2034_StockPriceFluctuation/src/main/java/com/leetcode/spf/StockPrice.java
@@ -1,13 +1,68 @@
 package com.leetcode.spf;
 
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
 /**
- * Hello world!
- *
+ * @author: wyq
+ * @create time: 2022/1/23
+ * @description: 股票价格波动
  */
-public class StockPrice
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class StockPrice {
+    int maxTimestamp;
+    HashMap<Integer, Integer> timePriceMap;
+    PriorityQueue<int[]> pqMax;
+    PriorityQueue<int[]> pqMin;
+
+    public StockPrice() {
+        maxTimestamp = 0;
+        timePriceMap = new HashMap<Integer, Integer>();
+        pqMax = new PriorityQueue<int[]>((a, b) -> b[0] - a[0]);
+        pqMin = new PriorityQueue<int[]>((a, b) -> a[0] - b[0]);
+    }
+
+    public void update(int timestamp, int price) {
+        maxTimestamp = Math.max(maxTimestamp, timestamp);
+        timePriceMap.put(timestamp, price);
+        pqMax.offer(new int[]{price, timestamp});
+        pqMin.offer(new int[]{price, timestamp});
+    }
+
+    public int current() {
+        return timePriceMap.get(maxTimestamp);
+    }
+
+    public int maximum() {
+        while (true) {
+            int[] priceTime = pqMax.peek();
+            int price = priceTime[0], timestamp = priceTime[1];
+            if (timePriceMap.get(timestamp) == price) {
+                return price;
+            }
+            pqMax.poll();
+        }
+    }
+
+    public int minimum() {
+        while (true) {
+            int[] priceTime = pqMin.peek();
+            int price = priceTime[0], timestamp = priceTime[1];
+            if (timePriceMap.get(timestamp) == price) {
+                return price;
+            }
+            pqMin.poll();
+        }
+    }
+
+    public static void main(String[] args) {
+        StockPrice stockPrice = new StockPrice();
+        stockPrice.update(1, 10);
+        stockPrice.update(2, 5);
+        stockPrice.current();
+        stockPrice.maximum();
+        stockPrice.update(1, 3);
+        stockPrice.maximum();
+        stockPrice.update(4, 2);
+        stockPrice.minimum();
     }
 }
