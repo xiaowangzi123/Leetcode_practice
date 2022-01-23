650a3456eaeaf91ad1153c38f65356197ba5f0cb
xiaowangzi123 <1103694415@qq.com>
2022-01-23 09:52:53 +0800

    LeetCode_2034

---
+++ num_2034_StockPriceFluctuation/src/main/java/com/leetcode/spf/StockPrice.java
@@ -0,0 +1,13 @@
+package com.leetcode.spf;
+
+/**
+ * Hello world!
+ *
+ */
+public class StockPrice
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
