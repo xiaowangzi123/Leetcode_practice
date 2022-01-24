6ca9fe59df61457c3c45edfcc7109b157aaa600a
xiaowangzi123 <1103694415@qq.com>
2022-01-24 09:56:18 +0800

    LeetCode_2045

---
+++ all_leetcode_solution/src/main/java/com/leetcode/todo/Solution2045.java
@@ -0,0 +1,24 @@
+package com.leetcode.todo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author: wyq
+ * @create time: 2022/1/24
+ * @description: 到达目的地的第二短时间
+ * TODO
+ */
+public class Solution2045 {
+    public static void main(String[] args) {
+        Solution2045 obj = new Solution2045();
+        int[][] edges ={{1,2},{1,3},{1,4},{3,4},{4,5}};
+    }
+
+    public int secondMinimum(int n, int[][] edges, int time, int change) {
+        int ans = 0;
+
+        return ans;
+    }
+}
