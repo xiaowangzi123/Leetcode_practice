410e740c122b12cb6d14d015eb853b9d370499a4
xiaowangzi123 <1103694415@qq.com>
2022-01-25 09:06:06 +0800

    LeetCode_1688

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1688.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1688.java
@@ -12,9 +12,24 @@ public class Solution1688 {
     public static void main(String[] args) {
         Solution1688 obj = new Solution1688();
         System.out.println(obj.numberOfMatches(7));
+        System.out.println(obj.numberOfMatches2(7));
     }
 
     public int numberOfMatches(int n) {
-        return n-1;
+        return n - 1;
+    }
+
+    public int numberOfMatches2(int n) {
+        int ans = 0;
+        while (n > 1) {
+            if (n % 2 == 0) {
+                ans += n / 2;
+                n /= 2;
+            } else {
+                ans += (n - 1) / 2;
+                n = (n - 1) / 2 + 1;
+            }
+        }
+        return ans;
     }
 }
