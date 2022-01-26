dbbdcbc5b336fb63948c8897791693a3e4556905
xiaowangzi123 <1103694415@qq.com>
2022-01-26 14:11:35 +0800

    LeetCode_2013

--- num_2013_DetectSquares/src/main/java/com/leetcode/ds/DetectSquares.java
+++ num_2013_DetectSquares/src/main/java/com/leetcode/ds/DetectSquares.java
@@ -11,35 +11,35 @@ import java.util.Set;
  */
 class DetectSquares {
 
+    //{x, {y : 点(x,y)数量}}
     Map<Integer, Map<Integer, Integer>> map;
 
     public DetectSquares() {
-        map = new HashMap<Integer, Map<Integer, Integer>>();
+        map = new HashMap<>();
     }
 
     public void add(int[] point) {
         int x = point[0], y = point[1];
-        map.putIfAbsent(y, new HashMap<Integer, Integer>());
-        Map<Integer, Integer> yCnt = map.get(y);
-        yCnt.put(x, yCnt.getOrDefault(x, 0) + 1);
+        map.putIfAbsent(x, new HashMap<>());
+        Map<Integer, Integer> cntMap = map.get(x);
+        cntMap.put(y, cntMap.getOrDefault(y, 0) + 1);
     }
 
     public int count(int[] point) {
         int res = 0;
-        int x = point[0], y = point[1];
-        if (!map.containsKey(y)) {
+        int a = point[0], b = point[1];
+        if (!map.containsKey(a)) {
             return 0;
         }
-        Map<Integer, Integer> yCnt = map.get(y);
-        Set<Map.Entry<Integer, Map<Integer, Integer>>> entries = map.entrySet();
-        for (Map.Entry<Integer, Map<Integer, Integer>> entry : entries) {
-            int col = entry.getKey();
-            Map<Integer, Integer> colCnt = entry.getValue();
-            if (col != y) {
+        Map<Integer, Integer> checkMap = map.get(a);
+        for (Map.Entry<Integer, Map<Integer, Integer>> entry : map.entrySet()) {
+            int x = entry.getKey();
+            Map<Integer, Integer> curMap = entry.getValue();
+            if (a != x) {
                 // 根据对称性，这里可以不用取绝对值
-                int d = col - y;
-                res += colCnt.getOrDefault(x, 0) * yCnt.getOrDefault(x + d, 0) * colCnt.getOrDefault(x + d, 0);
-                res += colCnt.getOrDefault(x, 0) * yCnt.getOrDefault(x - d, 0) * colCnt.getOrDefault(x - d, 0);
+                int d = a - x;
+                res += curMap.getOrDefault(b, 0) * checkMap.getOrDefault(b + d, 0) * map.getOrDefault(a + d, new HashMap<>()).getOrDefault(b+d,0);
+                res += curMap.getOrDefault(b, 0) * checkMap.getOrDefault(x - d, 0) * map.getOrDefault(x - d, new HashMap<>()).getOrDefault(b-d,0);
             }
         }
         return res;
@@ -50,6 +50,7 @@ class DetectSquares {
         DetectSquares detectSquares = new DetectSquares();
         detectSquares.add(new int[]{3, 10});
         detectSquares.add(new int[]{11, 2});
+//        detectSquares.add(new int[]{3, 2});
         detectSquares.add(new int[]{3, 2});
         detectSquares.count(new int[]{11, 10});
         detectSquares.count(new int[]{14, 8});
