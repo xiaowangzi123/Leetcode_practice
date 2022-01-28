bda7417e689f5bba90f6878bdfa5de3c77c13397
xiaowangzi123 <1103694415@qq.com>
2022-01-28 12:05:39 +0800

    LeetCode_1996

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1996.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1996.java
@@ -12,6 +12,8 @@ public class Solution1996 {
     public static void main(String[] args) {
         Solution1996 obj = new Solution1996();
         System.out.println(obj.numberOfWeakCharacters(new int[][]{{5, 5}, {6, 3}, {3, 6}}));
+        System.out.println(obj.numberOfWeakCharacters(new int[][]{{2, 2}, {3, 3}}));
+        System.out.println(obj.numberOfWeakCharacters(new int[][]{{7, 7}, {1, 2}, {9, 7}, {7, 3}, {3, 10}, {9, 8}, {8, 10}, {4, 3}, {1, 5}, {1, 5}}));
     }
 
 
@@ -19,8 +21,9 @@ public class Solution1996 {
         int ans = 0, n = properties.length;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (properties[i][0] < properties[j][0] && properties[j][1] < properties[j][1]) {
+                if (properties[i][0] < properties[j][0] && properties[i][1] < properties[j][1]) {
                     ans++;
+                    break;
                 }
             }
         }
