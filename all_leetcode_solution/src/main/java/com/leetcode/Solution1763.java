da70ceaadd07f016add11a08c5a9985feb47a408
xiaowangzi123 <1103694415@qq.com>
2022-02-01 10:47:16 +0800

    LeetCode_1763 改进

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1763.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1763.java
@@ -19,6 +19,7 @@ public class Solution1763 {
         System.out.println(obj.longestNiceSubstring("dDzeE"));
         System.out.println(obj.longestNiceSubstring2("aAbBcC"));
         System.out.println(obj.longestNiceSubstring2("adDzeE"));
+        System.out.println(obj.longestNiceSubstring3("adDzeE"));
         System.out.println("-----------------------------");
 
         System.out.println(obj.isLongFineString("b"));
@@ -59,6 +60,7 @@ public class Solution1763 {
     }
 
     /**
+     * 分治法
      * 字符串的每个字母添加到set集合
      * 遍历字符串，如果当前字母的大小写不在set中，说明完美字符串在该字母的前面或后面
      * 然后分别递归此字母前后两个字符串
@@ -86,18 +88,26 @@ public class Solution1763 {
         return s;
     }
 
+    /**
+     * set集合效率不如数组
+     */
     public String longestNiceSubstring3(String s) {
         int n = s.length();
         if (n < 2) {
             return "";
         }
-        Set<Character> set = new HashSet<>();
+        boolean[] low = new boolean[26];
+        boolean[] up = new boolean[26];
         for (char ch : s.toCharArray()) {
-            set.add(ch);
+            if (Character.isLowerCase(ch)) {
+                low[ch - 'a'] = true;
+            } else {
+                up[ch - 'A'] = true;
+            }
         }
         for (int i = 0; i < n; i++) {
             char temp = s.charAt(i);
-            if (set.contains(Character.toLowerCase(temp)) && set.contains(Character.toUpperCase(temp))) {
+            if (low[Character.toLowerCase(temp) - 'a'] && up[Character.toUpperCase(temp) - 'A']) {
                 continue;
             }
             String s1 = longestNiceSubstring2(s.substring(0, i));
