1d4c4a12a906f170d40aa7e2f1c0002c8c9c2b4e
xiaowangzi123 <1103694415@qq.com>
2022-02-01 10:35:31 +0800

    LeetCode_1763 注释

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1763.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1763.java
@@ -17,7 +17,8 @@ public class Solution1763 {
 //        String s = "YazaAay";
 //        String s = "Bb";
         System.out.println(obj.longestNiceSubstring("dDzeE"));
-        System.out.println(obj.longestNiceSubstring2("dDzeE"));
+        System.out.println(obj.longestNiceSubstring2("aAbBcC"));
+        System.out.println(obj.longestNiceSubstring2("adDzeE"));
         System.out.println("-----------------------------");
 
         System.out.println(obj.isLongFineString("b"));
@@ -57,6 +58,13 @@ public class Solution1763 {
         return true;
     }
 
+    /**
+     * 字符串的每个字母添加到set集合
+     * 遍历字符串，如果当前字母的大小写不在set中，说明完美字符串在该字母的前面或后面
+     * 然后分别递归此字母前后两个字符串
+     * 如果长度一致，则返回前面字符串；
+     * 遍历结束后，没有返回，说明原始字符串就是一个完美字符串，直接返回
+     */
     public String longestNiceSubstring2(String s) {
         int n = s.length();
         if (n < 2) {
@@ -75,7 +83,27 @@ public class Solution1763 {
             String s2 = longestNiceSubstring2(s.substring(i + 1));
             return s1.length() >= s2.length() ? s1 : s2;
         }
+        return s;
+    }
 
+    public String longestNiceSubstring3(String s) {
+        int n = s.length();
+        if (n < 2) {
+            return "";
+        }
+        Set<Character> set = new HashSet<>();
+        for (char ch : s.toCharArray()) {
+            set.add(ch);
+        }
+        for (int i = 0; i < n; i++) {
+            char temp = s.charAt(i);
+            if (set.contains(Character.toLowerCase(temp)) && set.contains(Character.toUpperCase(temp))) {
+                continue;
+            }
+            String s1 = longestNiceSubstring2(s.substring(0, i));
+            String s2 = longestNiceSubstring2(s.substring(i + 1));
+            return s1.length() >= s2.length() ? s1 : s2;
+        }
         return s;
     }
 }
