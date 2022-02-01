ff0c18b285d96f969fd34c6a906e099d50ee0824
xiaowangzi123 <1103694415@qq.com>
2022-02-01 15:54:37 +0800

    offer 0057 超时

--- Offer_2_practice/src/main/java/com/leetcode/OfferTwo0057.java
+++ Offer_2_practice/src/main/java/com/leetcode/OfferTwo0057.java
@@ -8,16 +8,20 @@ package com.leetcode;
 public class OfferTwo0057 {
     public static void main(String[] args) {
         OfferTwo0057 obj = new OfferTwo0057();
+        System.out.println(obj.containsNearbyAlmostDuplicate(new int[]{-2147483648, 2147483647}, 1, 1));
         System.out.println(obj.containsNearbyAlmostDuplicate(new int[]{1, 5, 9, 1, 5, 9}, 2, 3));
         System.out.println(obj.containsNearbyAlmostDuplicate(new int[]{1, 2, 3, 1}, 3, 0));
         System.out.println(obj.containsNearbyAlmostDuplicate(new int[]{1, 0, 1, 1}, 1, 2));
     }
 
+    /**
+     * 转换为long后，超时
+     */
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
         int n = nums.length;
         for (int i = 0; i < n; i++) {
-            for (int j = i + k; j < n; j++) {
-                if (Math.abs(nums[i] - nums[j]) <= t) {
+            for (int j = i + 1; j <= i + k && j < n; j++) {
+                if (Math.abs((long) nums[i] - (long) nums[j]) <= t) {
                     return true;
                 }
             }
