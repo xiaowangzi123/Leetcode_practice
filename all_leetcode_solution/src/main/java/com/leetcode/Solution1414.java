38aa55a71f722c618f9737eaa92bd1eb3273146c
xiaowangzi123 <1103694415@qq.com>
2022-02-03 10:04:28 +0800

    leetcode_0026 初始解答

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1414.java
@@ -0,0 +1,38 @@
+package com.leetcode;
+
+/**
+ * @author: wyq
+ * @create time: 2022/2/3
+ * @description: 和为 K 的最少斐波那契数字数目
+ */
+public class Solution1414 {
+    public static void main(String[] args) {
+        Solution1414 obj = new Solution1414();
+        System.out.println(obj.findMinFibonacciNumbers(7));
+        System.out.println(obj.findMinFibonacciNumbers(10));
+        System.out.println(obj.findMinFibonacciNumbers(19));
+    }
+
+    public int findMinFibonacciNumbers(int k) {
+        int n = 1;
+        while (getFibonacci(n) < k) {
+            n++;
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int temp = getFibonacci(n);
+            if (k >= temp) {
+                k -= temp;
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+    public int getFibonacci(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        return getFibonacci(n - 1) + getFibonacci(n - 2);
+    }
+}
