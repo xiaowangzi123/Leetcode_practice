881e3bc42bcbee5ed697e8e973cb74fc2009947f
xiaowangzi123 <1103694415@qq.com>
2022-02-03 10:17:11 +0800

    leetcode_0026 超时，需要改进

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1414.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1414.java
@@ -1,5 +1,8 @@
 package com.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author: wyq
  * @create time: 2022/2/3
@@ -8,12 +11,13 @@ package com.leetcode;
 public class Solution1414 {
     public static void main(String[] args) {
         Solution1414 obj = new Solution1414();
-        System.out.println(obj.findMinFibonacciNumbers(5));
-        System.out.println(obj.findMinFibonacciNumbers(7));
+        System.out.println(obj.findMinFibonacciNumbers2(5));
+        System.out.println(obj.findMinFibonacciNumbers2(7));
         System.out.println(obj.findMinFibonacciNumbers(10));
         System.out.println(obj.findMinFibonacciNumbers(19));
     }
 
+    //超时
     public int findMinFibonacciNumbers(int k) {
         int n = 1;
         while (getFibonacci(n) < k) {
@@ -38,5 +42,30 @@ public class Solution1414 {
             return 1;
         }
         return getFibonacci(n - 1) + getFibonacci(n - 2);
+    }
+
+
+    public int findMinFibonacciNumbers2(int k) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        int f1 = 1, f2 = 1;
+        while (f1 + f2 <= k) {
+            int temp = f1 + f2;
+            list.add(temp);
+            f1 = f2;
+            f2 = temp;
+        }
+        int ans = 0;
+        for (int i = list.size() - 1; i >= 0; i--) {
+            int temp = list.get(i);
+            if (k >= temp) {
+                k -= temp;
+                ans++;
+                if (k == 0) {
+                    return ans;
+                }
+            }
+        }
+        return ans;
     }
 }
