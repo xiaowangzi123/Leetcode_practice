1298c2d8dc3081fc1d267c47952ae4777dda47dd
xiaowangzi123 <1103694415@qq.com>
2022-02-04 12:09:45 +0800

    leetcode_0189 改进

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0189.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0189.java
@@ -10,7 +10,7 @@ public class Solution0189 {
     public static void main(String[] args) {
         Solution0189 obj = new Solution0189();
         int[] arr = {1, 2, 3, 4, 5, 6, 7};
-        obj.rotate(arr, 3);
+        obj.rotate2(arr, 3);
         System.out.println(Arrays.toString(arr));
 
     }
@@ -25,5 +25,14 @@ public class Solution0189 {
             }
             k--;
         }
+    }
+
+    public void rotate2(int[] nums, int k) {
+        int n = nums.length;
+        int[] temp = new int[n];
+        for (int i = 0; i < n; ++i) {
+            temp[(i + k) % n] = nums[i];
+        }
+        System.arraycopy(temp, 0, nums, 0, n);
     }
 }
