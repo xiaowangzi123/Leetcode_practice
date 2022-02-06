ba73867decd41c2dfb23bd51c8021fbb4031c4d2
xiaowangzi123 <1103694415@qq.com>
2022-02-06 21:49:52 +0800

    leetcode_0008

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0008.java
@@ -0,0 +1,18 @@
+package com.leetcode;
+
+/**
+ * @author: wyq
+ * @create time: 2022/2/6
+ * @description:  字符串转换整数 (atoi)
+ */
+public class Solution0008 {
+    public static void main(String[] args) {
+        Solution0008 obj = new Solution0008();
+    }
+
+    public int myAtoi(String s) {
+        return 0;
+    }
+
+
+}
