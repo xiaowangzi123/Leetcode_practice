23fddeb49c1444638aa70ec1f7d1bccf78cd18f1
xiaowangzi123 <1103694415@qq.com>
2022-02-19 22:34:08 +0800

    leetcode_0688

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0688.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0688.java
@@ -8,10 +8,16 @@ package com.leetcode;
 public class Solution0688 {
     public static void main(String[] args) {
         Solution0688 obj = new Solution0688();
+        System.out.println(obj.knightProbability(1, 0, 0, 0));
+        System.out.println(obj.knightProbability(3, 2, 0, 0));
     }
 
+    //八个方向
     static int[][] dirs = {{-2, -1}, {-2, 1}, {2, -1}, {2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}};
 
+    /**
+     * step：步数
+     */
     public double knightProbability(int n, int k, int row, int column) {
         double[][][] dp = new double[k + 1][n][n];
         for (int step = 0; step <= k; step++) {
