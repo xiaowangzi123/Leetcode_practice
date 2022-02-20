1f71db612ddb58c9c8fdccc3b6f52e50dba0b226
xiaowangzi123 <1103694415@qq.com>
2022-02-20 10:50:33 +0800

    leetcode_0688优化

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0688.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0688.java
@@ -1,5 +1,7 @@
 package com.leetcode;
 
+import java.util.Arrays;
+
 /**
  * @author: wyq
  * @create time: 2022/2/19
@@ -12,25 +14,30 @@ public class Solution0688 {
         System.out.println(obj.knightProbability(3, 2, 0, 0));
     }
 
-    //八个方向
+    /**
+     * 八个方向
+     */
     static int[][] dirs = {{-2, -1}, {-2, 1}, {2, -1}, {2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}};
 
     /**
      * step：步数
+     * 定义dp[step][i][j]表示骑士从棋盘上的点(i,j) 出发，走了step步时仍然留在棋盘上的概率
+     * 当点(i,j)不在棋盘上时，dp[step][i][j]=0
+     * 当点(i,j) 在棋盘上且step=0 时，dp[step][i][j]=1
      */
     public double knightProbability(int n, int k, int row, int column) {
         double[][][] dp = new double[k + 1][n][n];
-        for (int step = 0; step <= k; step++) {
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[0][i], 1);
+        }
+
+        for (int step = 1; step <= k; step++) {
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    if (step == 0) {
-                        dp[step][i][j] = 1;
-                    } else {
-                        for (int[] dir : dirs) {
-                            int ni = i + dir[0], nj = j + dir[1];
-                            if (ni >= 0 && ni < n && nj >= 0 && nj < n) {
-                                dp[step][i][j] += dp[step - 1][ni][nj] / 8;
-                            }
+                    for (int[] dir : dirs) {
+                        int x = i + dir[0], y = j + dir[1];
+                        if (x >= 0 && x < n && y >= 0 && y < n) {
+                            dp[step][i][j] += dp[step - 1][x][y] / 8;
                         }
                     }
                 }
