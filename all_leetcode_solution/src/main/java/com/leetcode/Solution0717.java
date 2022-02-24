660249fd6b3a30ba8a969d63775da5e6b7e54a19
xiaowangzi123 <1103694415@qq.com>
2022-02-24 21:36:50 +0800

    leetcode_0917

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0717.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0717.java
@@ -11,8 +11,13 @@ public class Solution0717 {
 //        int[] bits = {1, 0, 0};
         int[] bits = {1, 1, 1, 1, 0};
         System.out.println(obj.isOneBitCharacter(bits));
+        System.out.println(obj.isOneBitCharacter2(bits));
     }
 
+    /**
+     * 当前位是0，向前跳一位；
+     * 当前位是1，向前跳两位；
+     */
     public boolean isOneBitCharacter(int[] bits) {
         int i = 0;
         while (i < bits.length - 1) {
@@ -21,6 +26,15 @@ public class Solution0717 {
             } else {
                 i += 2;
             }
+        }
+        return i == bits.length - 1;
+    }
+
+    //
+    public boolean isOneBitCharacter2(int[] bits) {
+        int i = 0;
+        while (i < bits.length - 1) {
+            i += bits[i] + 1;
         }
         return i == bits.length - 1;
     }
