c2899f5ec4862eebb95be1d929f90d4bf0e329a8
xiaowangzi123 <1103694415@qq.com>
2022-02-25 23:01:53 +0800

    leetcode_0537

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0537.java
@@ -0,0 +1,14 @@
+package com.leetcode;
+
+/**
+ * @author: wyq
+ * @create time: 2022/2/25
+ * @description: TODO
+ * 复数乘法
+ */
+public class Solution0537 {
+    public static void main(String[] args) {
+        Solution0537 obj = new Solution0537();
+    }
+
+}
