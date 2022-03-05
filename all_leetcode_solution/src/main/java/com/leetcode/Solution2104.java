42f15c07f0d144c274d40f136a5077fc3a33ef05
xiaowangzi123 <1103694415@qq.com>
2022-03-05 17:14:50 +0800

    leetcode_2104

--- all_leetcode_solution/src/main/java/com/leetcode/Solution2104.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution2104.java
@@ -3,7 +3,7 @@ package com.leetcode;
 /**
  * @author: wyq
  * @create time: 2022/3/5
- * @description: TODO
+ * @description: 子数组范围和
  */
 public class Solution2104 {
     public static void main(String[] args) {
@@ -14,26 +14,11 @@ public class Solution2104 {
     }
 
     /**
-     *
+     * 子数组是数组中一个连续 非空 的元素序列。
      */
     public long subArrayRanges(int[] nums) {
         long ans = 0;
         int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
-            for (int j = i; j < n; j++) {
-                min = Math.min(min, nums[j]);
-                max = Math.max(max, nums[j]);
-                ans += (max - min);
-            }
-        }
-        return ans;
-    }
-
-    public long subArrayRanges2(int[] nums) {
-        long ans = 0;
-        int n = nums.length;
-//        Arrays.sort(nums);
         for (int i = 0; i < n; i++) {
             int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
             for (int j = i; j < n; j++) {
