7868e0ec1849e114440a81763a94cf311c7f4074
xiaowangzi123 <1103694415@qq.com>
2022-03-09 21:51:44 +0800

    leetcode_0798

--- all_leetcode_solution/src/main/java/com/leetcode/todo/Solution0798.java
+++ all_leetcode_solution/src/main/java/com/leetcode/todo/Solution0798.java
@@ -11,26 +11,59 @@ public class Solution0798 {
         Solution0798 obj = new Solution0798();
         System.out.println(obj.bestRotation(new int[]{2, 3, 1, 4, 0}));
         System.out.println(obj.bestRotation(new int[]{1, 3, 0, 2, 4}));
+
     }
 
+    //超时
     public int bestRotation(int[] nums) {
-        //k
-        int k = 0, n = nums.length, ans = 0;
+        int ans = 0;
+        int k = 0, n = nums.length, max = 0;
         while (k < n) {
-            for (int i = n - 1; i > 0; i--) {
-                int temp = nums[i];
-                nums[i] = nums[i - 1];
-                nums[i - 1] = temp;
-            }
-            int max = 0;
-            for (int i = 0; i < n; i++) {
-                if (nums[i] <= i) {
-                    max++;
+            if (k != 0) {
+                for (int i = 0; i < n - 1; i++) {
+                    int temp = nums[i];
+                    nums[i] = nums[i + 1];
+                    nums[i + 1] = temp;
                 }
             }
-            ans = Math.max(ans, max);
+
+            int temp = 0;
+            for (int i = 0; i < n; i++) {
+                if (nums[i] <= i) {
+                    temp++;
+                }
+            }
+            if (temp > max) {
+                max = temp;
+                ans = k;
+            }
             k++;
         }
         return ans;
+    }
+
+    public int bestRotation2(int[] nums) {
+        int n = nums.length;
+        int[] diffs = new int[n];
+        for (int i = 0; i < n; i++) {
+            int low = (i + 1) % n;
+            int high = (i - nums[i] + n + 1) % n;
+            diffs[low]++;
+            diffs[high]--;
+            if (low >= high) {
+                diffs[0]++;
+            }
+        }
+        int bestIndex = 0;
+        int maxScore = 0;
+        int score = 0;
+        for (int i = 0; i < n; i++) {
+            score += diffs[i];
+            if (score > maxScore) {
+                bestIndex = i;
+                maxScore = score;
+            }
+        }
+        return bestIndex;
     }
 }
