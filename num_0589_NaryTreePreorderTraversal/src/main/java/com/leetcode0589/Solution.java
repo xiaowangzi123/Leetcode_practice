1efe2974e1e2d2047cd88e343e87e7c860e5cc62
xiaowangzi123 <1103694415@qq.com>
2022-03-10 21:56:34 +0800

    leetcode_0589

--- num_0589_NaryTreePreorderTraversal/src/main/java/com/leetcode0589/Solution.java
+++ num_0589_NaryTreePreorderTraversal/src/main/java/com/leetcode0589/Solution.java
@@ -1,12 +1,48 @@
 package com.leetcode0589;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Hello world!
+ * @author: wyq
+ * @create time: 2022/3/10
+ * @description :
  */
 public class Solution {
     public static void main(String[] args) {
+        Node n5 = new Node(5);
+        Node n6 = new Node(6);
+        List<Node> list = new ArrayList<>();
+        list.add(n5);
+        list.add(n6);
+        Node n3 = new Node(3, list);
+        Node n2 = new Node(2);
+        Node n4 = new Node(4);
+        List<Node> list1 = new ArrayList<>();
+        list1.add(n3);
+        list1.add(n2);
+        list1.add(n4);
+        Node n1 = new Node(1, list1);
+
+        Solution obj = new Solution();
+        System.out.println(obj.preorder(n1));
+        System.out.println("--------------");
+        System.out.println(obj.preorder2(n1));
+
+    }
+
+    List<Integer> ans = new ArrayList<>();
+    public List<Integer> preorder2(Node root) {
+        if (root == null) {
+            return ans;
+        }
+        ans.add(root.val);
+        if (root.children != null) {
+            for (Node node : root.children) {
+                preorder2(node);
+            }
+        }
+        return ans;
     }
 
     public List<Integer> preorder(Node root) {
@@ -20,8 +56,10 @@ public class Solution {
             return;
         }
         res.add(root.val);
-        for (Node ch : root.children) {
-            helper(ch, res);
+        if (root.children != null) {
+            for (Node node : root.children) {
+                helper(node, res);
+            }
         }
     }
 }
