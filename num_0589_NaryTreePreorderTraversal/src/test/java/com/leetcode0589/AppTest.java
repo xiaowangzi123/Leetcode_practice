1fce53508262bd64325ed2876f11768dd28ef32f
xiaowangzi123 <1103694415@qq.com>
2022-03-10 20:35:31 +0800

    leetcode_0589

---
+++ num_0589_NaryTreePreorderTraversal/src/test/java/com/leetcode0589/AppTest.java
@@ -0,0 +1,20 @@
+package com.leetcode0589;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
