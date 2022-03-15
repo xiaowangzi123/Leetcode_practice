7064048305a59b1342be2de7f853f58e2d41f570
xiaowangzi123 <1103694415@qq.com>
2022-03-15 09:35:35 +0800

    leetcode_2044

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution2044.java
@@ -0,0 +1,35 @@
+package com.leetcode;
+
+/**
+ * @author: wyq
+ * @create time: 2022/3/15
+ * @description ：统计按位或能得到最大值的子集数目
+ * TODO
+ */
+public class Solution2044 {
+    public static void main(String[] args) {
+        Solution2044 obj = new Solution2044();
+        System.out.println(obj.countMaxOrSubsets(new int[]{3, 1}));
+        System.out.println(obj.countMaxOrSubsets(new int[]{2, 2, 2}));
+        System.out.println(obj.countMaxOrSubsets(new int[]{3, 2, 1, 5}));
+    }
+
+    public int countMaxOrSubsets(int[] nums) {
+        int maxOr = 0, cnt = 0;
+        for (int i = 0; i < 1 << nums.length; i++) {
+            int orVal = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (((i >> j) & 1) == 1) {
+                    orVal |= nums[j];
+                }
+            }
+            if (orVal > maxOr) {
+                maxOr = orVal;
+                cnt = 1;
+            } else if (orVal == maxOr) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
