21e206a9b40b62a2bdc13dfd49f16d4c66077d58
xiaowangzi123 <1103694415@qq.com>
2022-03-18 19:54:45 +0800

    leetcode_2043

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution2043.java
@@ -0,0 +1,48 @@
+package com.leetcode;
+
+/**
+ * @author: wyq
+ * @create time: 2022/3/15
+ * @description ：简易银行系统
+ * TODO
+ */
+public class Solution2043 {
+    public static void main(String[] args) {
+        Solution2043 obj = new Solution2043();
+
+    }
+
+}
+
+class Bank {
+    long[] balance;
+
+    public Bank(long[] balance) {
+        this.balance = balance;
+    }
+
+    public boolean transfer(int account1, int account2, long money) {
+        if (account1 > balance.length || account2 > balance.length || balance[account1 - 1] < money) {
+            return false;
+        }
+        balance[account1 - 1] -= money;
+        balance[account2 - 1] += money;
+        return true;
+    }
+
+    public boolean deposit(int account, long money) {
+        if (account > balance.length) {
+            return false;
+        }
+        balance[account - 1] += money;
+        return true;
+    }
+
+    public boolean withdraw(int account, long money) {
+        if (account > balance.length || balance[account - 1] < money) {
+            return false;
+        }
+        balance[account - 1] -= money;
+        return true;
+    }
+}
