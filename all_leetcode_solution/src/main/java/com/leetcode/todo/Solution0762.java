0621e20a8679637de97137f684c857893a47f5a4
xiaowangzi123 <1103694415@qq.com>
2022-04-05 21:13:11 +0800

    leetcode_0307

---
+++ all_leetcode_solution/src/main/java/com/leetcode/todo/Solution0762.java
@@ -0,0 +1,39 @@
+package com.leetcode.todo;
+
+/**
+ * @author: wyq
+ * @create time: 2022/4/5
+ * @description: TODO
+ * 二进制表示中质数个计算置位
+ */
+public class Solution0762 {
+    public static void main(String[] args) {
+        Solution0762 obj = new Solution0762();
+        System.out.println(obj.countPrimeSetBits(1, 22));
+        System.out.println(obj.countPrimeSetBits(47, 85));
+    }
+
+
+    public int countPrimeSetBits(int left, int right) {
+        int ans = 0;
+        for (int x = left; x <= right; ++x) {
+            if (isPrime(Integer.bitCount(x))) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+
+    private boolean isPrime(int x) {
+        if (x < 2) {
+            return false;
+        }
+        for (int i = 2; i * i <= x; ++i) {
+            if (x % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
