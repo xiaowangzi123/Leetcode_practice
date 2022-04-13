942943991e4ccb5706b6e51d73af5ef61aa89820
xiaowangzi123 <1103694415@qq.com>
2022-04-13 22:32:51 +0800

    leetcode_0380

---
+++ all_leetcode_solution/src/main/java/com/leetcode/todo/Solution0380.java
@@ -0,0 +1,56 @@
+package com.leetcode.todo;
+
+import java.util.*;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2022/4/13
+ * @description : TODO
+ * 380. O(1) 时间插入、删除和获取随机元素
+ */
+public class Solution0380 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class RandomizedSet {
+    List<Integer> nums;
+    Map<Integer, Integer> indices;
+    Random random;
+
+    public RandomizedSet() {
+        nums = new ArrayList<Integer>();
+        indices = new HashMap<Integer, Integer>();
+        random = new Random();
+    }
+
+    public boolean insert(int val) {
+        if (indices.containsKey(val)) {
+            return false;
+        }
+        int index = nums.size();
+        nums.add(val);
+        indices.put(val, index);
+        return true;
+    }
+
+    public boolean remove(int val) {
+        if (!indices.containsKey(val)) {
+            return false;
+        }
+        int index = indices.get(val);
+        int last = nums.get(nums.size() - 1);
+        nums.set(index, last);
+        indices.put(last, index);
+        nums.remove(nums.size() - 1);
+        indices.remove(val);
+        return true;
+    }
+
+    public int getRandom() {
+        int randomIndex = random.nextInt(nums.size());
+        return nums.get(randomIndex);
+    }
+}
+
