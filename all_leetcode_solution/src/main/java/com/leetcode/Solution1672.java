68ec97346f27f75a9b41d56cc976c444f7ba24f1
xiaowangzi123 <1103694415@qq.com>
2022-04-14 19:13:12 +0800

    leetcode_1672

--- all_leetcode_solution/src/main/java/com/leetcode/Solution1672.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution1672.java
@@ -16,6 +16,7 @@ public class Solution1672 {
         int max = Integer.MIN_VALUE;
         for (int i = 0; i < accounts.length; i++) {
             int sum = 0;
+            //每一行都可以看作是一个人的资产
             for (int j = 0; j < accounts[0].length; j++) {
                 sum += accounts[i][j];
             }
