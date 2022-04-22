06f458e712f7c2eeef3e48d2a52a419dfeb18c23
xiaowangzi123 <1103694415@qq.com>
2022-04-22 22:06:20 +0800

    leetcode_0388

---
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0388.java
@@ -0,0 +1,60 @@
+package com.leetcode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2021/10/9
+ * @description : TODO
+ * 388. 文件的最长绝对路径
+ */
+public class Solution0388 {
+    public static void main(String[] args) {
+        Solution0388 obj = new Solution0388();
+        System.out.println(obj.lengthLongestPath("dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext"));
+    }
+
+    public int lengthLongestPath(String input) {
+        int n = input.length();
+        int pos = 0;
+        int ans = 0;
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        while (pos < n) {
+            /* 检测当前文件的深度 */
+            int depth = 1;
+            while (pos < n && input.charAt(pos) == '\t') {
+                pos++;
+                depth++;
+            }
+            /* 统计当前文件名的长度 */
+            boolean isFile = false;
+            int len = 0;
+            while (pos < n && input.charAt(pos) != '\n') {
+                if (input.charAt(pos) == '.') {
+                    isFile = true;
+                }
+                len++;
+                pos++;
+            }
+            /* 跳过当前的换行符 */
+            pos++;
+
+            while (stack.size() >= depth) {
+                stack.pop();
+            }
+            if (!stack.isEmpty()) {
+                len += stack.peek() + 1;
+            }
+            if (isFile) {
+                ans = Math.max(ans, len);
+            } else {
+                stack.push(len);
+            }
+        }
+        return ans;
+    }
+
+
+}
