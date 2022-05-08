f6a11015ae2d20281962cd235f0bf8394e515da2
xiaowangzi123 <1103694415@qq.com>
2022-05-08 12:12:11 +0800

    leetcode_0422

--- all_leetcode_solution/src/main/java/com/leetcode/todo/Solution0422.java
+++ all_leetcode_solution/src/main/java/com/leetcode/todo/Solution0422.java
@@ -6,7 +6,8 @@ import java.util.List;
 /**
  * @author :wyq
  * @date ：Created in 2022/5/8
- * @description :
+ * @description :TODO
+ * 442. 数组中重复的数据
  */
 public class Solution0422 {
     public static void main(String[] args) {
@@ -14,6 +15,10 @@ public class Solution0422 {
         System.out.println(obj.findDuplicates(new int[]{4, 3, 2, 7, 8, 2, 3, 1}));
     }
 
+    /**
+     * 可以把每个元素值当做下标去处理
+     * 其值取反，如果有同样的元素，其值就变成了正数
+     */
     public List<Integer> findDuplicates(int[] nums) {
         List<Integer> res = new ArrayList<>();
         for (int num : nums) {
@@ -25,4 +30,5 @@ public class Solution0422 {
         }
         return res;
     }
+
 }
