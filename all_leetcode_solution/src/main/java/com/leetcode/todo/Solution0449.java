e4188ab1bfb29eb3186316b71199ee17dc9f7206
xiaowangzi123 <1103694415@qq.com>
2022-05-11 23:36:44 +0800

    leetcode_1728

---
+++ all_leetcode_solution/src/main/java/com/leetcode/todo/Solution0449.java
@@ -0,0 +1,12 @@
+package com.leetcode.todo;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2022/5/11
+ * @description :
+ */
+public class Solution0449 {
+    public static void main(String[] args) {
+
+    }
+}
