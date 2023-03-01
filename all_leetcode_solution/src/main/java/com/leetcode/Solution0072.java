2d830003d2203a80e316f85127dc76bb6e7251d7
xiaowangzi123 <1103694415@qq.com>
2023-03-01 15:30:40 +0800

    LeetCode_0072

--- all_leetcode_solution/src/main/java/com/leetcode/Solution0072.java
+++ all_leetcode_solution/src/main/java/com/leetcode/Solution0072.java
@@ -8,9 +8,9 @@ package com.leetcode;
 public class Solution0072 {
     public static void main(String[] args) {
         Solution0072 obj = new Solution0072();
-        System.out.println(obj.minDistance("a","ab"));
-        System.out.println(obj.minDistance("a","b"));
-        System.out.println(obj.minDistance("中文有很多人用","中国有很多人"));
+        System.out.println(obj.minDistance("a", "ab"));
+        System.out.println(obj.minDistance("a", "b"));
+        System.out.println(obj.minDistance("中文有很多人用", "中国有很多人"));
     }
 
     public int minDistance(String word1, String word2) {
@@ -47,6 +47,33 @@ public class Solution0072 {
         }
         return D[n][m];
     }
+
+    // 求三个数的最小值的实用函数
+    public static int minimum(int a, int b, int c) {
+        return Integer.min(a, Integer.min(b, c));
+    }
+
+    // 查找字符串 `X` 和 `Y` 之间的 Levenshtein 距离的函数。
+    // `m` 和 `n` 分别是 `X` 和 `Y` 中的字符总数
+    public static int dist(String X, int m, String Y, int n) {
+        // 基本情况：空字符串(情况 1)
+        if (m == 0) {
+            return n;
+        }
+
+        if (n == 0) {
+            return m;
+        }
+
+        // 如果字符串的最后一个字符匹配(案例 2)
+        int cost = (X.charAt(m - 1) == Y.charAt(n - 1)) ? 0 : 1;
+
+        return minimum(dist(X, m - 1, Y, n) + 1,    // 删除(案例 3a))
+                dist(X, m, Y, n - 1) + 1,           // 插入(案例 3b))
+                dist(X, m - 1, Y, n - 1) + cost);   // 替换(案例 2 和 3c)
+    }
+
+
 }
 
 
