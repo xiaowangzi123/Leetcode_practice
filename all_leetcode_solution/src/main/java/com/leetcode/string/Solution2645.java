87c6f0f732c00456184e04244fb289c34e36d3e4
xiaowangzi123 <1103694415@qq.com>
2024-01-11 13:43:16 +0800

    LeetCode_0072

--- all_leetcode_solution/src/main/java/com/leetcode/string/Solution2645.java
+++ all_leetcode_solution/src/main/java/com/leetcode/string/Solution2645.java
@@ -19,21 +19,33 @@ public class Solution2645 {
      * 分组，对于abac，可以分成ab,ac,两组，每组填充1个字母
      */
     public int addMinimum(String word) {
+        int count = 0;
+        for (int i = 1; i < word.length(); i++) {
+            if (word.charAt(i - 1) >= word.charAt(i)) {
+                count++;
+            }
+        }
+        count++;
+        return count * 3 - word.length();
+    }
+
+
+    public int addMinimum2(String word) {
 
         List<String> strings = groupContinuous(word);
-        return 3*strings.size()-word.length();
+        return 3 * strings.size() - word.length();
     }
 
     public static List<String> groupContinuous(String str) {
         List<String> result = new ArrayList<>();
-        if (str == null || str.length()==0) {
+        if (str == null || str.length() == 0) {
             return new ArrayList<>();
         }
         StringBuilder temp = new StringBuilder();
         for (int i = 1; i < str.length(); i++) {
-            if (str.charAt(i) > str.charAt(i-1)){
+            if (str.charAt(i) > str.charAt(i - 1)) {
                 temp.append(str.charAt(i));
-            }else {
+            } else {
                 result.add(temp.toString());
                 temp.setLength(0);
                 temp.append(str.charAt(i));
