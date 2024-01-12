7250be99d167ea3274bebe2176154d5e0dc280d2
xiaowangzi123 <1103694415@qq.com>
2024-01-12 11:59:52 +0800

    2696

---
+++ all_leetcode_solution/src/main/java/com/leetcode/s2701_2800/Solution2707.java
@@ -0,0 +1,17 @@
+package com.leetcode.s2701_2800;
+
+/**
+ * @author wyq
+ * @date 2024/1/12
+ * @desc 字符串中的额外字符串
+ */
+public class Solution2707 {
+    public static void main(String[] args) {
+
+    }
+
+    public int minExtraChar(String s, String[] dictionary) {
+
+        return -1;
+    }
+}
