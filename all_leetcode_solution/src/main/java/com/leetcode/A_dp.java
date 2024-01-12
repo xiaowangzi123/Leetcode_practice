1e1f16141621711e3bf25c86e60e3611905dcd6f
xiaowangzi123 <1103694415@qq.com>
2024-01-12 16:18:22 +0800

    2707

--- all_leetcode_solution/src/main/java/com/leetcode/A_dp.java
+++ all_leetcode_solution/src/main/java/com/leetcode/A_dp.java
@@ -5,9 +5,9 @@ import com.leetcode.s2701_2800.Solution2707;
 /**
  * 动态规划
  */
-public class Ahuizong {
+public class A_dp {
     /**
      *
-     * {@link Solution2707}
+     * {@link Solution2707} 2707字符串中的额外字符串
      */
 }
--- all_leetcode_solution/src/main/java/com/leetcode/s2701_2800/Solution2707.java
+++ all_leetcode_solution/src/main/java/com/leetcode/s2701_2800/Solution2707.java
@@ -1,5 +1,9 @@
 package com.leetcode.s2701_2800;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author wyq
  * @date 2024/1/12
@@ -8,10 +12,27 @@ package com.leetcode.s2701_2800;
 public class Solution2707 {
     public static void main(String[] args) {
 
+
     }
 
     public int minExtraChar(String s, String[] dictionary) {
 
-        return -1;
+        int n = s.length();
+        int[] d = new int[n + 1];
+        Arrays.fill(d, Integer.MAX_VALUE);
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for (String str : dictionary) {
+            map.put(str, map.getOrDefault(str, 0) + 1);
+        }
+        d[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            d[i] = d[i - 1] + 1;
+            for (int j = i - 1; j >= 0; j--) {
+                if (map.containsKey(s.substring(j, i))) {
+                    d[i] = Math.min(d[i], d[j]);
+                }
+            }
+        }
+        return d[n];
     }
 }
