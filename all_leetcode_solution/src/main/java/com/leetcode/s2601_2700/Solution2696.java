016d4fd3f815115e6fc36c593385ffddc082b111
xiaowangzi123 <1103694415@qq.com>
2024-01-12 11:56:25 +0800

    2696

--- all_leetcode_solution/src/main/java/com/leetcode/s2601_2700/Solution2696.java
+++ all_leetcode_solution/src/main/java/com/leetcode/s2601_2700/Solution2696.java
@@ -1,7 +1,9 @@
 package com.leetcode.s2601_2700;
 
+import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @author wyq
@@ -11,27 +13,9 @@ import java.util.LinkedList;
 public class Solution2696 {
     public static void main(String[] args) {
         Solution2696 solution = new Solution2696();
-        System.out.println(solution.minLength2("ABFCACDB"));
-        System.out.println(solution.minLength2("ACBBD"));
-    }
+        System.out.println(solution.minLength("ABFCACDB"));
+        System.out.println(solution.minLength("ACBBD"));
 
-    public int minLength(String s) {
-        Deque<Character> queue = new LinkedList<>();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == 'B' || c == 'D') {
-                if ('B' == c && !queue.isEmpty() && queue.peek() == 'A') {
-                    queue.poll();
-                } else if ('D' == c && !queue.isEmpty() && queue.peek() == 'C') {
-                    queue.poll();
-                } else {
-                    queue.addFirst(c);
-                }
-            } else {
-                queue.addFirst(c);
-            }
-        }
-        return queue.size();
     }
 
     public int minLength2(String s) {
@@ -44,6 +28,21 @@ public class Solution2696 {
                 queue.poll();
             } else {
                 queue.addFirst(c);
+            }
+        }
+        return queue.size();
+    }
+
+    public int minLength(String s) {
+        List<Character> queue = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if ('B' == c && !queue.isEmpty() && queue.get(queue.size() - 1) == 'A') {
+                queue.remove(queue.size() - 1);
+            } else if ('D' == c && !queue.isEmpty() && queue.get(queue.size() - 1) == 'C') {
+                queue.remove(queue.size() - 1);
+            } else {
+                queue.add(c);
             }
         }
         return queue.size();
