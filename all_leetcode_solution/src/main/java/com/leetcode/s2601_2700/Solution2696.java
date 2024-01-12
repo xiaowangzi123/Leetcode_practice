6b5ece17072d0232d2199e03b1508ea95b347364
xiaowangzi123 <1103694415@qq.com>
2024-01-12 11:36:43 +0800

    2696

--- all_leetcode_solution/src/main/java/com/leetcode/s2601_2700/Solution2696.java
+++ all_leetcode_solution/src/main/java/com/leetcode/s2601_2700/Solution2696.java
@@ -1,5 +1,8 @@
 package com.leetcode.s2601_2700;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * @author wyq
  * @date 2024/1/12
@@ -7,15 +10,26 @@ package com.leetcode.s2601_2700;
  */
 public class Solution2696 {
     public static void main(String[] args) {
-        Solution2696 solution =new Solution2696();
+        Solution2696 solution = new Solution2696();
         System.out.println(solution.minLength("ABFCACDB"));
+        System.out.println(solution.minLength("ACBBD"));
     }
 
     public int minLength(String s) {
-        s = s.replaceAll("AB", "");
-        s = s.replaceAll("CD", "");
-        s = s.replaceAll("AB", "");
-        s = s.replaceAll("CD", "");
-        return s.length();
+        Deque<Character> queue = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == 'B' || c == 'D') {
+                if ('B' == c && !queue.isEmpty() && queue.peek() == 'A') {
+                    queue.poll();
+                }
+                if ('D' == c && !queue.isEmpty() && queue.peek() == 'C') {
+                    queue.poll();
+                }
+            } else {
+                queue.addFirst(c);
+            }
+        }
+        return queue.size();
     }
 }
