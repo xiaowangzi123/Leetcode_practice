5709783259e8026b68e041291e7f45b6812580f3
xiaowangzi123 <1103694415@qq.com>
2024-01-13 22:35:48 +0800

    2182

--- all_leetcode_solution/src/main/java/com/leetcode/s2101_2200/Solution2182.java
+++ all_leetcode_solution/src/main/java/com/leetcode/s2101_2200/Solution2182.java
@@ -13,13 +13,15 @@ public class Solution2182 {
         String s = "cczazcc";
         int repeatLimit = 3;
         System.out.println(solution.repeatLimitedString(s, repeatLimit));
+        System.out.println(solution.repeatLimitedString2(s, repeatLimit));
         System.out.println(solution.repeatLimitedString(s, 1));
+        System.out.println(solution.repeatLimitedString2(s, 1));
     }
 
     public static int SIZE = 26;
 
     public String repeatLimitedString(String s, int repeatLimit) {
-        int m = 0;
+        int m = 0; //当前字母已经连续的个数
         int[] cnt = new int[SIZE];
         for (int i = 0; i < s.length(); i++) {
             cnt[s.charAt(i) - 'a']++;
@@ -44,5 +46,42 @@ public class Solution2182 {
 
         }
         return sb.toString();
+    }
+
+    public String repeatLimitedString2(String s, int repeatLimit) {
+        int[] cnt = new int[SIZE];
+        for (int i = 0; i < s.length(); i++) {
+            cnt[s.charAt(i) - 'a']++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = SIZE - 1, j = SIZE - 2; i >= 0 && j >= 0; ) {
+            if (cnt[i] == 0) {
+                i--;
+                j--;
+            }else if (cnt[i] <= repeatLimit){
+                getStr(sb,cnt[i],(char) ('a' + i));
+                i--;
+                j--;
+            }else {
+                while (cnt[i]>repeatLimit && j>=0){
+                    cnt[i]=-repeatLimit;
+                    getStr(sb,repeatLimit,(char) ('a' + i));
+                    if (cnt[j]==0){
+                        j--;
+                    }else {
+                        sb.append((char) ('a' + j));
+                    }
+                }
+            }
+        }
+
+        return sb.toString();
+
+    }
+
+    public void getStr(StringBuilder s,int n,char c){
+        for (int i=0;i<n;i++){
+            s.append(c);
+        }
     }
 }
