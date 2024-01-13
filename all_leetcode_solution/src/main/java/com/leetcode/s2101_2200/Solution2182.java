528c2ba38457cca231d68e0332fa42f2e2f39301
xiaowangzi123 <1103694415@qq.com>
2024-01-13 19:15:36 +0800

    2182

--- all_leetcode_solution/src/main/java/com/leetcode/s2101_2200/Solution2182.java
+++ all_leetcode_solution/src/main/java/com/leetcode/s2101_2200/Solution2182.java
@@ -1,6 +1,5 @@
 package com.leetcode.s2101_2200;
 
-import sun.lwawt.macosx.CSystemTray;
 
 /**
  * @author wyq
@@ -12,14 +11,15 @@ public class Solution2182 {
     public static void main(String[] args) {
         Solution2182 solution = new Solution2182();
         String s = "cczazcc";
-        Integer repeatLimit = 3;
+        int repeatLimit = 3;
         System.out.println(solution.repeatLimitedString(s, repeatLimit));
+        System.out.println(solution.repeatLimitedString(s, 1));
     }
 
     public static int SIZE = 26;
 
     public String repeatLimitedString(String s, int repeatLimit) {
-        int count = 0;
+        int m = 0;
         int[] cnt = new int[SIZE];
         for (int i = 0; i < s.length(); i++) {
             cnt[s.charAt(i) - 'a']++;
@@ -27,6 +27,20 @@ public class Solution2182 {
         StringBuilder sb = new StringBuilder();
 
         for (int i = SIZE - 1, j = SIZE - 2; i >= 0 && j >= 0; ) {
+            if (cnt[i] == 0) {
+                m = 0;
+                i--;
+            } else if (m < repeatLimit) {
+                cnt[i]--;
+                sb.append((char) ('a' + i));
+                m++;
+            } else if (j >= i || cnt[j] == 0) {
+                j--;
+            } else {
+                cnt[j]--;
+                sb.append((char) ('a' + j));
+                m = 0;
+            }
 
         }
         return sb.toString();
