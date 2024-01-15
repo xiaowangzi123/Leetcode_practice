362eb96979d34db881bdab9b966d72f8758e48a4
xiaowangzi123 <1103694415@qq.com>
2024-01-15 09:26:54 +0800

    2182

--- all_leetcode_solution/src/main/java/com/leetcode/A_Linked_List.java
+++ all_leetcode_solution/src/main/java/com/leetcode/A_Linked_List.java
@@ -6,7 +6,7 @@ package com.leetcode;
 public interface A_Linked_List {
 
     /**
-     *
+     * {@link com.leetcode.s0001_0100.Solution0082} 0082. 删除排序连表中的重复元素Ⅱ
      * {@link com.leetcode.s0001_0100.Solution0083} 0083. 删除排序连表中的重复元素
      */
 }
---
+++ all_leetcode_solution/src/main/java/com/leetcode/s0001_0100/Solution0082.java
@@ -0,0 +1,34 @@
+package com.leetcode.s0001_0100;
+
+import com.leetcode.bean.ListNode;
+
+/**
+ * @author :wyq
+ * @date ：Created in 2021/9/30
+ * @description : 删除排序链表中的重复元素Ⅱ
+ */
+public class Solution0082 {
+    public static void main(String[] args) {
+        Solution0082 obj = new Solution0082();
+        ListNode list = new ListNode(1);
+        list.next = new ListNode(1);
+        list.next.next = new ListNode(1);
+        System.out.println(obj.deleteDuplicates(list));
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode temp = new ListNode(0,head);
+        ListNode cur = temp;
+        while (cur.next != null) {
+            if (cur.val == cur.next.val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return temp.next;
+    }
+}
