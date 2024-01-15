c7e2d65ae9b29135a0e8fb6bf4be6f6ef39d8669
xiaowangzi123 <1103694415@qq.com>
2024-01-15 11:42:58 +0800

    2707

--- all_leetcode_solution/src/main/java/com/leetcode/s2701_2800/Solution2707.java
+++ all_leetcode_solution/src/main/java/com/leetcode/s2701_2800/Solution2707.java
@@ -1,8 +1,6 @@
 package com.leetcode.s2701_2800;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author wyq
@@ -14,7 +12,7 @@ public class Solution2707 {
         Solution2707 solution = new Solution2707();
         String s = "leetscleetode";
         String[] dictionary = new String[]{"leet", "code", "leetcode"};
-        System.out.println(solution.minExtraChar(s, dictionary));
+        System.out.println(solution.minExtraChar2(s, dictionary));
     }
 
     /**
@@ -35,6 +33,25 @@ public class Solution2707 {
             d[i] = d[i - 1] + 1;
             for (int j = i - 1; j >= 0; j--) {
                 if (map.containsKey(s.substring(j, i))) {
+                    d[i] = Math.min(d[i], d[j]);
+                }
+            }
+        }
+        return d[n];
+    }
+
+    public int minExtraChar2(String s, String[] dictionary) {
+
+        int n = s.length();
+        int[] d = new int[n + 1];
+        Arrays.fill(d, Integer.MAX_VALUE);
+        Set<String> set = new HashSet<>();
+        Collections.addAll(set, dictionary);
+        d[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            d[i] = d[i - 1] + 1;
+            for (int j = i - 1; j >= 0; j--) {
+                if (set.contains(s.substring(j, i))) {
                     d[i] = Math.min(d[i], d[j]);
                 }
             }
