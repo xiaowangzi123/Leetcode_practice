35c67aaec83ce5ffd07b8b0ce4952e1c6b7a51de
xiaowangzi123 <1103694415@qq.com>
2024-01-15 17:12:05 +0800

    2707-递归

--- all_leetcode_solution/src/main/java/com/leetcode/s2701_2800/Solution2707.java
+++ all_leetcode_solution/src/main/java/com/leetcode/s2701_2800/Solution2707.java
@@ -35,7 +35,9 @@ public class Solution2707 {
             d[i] = d[i - 1] + 1;
             //第二种切分方式，两者取最小值
             for (int j = i - 1; j >= 0; j--) {
-                if (map.containsKey(s.substring(j, i))) {
+                String word = s.substring(j, i);
+                //word存在于字典中时，此时d[i]=d[j]
+                if (map.containsKey(word)) {
                     d[i] = Math.min(d[i], d[j]);
                 }
             }
@@ -43,6 +45,9 @@ public class Solution2707 {
         return d[n];
     }
 
+    /**
+     * 优化
+     */
     public int minExtraChar2(String s, String[] dictionary) {
 
         int n = s.length();
@@ -63,32 +68,56 @@ public class Solution2707 {
     }
 
 
+    /**
+     * 递归方式
+     */
     public int minExtraChar3(String s, String[] dictionary) {
         Set<String> set = new HashSet<>(Arrays.asList(dictionary));
-        Map<Integer, Integer> memo = new HashMap<>();
-        return minExtraCharRecursive(s, set, s.length(), memo);
+//        Map<Integer, Integer> memo = new HashMap<>();
+//        return minExtraCharRecursive(s, set, s.length(), memo);
+        return recursive(s, set, s.length());
     }
 
-    private int minExtraCharRecursive(String s, Set<String> set, int end, Map<Integer, Integer> memo) {
-        if (end == 0) {
+    private int minExtraCharRecursive(String s, Set<String> set, int len, Map<Integer, Integer> memo) {
+        if (len == 0) {
             return 0;
         }
 
-        if (memo.containsKey(end)) {
-            return memo.get(end);
+        if (memo.containsKey(len)) {
+            return memo.get(len);
         }
 
-        int minExtra = minExtraCharRecursive(s, set, end - 1, memo) + 1;
+        int minExtra = minExtraCharRecursive(s, set, len - 1, memo) + 1;
 
-        for (int j = end - 1; j >= 0; j--) {
-            String substring = s.substring(j, end);
+        for (int j = len - 1; j >= 0; j--) {
+            String substring = s.substring(j, len);
             if (set.contains(substring)) {
                 minExtra = Math.min(minExtra, minExtraCharRecursive(s, set, j, memo));
             }
         }
 
-        memo.put(end, minExtra);
+        memo.put(len, minExtra);
         return minExtra;
     }
 
+
+    /**
+     * 没有使用记忆化技术，每次计算子串，导致最后超时
+     */
+    private int recursive(String s, Set<String> set, int len) {
+        if (len == 0) {
+            return 0;
+        }
+
+        int minExtra = recursive(s, set, len - 1) + 1;
+
+        for (int j = len - 1; j >= 0; j--) {
+            String word = s.substring(j, len);
+            if (set.contains(word)) {
+                minExtra = Math.min(minExtra, recursive(s, set, j));
+            }
+        }
+
+        return minExtra;
+    }
 }
