e82c7468e4e766388ee7f210c3e898b21c3775c8
xiaowangzi123 <1103694415@qq.com>
2024-01-15 14:04:58 +0800

    447

---
+++ all_leetcode_solution/src/main/java/com/leetcode/s0001_0100/Solution0447.java
@@ -0,0 +1,19 @@
+package com.leetcode.s0001_0100;
+
+/**
+ * @author wyq
+ * @date 2024/1/15
+ * @desc
+ */
+public class Solution0447 {
+    public static void main(String[] args) {
+        Solution0447 solution = new Solution0447();
+        int[][] points = {{0, 0},{1, 0},{2, 0}};
+        System.out.println(solution.numberOfBoomerangs(points));
+    }
+
+    public int numberOfBoomerangs(int[][] points) {
+
+        return 0;
+    }
+}
