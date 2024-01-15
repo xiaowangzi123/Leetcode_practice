ce3d1641f76d43391bd3745ebe72afb1536b553c
xiaowangzi123 <1103694415@qq.com>
2024-01-15 17:13:01 +0800

    2707-递归

--- all_leetcode_solution/src/main/java/com/leetcode/A_Recursion.java
+++ all_leetcode_solution/src/main/java/com/leetcode/A_Recursion.java
@@ -3,8 +3,9 @@ package com.leetcode;
 /**
  * 递归
  */
-public class A_Recursion {
+public interface A_Recursion {
     /**
+     *
      * {@link com.leetcode.s2701_2800.Solution2707} 2707字符串中的额外字符串
      *
      */
