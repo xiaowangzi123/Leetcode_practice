441465c3bc75ea9d337cc26c1cf51b4cf5197c42
xiaowangzi123 <1103694415@qq.com>
2024-01-15 09:35:56 +0800

    2182-s

--- all_leetcode_solution/src/main/java/com/leetcode/s0001_0100/Solution0082.java
+++ all_leetcode_solution/src/main/java/com/leetcode/s0001_0100/Solution0082.java
@@ -20,11 +20,14 @@ public class Solution0082 {
         if (head == null) {
             return head;
         }
-        ListNode temp = new ListNode(0,head);
+        ListNode temp = new ListNode(0, head);
         ListNode cur = temp;
-        while (cur.next != null) {
-            if (cur.val == cur.next.val) {
-                cur.next = cur.next.next;
+        while (cur.next != null && cur.next.next != null) {
+            if (cur.next.val == cur.next.next.val) {
+                int value = cur.next.val;
+                while (cur.next != null && cur.next.val == value) {
+                    cur.next = cur.next.next;
+                }
             } else {
                 cur = cur.next;
             }
