02fbb2aa85cbf157d9ff1379c2c4b57ca0c77014
xiaowangzi123 <1103694415@qq.com>
2024-01-17 09:44:47 +0800

    2744数组

--- all_leetcode_solution/src/main/java/com/leetcode/A_Array.java
+++ all_leetcode_solution/src/main/java/com/leetcode/A_Array.java
@@ -6,7 +6,8 @@ package com.leetcode;
 public interface A_Array {
 
     /**
-     *
      * {@link com.leetcode.s0001_0100.Solution0447} 0447.回旋镖的数量
+     *
+     * {@link com.leetcode.s2701_2800.Solution2744} 2744.最大字符串匹配数目
      */
 }
