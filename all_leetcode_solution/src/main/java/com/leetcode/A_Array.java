1191ee01b55b4b2bede2e7a903af9a389156575a
xiaowangzi123 <1103694415@qq.com>
2024-01-18 09:48:49 +0800

    2171

--- all_leetcode_solution/src/main/java/com/leetcode/A_Array.java
+++ all_leetcode_solution/src/main/java/com/leetcode/A_Array.java
@@ -7,7 +7,7 @@ public interface A_Array {
 
     /**
      * {@link com.leetcode.s0001_0100.Solution0447} 0447.回旋镖的数量
-     *
+     * {@link com.leetcode.s2101_2200.Solution2171} 2171.拿出最少数目的魔法豆
      * {@link com.leetcode.s2701_2800.Solution2744} 2744.最大字符串匹配数目
      */
 }
