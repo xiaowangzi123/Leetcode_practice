39f1461dfc030e53518b5ffe32fc08ddc9987231
xiaowangzi123 <1103694415@qq.com>
2024-01-20 20:37:21 +0800

    2788

---
+++ all_leetcode_solution/src/main/java/com/leetcode/s2701_2800/Solution2788.java
@@ -0,0 +1,46 @@
+package com.leetcode.s2701_2800;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 按分隔符拆分字符串
+ */
+public class Solution2788 {
+    public static void main(String[] args) {
+        Solution2788 solution = new Solution2788();
+        List<String> word = new ArrayList<>(Arrays.asList("one.two.three", "four.five", "six"));
+        List<String> word2 = new ArrayList<>(Arrays.asList("$easy$","$problem$"));
+        System.out.println(solution.splitWordsBySeparator(word, '.'));
+        System.out.println(solution.splitWordsBySeparator(word2, '$'));
+
+
+    }
+
+    public List<String> splitWordsBySeparator(List<String> words, char separator) {
+        List<String> res = new ArrayList<>();
+        for (String word:words){
+            res.addAll(getStrList(word, String.valueOf(separator)));
+        }
+        return res;
+    }
+
+    //
+    public List<String> getStrList(String s, String separator) {
+        if (s == null || s.isEmpty()) {
+            return new ArrayList<>();
+        }
+        int index = s.indexOf(separator);
+        List<String> res = new ArrayList<>();
+        while (index != -1) {
+            if (index != 0) {
+                res.add(s.substring(0, index));
+            }
+            s = s.substring(index+separator.length());
+            index = s.indexOf(separator);
+        }
+        res.add(s);
+        return res;
+    }
+}
