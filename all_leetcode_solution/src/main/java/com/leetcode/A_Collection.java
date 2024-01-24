1385ac53573446225669ed5a1ebe3b970231d89f
xiaowangzi123 <1103694415@qq.com>
2024-01-24 14:03:03 +0800

    2865

---
+++ all_leetcode_solution/src/main/java/com/leetcode/A_Collection.java
@@ -0,0 +1,14 @@
+package com.leetcode;
+
+/**
+ * 集合
+ */
+public interface A_Collection {
+
+    /**
+     *
+     * {@link com.leetcode.s0201_0300.Solution0118} 0118.杨辉三角
+     *
+     *
+     */
+}
--- all_leetcode_solution/src/main/java/com/leetcode/s0101_0200/Solution0118.java
+++ all_leetcode_solution/src/main/java/com/leetcode/s0101_0200/Solution0118.java
@@ -1,12 +1,10 @@
-package com.leetcode;
+package com.leetcode.s0101_0200;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @author ：wyq
- * @date ：Created in 2021/9/16
- * @description : 杨辉三角
+ * 杨辉三角
  */
 public class Solution0118 {
     public static void main(String[] args) {
@@ -18,6 +16,9 @@ public class Solution0118 {
         System.out.println(obj.generate2(4));
     }
 
+    /**
+     * 直接遍历
+     */
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> res = new ArrayList<>();
         if (numRows == 0) {
@@ -44,12 +45,11 @@ public class Solution0118 {
 
     public List<List<Integer>> generate2(int numRows) {
         List<List<Integer>> res = new ArrayList<>();
-        ArrayList<Integer> arr = new ArrayList<>();
+        List<Integer> arr = new ArrayList<>();
 
         for (int i = 0; i < numRows; i++) {
             arr.add(0, 1);
-
-            for (int j = 1; j < arr.size() - 1; ++j) {
+            for (int j = 1; j < arr.size() - 1; j++) {
                 arr.set(j, arr.get(j) + arr.get(j + 1));
             }
             res.add(new ArrayList<>(arr));
