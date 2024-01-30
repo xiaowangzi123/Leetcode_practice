1ea2056dc1a819d92d8a318ca5d6bfbe54b9a949
xiaowangzi123 <1103694415@qq.com>
2024-01-30 11:06:50 +0800

    2861

---
+++ all_leetcode_solution/src/main/java/com/leetcode/A_Map.java
@@ -0,0 +1,14 @@
+package com.leetcode;
+
+/**
+ * 哈希表
+ */
+public interface A_Map {
+
+    /**
+     *
+     * {@link com.leetcode.s2801_2900.Solution2808} 2808.使循环数组所有元素相等的最少秒数
+     *
+     *
+     */
+}
