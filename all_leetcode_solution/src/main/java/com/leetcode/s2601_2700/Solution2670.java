9b9d54598bab2aa7fcf41d589aab6a27cdce45f5
xiaowangzi123 <1103694415@qq.com>
2024-01-31 11:24:58 +0800

    2670

--- all_leetcode_solution/src/main/java/com/leetcode/s2601_2700/Solution2670.java
+++ all_leetcode_solution/src/main/java/com/leetcode/s2601_2700/Solution2670.java
@@ -11,12 +11,18 @@ public class Solution2670 {
     public static void main(String[] args) {
         Solution2670 solution = new Solution2670();
         int[] nums = new int[]{1, 2, 3, 4, 5};
+        int[] nums2 = new int[]{3,2,3,4,2};
         System.out.println(Arrays.toString(solution.distinctDifferenceArray(nums)));
-
+        System.out.println(Arrays.toString(solution.distinctDifferenceArray(nums2)));
     }
 
+    /**
+     *
+     */
     public int[] distinctDifferenceArray(int[] nums) {
+        //当前锁定，不同元素的个数
         int[] count = new int[nums.length + 1];
+        //记录不同的元素
         Set<Integer> set = new HashSet<>();
         for (int i = nums.length - 1; i >= 0; i--) {
             set.add(nums[i]);
