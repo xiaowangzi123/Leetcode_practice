9c6737f4afc9b9b4f78a167cf7afc7a40c4d8ef9
xiaowangzi123 <1103694415@qq.com>
2024-02-21 13:50:35 +0800

    树

--- all_leetcode_solution/src/main/java/com/leetcode/A_Tree.java
+++ all_leetcode_solution/src/main/java/com/leetcode/A_Tree.java
@@ -8,6 +8,7 @@ public interface A_Tree {
     /**
      *
      * {@link com.leetcode.s0101_0200.Solution0105} 0105.从前序与中序遍历序列构造二叉树
+     * {@link com.leetcode.s0101_0200.Solution0106} 0106.从中序与后序遍历序列构造二叉树
      * {@link com.leetcode.s0501_0600.Solution0589} 0589.N叉树的前序遍历
      * {@link com.leetcode.s0501_0600.Solution0590} 0590.N叉树的后序遍历
      *
