77791819f45866b17a890fd41e04f6fcd95e523c
xiaowangzi123 <1103694415@qq.com>
2024-02-28 16:36:48 +0800

    2673

--- all_leetcode_solution/src/main/java/com/leetcode/s2601_2700/Solution2673.java
+++ all_leetcode_solution/src/main/java/com/leetcode/s2601_2700/Solution2673.java
@@ -27,4 +27,15 @@ public class Solution2673 {
         return ans;
 
     }
+
+
+    public int minIncrements2(int n, int[] cost) {
+        int ans = 0;
+        for (int i = n - 2; i > 0; i -= 2) {
+            ans += Math.abs(cost[i] - cost[i + 1]);
+            // 叶节点 i 和 i+1 的双亲节点下标为 i/2（整数除法）
+            cost[i / 2] += Math.max(cost[i], cost[i + 1]);
+        }
+        return ans;
+    }
 }
