8e2a366d1fafc734e4e83c1dfecfc20d5827577f
xiaowangzi123 <1103694415@qq.com>
2024-02-29 17:27:46 +0800

    1768

--- all_leetcode_solution/src/main/java/com/leetcode/s1701_1800/Solution1768.java
+++ all_leetcode_solution/src/main/java/com/leetcode/s1701_1800/Solution1768.java
@@ -1,23 +1,21 @@
-package com.leetcode;
-
-import java.util.Map;
+package com.leetcode.s1701_1800;
 
 /**
- * @author :wyq
- * @date ：Created in 2021/11/6
- * @description : 交替合并字符串
+ * 交替合并字符串
  */
 public class Solution1768 {
     public static void main(String[] args) {
         Solution1768 obj = new Solution1768();
-        String word1 = "1234", word2 = "ab";
+        String word1 = "1234";
+        String word2 = "ab";
         System.out.println(obj.mergeAlternately(word1, word2));
         System.out.println(obj.mergeAlternately2(word1, word2));
     }
 
     public String mergeAlternately(String word1, String word2) {
         StringBuilder ans = new StringBuilder();
-        int n1 = word1.length(), n2 = word2.length();
+        int n1 = word1.length();
+        int n2 = word2.length();
         int i = 0;
         while (i < n1 || i < n2) {
             if (i < n1) {
@@ -32,7 +30,8 @@ public class Solution1768 {
     }
 
     public String mergeAlternately2(String word1, String word2) {
-        int n1 = word1.length(), n2 = word2.length();
+        int n1 = word1.length();
+        int n2 = word2.length();
         char[] ans = new char[n1 + n2];
         int i = 0, index = 0;
         while (i < n1 || i < n2) {
