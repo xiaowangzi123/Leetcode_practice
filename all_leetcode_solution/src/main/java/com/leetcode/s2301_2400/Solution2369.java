1b73e47ee1a99f1b1e79e0beec54fa56e16d8d89
xiaowangzi123 <1103694415@qq.com>
2024-03-01 09:59:07 +0800

    2396

--- all_leetcode_solution/src/main/java/com/leetcode/s2301_2400/Solution2369.java
+++ all_leetcode_solution/src/main/java/com/leetcode/s2301_2400/Solution2369.java
@@ -1,5 +1,7 @@
 package com.leetcode.s2301_2400;
 
+import java.util.Arrays;
+
 /**
  * 检查数组是否存在有效划分
  */
@@ -7,7 +9,9 @@ public class Solution2369 {
     public static void main(String[] args) {
         Solution2369 solution = new Solution2369();
         int[] nums = {4, 4, 4, 5, 6};
+        int[] nums2 = {1,1,1,2};
         System.out.println(solution.validPartition(nums));
+        System.out.println(solution.validPartition(nums2));
     }
 
     /**
@@ -17,8 +21,6 @@ public class Solution2369 {
      * 例如，子数组 [3,4,5] ，但是子数组 [1,3,5] 不符合要求。
      */
     public boolean validPartition(int[] nums) {
-
-
         int n = nums.length;
         boolean[] dp = new boolean[n + 1];
         dp[n] = true;
@@ -42,4 +44,39 @@ public class Solution2369 {
     }
 
 
+    public boolean validPartition2(int[] nums) {
+        int n = nums.length;
+        Boolean[] memo = new Boolean[n + 1];
+        // 初始化 memo 数组为 null
+        Arrays.fill(memo, null);
+
+        return dfs(nums, 0, memo);
+    }
+
+    private boolean dfs(int[] nums, int index, Boolean[] memo) {
+        if (index == nums.length) {
+            return true;
+        }
+
+        if (memo[index] != null) {
+            return memo[index];
+        }
+
+        boolean result = false;
+
+        if (index + 1 < nums.length && nums[index] == nums[index + 1]) {
+            result = dfs(nums, index + 2, memo);
+        }
+
+        if (!result && index + 2 < nums.length && nums[index] == nums[index + 1] && nums[index + 1] == nums[index + 2]) {
+            result = dfs(nums, index + 3, memo);
+        }
+
+        if (!result && index + 2 < nums.length && nums[index] + 1 == nums[index + 1] && nums[index + 1] + 1 == nums[index + 2]) {
+            result = dfs(nums, index + 3, memo);
+        }
+
+        memo[index] = result;
+        return result;
+    }
 }
