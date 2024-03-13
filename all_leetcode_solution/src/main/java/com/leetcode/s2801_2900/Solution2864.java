f05c874454d0152b53404691435a39eb754a79e9
xiaowangzi123 <1103694415@qq.com>
2024-03-13 17:19:38 +0800

    2864

--- all_leetcode_solution/src/main/java/com/leetcode/s2801_2900/Solution2864.java
+++ all_leetcode_solution/src/main/java/com/leetcode/s2801_2900/Solution2864.java
@@ -10,13 +10,14 @@ public class Solution2864 {
     }
 
     /**
-     * 美
+     * 想要最终成奇数，最后一个数字必须是1
+     * 统计1的个数为cnt1，应该有cnt1-1个1
      */
     public String maximumOddBinaryNumber(String s) {
         int cnt1 = (int) s.chars().filter(c -> c == '1').count();
         String s1 = repeatStr("1", cnt1 - 1);
-        String s2 = repeatStr("0",s.length()-cnt1);
-        return s1 + s2+"1";
+        String s2 = repeatStr("0", s.length() - cnt1);
+        return s1 + s2 + "1";
     }
 
     private String repeatStr(String str, int count) {
